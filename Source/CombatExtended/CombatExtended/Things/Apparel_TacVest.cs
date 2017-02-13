bfa0e2e6effb919b1a3ee2f87cb086ca7e7513df
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2017-02-13 23:00:00 +0100

    Backpack fix
    Extended tac vest draw height fix to backpack. Also fixed def error
    causing backpack and tac vest to occupy the same slot.

--- Source/CombatExtended/CombatExtended/Things/Apparel_TacVest.cs
+++ Source/CombatExtended/CombatExtended/Things/Apparel_TacVest.cs
@@ -10,8 +10,6 @@ namespace CombatExtended
 {
     public class Apparel_TacVest : Apparel_VisibleAccessory
     {
-        private const float YOffsetInterval_Clothes = 0.005f; // copy-pasted from PawnRenderer
-
         protected override float GetAltitudeOffset(Rot4 rotation)
         {
         	/* Pawn offset reference:
