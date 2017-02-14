aae0a33196a778ba45d1e0bfae6061826e725d49
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2017-02-14 16:39:04 +0100

    New suppression algorithm
    Changed the way suppressed pawns choose cover positions to run to.
    Should reduce extreme sillyness such as running out of a room to the
    other side of a wall.
    
    Also fixed Mechanoids having too little carrying capacity for god knows
    what reason.

--- Source/CombatExtended/CombatExtended/Jobs/JobGiver_RunForCover.cs
+++ Source/CombatExtended/CombatExtended/Jobs/JobGiver_RunForCover.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using RimWorld;
 using UnityEngine;
 using Verse;
@@ -8,11 +9,10 @@ namespace CombatExtended
 {
     class JobGiver_RunForCover : ThinkNode_JobGiver
     {
-        public const float maxCoverDist = 15f; //Maximum distance to run for cover to
+        public const float maxCoverDist = 10f; //Maximum distance to run for cover to
 
         protected override Job TryGiveJob(Pawn pawn)
         {
-            Log.Message("CE :: Trying to issue RunForCover job to " + pawn.ToString());
             //Calculate cover position
             CompSuppressable comp = pawn.TryGetComp<CompSuppressable>();
             if (comp == null)
@@ -30,7 +30,7 @@ namespace CombatExtended
             }
 
             //Sanity check
-            if (pawn.Position == coverPosition)
+            if (pawn.Position.Equals(coverPosition))
             {
                 return null;
             }
@@ -46,58 +46,64 @@ namespace CombatExtended
 
         public static bool GetCoverPositionFrom(Pawn pawn, IntVec3 fromPosition, float maxDist, out IntVec3 coverPosition)
         {
-            //First check if we have cover already
-            Vector3 coverVec = (fromPosition - pawn.Position).ToVector3().normalized;
-            IntVec3 coverCell = (pawn.Position.ToVector3Shifted() + coverVec).ToIntVec3();
-            Thing cover = coverCell.GetCover(pawn.Map);
-            if (pawn.Position.Standable(pawn.Map) && cover != null && !pawn.Position.ContainsStaticFire(pawn.Map))
-            {
-                coverPosition = pawn.Position;
-                return true;
-            }
-
             List<IntVec3> cellList = new List<IntVec3>(GenRadial.RadialCellsAround(pawn.Position, maxDist, true));
-            IntVec3 nearestPosWithPlantCover = IntVec3.Invalid; //Store the nearest position with plant cover here as a fallback in case we find no hard cover
+            IntVec3 bestPos = pawn.Position;
+            float bestRating = GetCellCoverRatingForPawn(pawn, pawn.Position, fromPosition);
 
-            //Go through each cell in radius around the pawn
-            foreach (IntVec3 cell in cellList)
+            if (bestRating <= 0)
             {
-                //Sanity checks
-                if (cell.IsValid 
-                    && cell.Standable(pawn.Map) 
-                    && !pawn.Map.pawnDestinationManager.DestinationIsReserved(cell)
-                    && pawn.CanReach(cell, PathEndMode.ClosestTouch, Danger.Deadly, false)
-                    )
+                // Go through each cell in radius around the pawn
+                Region pawnRegion = pawn.Position.GetRegion(pawn.Map);
+                List<Region> adjacentRegions = pawnRegion.NonPortalNeighbors.ToList();
+                foreach (IntVec3 cell in cellList)
                 {
-                    coverVec = (fromPosition - cell).ToVector3().normalized;    //The direction in which we want to have cover
-                    coverCell = (cell.ToVector3Shifted() + coverVec).ToIntVec3();   //The cell we check for cover
-                    cover = coverCell.GetCover(pawn.Map);
-                    if (cover != null)
+                    // Check for adjacency so we don't path to the other side of a wall or some such
+                    if (adjacentRegions.Contains(cell.GetRegion(pawn.Map)))
                     {
-                        //If the cover is a plant we store the location for later
-                        if (cover.def.category == ThingCategory.Plant && nearestPosWithPlantCover == IntVec3.Invalid)
+                        float cellRating = GetCellCoverRatingForPawn(pawn, cell, fromPosition);
+                        if (cellRating > bestRating)
                         {
-                            nearestPosWithPlantCover = cell;
-                        }
-                        //The cell has hard cover in the direction we want, so we return the cell and report success
-                        else
-                        {
-                            coverPosition = cell;
-                            return true;
+                            bestRating = cellRating;
+                            bestPos = cell;
                         }
                     }
                 }
             }
-            //No hard cover to move up to, use nearest plant cover instead and report success
-            if (nearestPosWithPlantCover.IsValid)
+            coverPosition = bestPos;
+            return bestRating >= 0;
+        }
+
+        private static float GetCellCoverRatingForPawn(Pawn pawn, IntVec3 cell, IntVec3 shooterPos)
+        {
+            // Check for invalid locations
+            if (!cell.IsValid || !cell.Standable(pawn.Map) || !pawn.CanReserveAndReach(cell, PathEndMode.OnCell, Danger.Deadly) || cell.ContainsStaticFire(pawn.Map))
             {
-                coverPosition = nearestPosWithPlantCover;
-                return true;
+                return -1;
             }
 
-            //No hard nor plant cover in the radius, report failure
-            coverPosition = IntVec3.Invalid;
-            return false;
+            float cellRating = 0;
+
+            //Check if cell has cover in desired direction
+            Vector3 coverVec = (shooterPos - cell).ToVector3().normalized;
+            IntVec3 coverCell = (cell.ToVector3Shifted() + coverVec).ToIntVec3();
+            Thing cover = coverCell.GetCover(pawn.Map);
+            cellRating += GetCoverRating(cover);
+
+            //Check time to path to that location
+            if (!pawn.Position.Equals(cell))
+            {
+                // float pathCost = pawn.Map.pathFinder.FindPath(pawn.Position, cell, TraverseMode.NoPassClosedDoors).TotalCost;
+                float pathCost = (pawn.Position - cell).LengthHorizontal;
+                cellRating = cellRating / pathCost;
+            }
+            return cellRating;
+        }
+
+        private static float GetCoverRating(Thing cover)
+        {
+            if (cover == null) return 0;
+            if (cover.def.category == ThingCategory.Plant) return cover.def.fillPercent; // Plant cover only has a random chance to block gunfire and is rated lower
+            return 1;
         }
     }
 }
