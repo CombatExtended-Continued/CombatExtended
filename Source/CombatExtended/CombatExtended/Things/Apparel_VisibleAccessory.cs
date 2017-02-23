a28300b1d652339d314c9b927058c5ae918a4d13
ProfoundDarkness <prodark77@gmail.com>
2017-02-23 06:16:48 -0700

    Modified DrawnWornExtras to reduce branching and assignments.
    Some of the changes include the fact that wearer.Spawned returns false if a pawn is being carried or is dead so was able to chop some parts out there.
    
    Some additional code to handle moving some features into xml and expand capability towards Apparel_VisibleAccessory on the head.
    
    The xml part hasn't been done as of this time and the mod is in a position to have that attempt and added feature chopped out easily while retaining the new logic.

--- Source/CombatExtended/CombatExtended/Things/Apparel_VisibleAccessory.cs
+++ Source/CombatExtended/CombatExtended/Things/Apparel_VisibleAccessory.cs
@@ -10,65 +10,83 @@ namespace CombatExtended
 {
     public abstract class Apparel_VisibleAccessory : Apparel
     {
-        public override void DrawWornExtras()
+    	const float _HeadOffset = 0.032f;
+    	const float _BodyOffset = 0.0269999985f;
+    	static Dictionary<string, bool> _OnHeadCache = new Dictionary<string, bool>();
+    	
+    	public override void DrawWornExtras()
         {
             if (wearer == null || !wearer.Spawned) return;
-            Vector3 drawVec = wearer.Drawer.DrawPos;
-            
-            // Early exit if the pawn is in a bed that doesn't show their body...
             Building_Bed bed = wearer.CurrentBed();
-            if (bed != null && !bed.def.building.bed_showSleeperBody) return;
+            if (bed != null && !bed.def.building.bed_showSleeperBody && !onHead) return;
             
-            /* //additional refactoring required to allow for this to work on head apparel...  Need a head based apparel for testing purposes first.
-            List<BodyPartGroupDef> parts = def.apparel.bodyPartGroups;
-            bool apparelHead = parts.Contains(BodyPartGroupDefOf.FullHead) || parts.Contains(BodyPartGroupDefOf.UpperHead) || parts.Contains(BodyPartGroupDefOf.Eyes);
-            if (bed != null && !bed.def.building.bed_showSleeperBody && !apparelHead) return;
-            */
-            
-            // Get wearer's base altitude, intentionally skip figuring this out if pawn is in a bed.
-            if (wearer.GetPosture() != PawnPosture.Standing && !wearer.Dead && wearer.CarriedBy == null)
+            //Since I haven't a head apparel item to test the drawing code against for now we throw an error (ONCE) and exit.
+            if (this.onHead)
             {
-           		drawVec.y = Altitudes.AltitudeFor(AltitudeLayer.LayingPawn) + 0.005f;
-            } else if (bed == null) {
-            	drawVec.y = Altitudes.AltitudeFor(AltitudeLayer.Pawn);
+            	Log.ErrorOnce(string.Concat("CombatExtended :: Apparel_VisibleAccessory: The head drawing code is incomplete and the apparel '",
+            	                            this.Label, "' will not be drawn."), this.def.debugRandomId);
+            	return;
             }
-            Vector3 s = new Vector3(1.5f, 1.5f, 1.5f);
+            
+            // compute angle and Rot4 vars
+            Rot4 rotation;
+            Rot4 bedRotation = new Rot4();
+            float angle = 0;
+            Vector3 drawVec = wearer.Drawer.DrawPos;
+            if (wearer.GetPosture() != PawnPosture.Standing) 
+            {
+                rotation = LayingFacing();
+                if (bed != null)
+                {
+                    bedRotation = bed.Rotation;
+                    bedRotation.AsInt += 2;
+                    angle = bedRotation.AsAngle;
+		            AltitudeLayer altitude = (AltitudeLayer)((byte)Mathf.Max((int)bed.def.altitudeLayer, 14));
+		            drawVec.y = wearer.Position.ToVector3ShiftedWithAltitude(altitude).y;
+		            drawVec += bedRotation.FacingCell.ToVector3() * (-wearer.Drawer.renderer.BaseHeadOffsetAt(Rot4.South).z);
+                } else {
+            		drawVec.y = wearer.Position.ToVector3ShiftedWithAltitude(AltitudeLayer.LayingPawn).y;
+	                if (wearer.Downed)  // wearer.Spawned == false when Pawn.Dead == true.
+	                {
+	                    float? newAngle = (((((wearer.Drawer == null) ? null : wearer.Drawer.renderer) == null) ? null : wearer.Drawer.renderer.wiggler) == null) ? (float?)null : wearer.Drawer.renderer.wiggler.downedAngle;
+	                    if (newAngle != null)
+	                        angle = newAngle.Value;
+	                }
+	                else
+	                {
+	                    angle = rotation.FacingCell.AngleFlat;
+	                }
+                }
+                drawVec.y += 0.005f;
+            } else {
+            	rotation = wearer.Rotation;
+            	drawVec.y = wearer.Position.ToVector3ShiftedWithAltitude(AltitudeLayer.Pawn).y;
+            }
+            
+            drawVec.y += GetAltitudeOffset(rotation);
+            
+            //if (!onHead)
+            //{
+            //	if (rotation == Rot4.North)
+            //		drawVec.y += _HeadOffset;
+            //	else
+            //		drawVec.y += _BodyOffset;
+            //} else if (onHead) {
+            //	if (rotation == Rot4.North)
+            //		drawVec.y += _BodyOffset;
+            //	else
+            //		drawVec.y += _HeadOffset;
+            //} else {
+            //	Log.ErrorOnce(string.Concat("CombatExtended :: Apparel_VisibleAccessory: Undefined state drawing apparel '", Label, "'."), def.debugRandomId);
+            //}
             
             // Get the graphic path
             string path = def.graphicData.texPath + "_" + ((wearer == null) ? null : wearer.story.bodyType.ToString());
             Graphic graphic = GraphicDatabase.Get<Graphic_Multi>(path, ShaderDatabase.Cutout, def.graphicData.drawSize, DrawColor);
             ApparelGraphicRecord apparelGraphic = new ApparelGraphicRecord(graphic, this);
 
-            // Downed check
-            Rot4 rotation = wearer.Rotation;
-            float angle = 0;
-            if(wearer.GetPosture() != PawnPosture.Standing)
-            {
-                rotation = LayingFacing();
-                if (bed != null)
-                {
-                    Rot4 bedRotation = bed.Rotation;
-                    bedRotation.AsInt += 2;
-                    angle = bedRotation.AsAngle;
-                    // Now that we have bed information we can calculate pawn altitude AND position for while they are in bed.
-                    AltitudeLayer altitude = (AltitudeLayer)((byte)Mathf.Max((int)bed.def.altitudeLayer, 14));
-                    drawVec = wearer.Position.ToVector3ShiftedWithAltitude(altitude);
-                    drawVec.y += 0.005f;
-                    drawVec += bedRotation.FacingCell.ToVector3() * (-wearer.Drawer.renderer.BaseHeadOffsetAt(Rot4.South).z);
-                }
-                else if (wearer.Downed || wearer.Dead)
-                {
-                    float? newAngle = (((((wearer.Drawer == null) ? null : wearer.Drawer.renderer) == null) ? null : wearer.Drawer.renderer.wiggler) == null) ? (float?)null : wearer.Drawer.renderer.wiggler.downedAngle;
-                    if (newAngle != null)
-                        angle = newAngle.Value;
-                }
-                else
-                {
-                    angle = rotation.FacingCell.AngleFlat;
-                }
-            }
-            drawVec.y += GetAltitudeOffset(rotation);
             Material mat = apparelGraphic.graphic.MatAt(rotation);
+            Vector3 s = new Vector3(1.5f, 1.5f, 1.5f);
 
             mat.shader = ShaderDatabase.CutoutComplex;
             mat.color = DrawColor;
@@ -120,5 +138,48 @@ namespace CombatExtended
             }
             return Rot4.Random;
         }
+		
+		//Utility, return if the apparel is worn on the head/body.        
+       	protected bool onHead
+    	{
+       		get {
+       			if (!_OnHeadCache.ContainsKey(def.defName))
+       			{
+		   			List<BodyPartRecord> parts = wearer.RaceProps.body.AllParts.Where(def.apparel.CoversBodyPart).ToList();
+		   			bool gotHit = false;
+		   			foreach (BodyPartRecord part in parts)
+		   			{
+		   				BodyPartRecord p = part;
+		   				while (p != null)
+		   				{
+		   					if (p.groups.Contains(BodyPartGroupDefOf.Torso))
+			   				{
+		   						_OnHeadCache.Add(def.defName, false);
+		   						gotHit = true;
+		   						break;
+			   				}
+		   					if (p.groups.Contains(BodyPartGroupDefOf.FullHead))
+		   				    {
+			   					_OnHeadCache.Add(def.defName, true);
+		   						gotHit = true;
+			   					break;
+		   				    }
+			   				p = p.parent;
+		   				}
+		   				if (gotHit)
+		   					break;
+		   			}
+		   			if (!_OnHeadCache.ContainsKey(def.defName))
+		   			{
+		   				Log.ErrorOnce(string.Concat("CombatExtended :: ", this.GetType(), " was unable to determine if body or head on item '", Label,
+		   				                            "', might the wearer be non-human?  Assuming apparel is on body."), def.debugRandomId);
+		   				_OnHeadCache.Add(def.defName, false);
+		   			}
+       			}
+       			bool ret;
+       			_OnHeadCache.TryGetValue(def.defName, out ret);  // is there a better way? Dictionary.Item isn't there.  Didn't bother with try/catch as by now it should have the key.
+       			return ret;
+       		}
+    	}
     }
 }
