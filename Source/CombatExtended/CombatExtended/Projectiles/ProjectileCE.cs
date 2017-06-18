2d0eb21465b3545fc50104af7afcd39be57df849
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2017-06-18 17:08:52 +0200

    Fixed wall-phasing bullets

--- Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE.cs
+++ Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE.cs
@@ -314,6 +314,7 @@ namespace CombatExtended
             var shotLine = new Ray(from, (dest - from));
 
             // Early opt-out, if we only moved by one cell only check the new cell
+            /*
             if ((newPos - lastPos).LengthManhattan == 1)
             {
                 if (DebugViewSettings.drawInterceptChecks)
@@ -322,6 +323,7 @@ namespace CombatExtended
                 }
                 return CheckCellForCollision(newPos, shotLine);
             }
+            */
 
             // Iterate through all cells between the last and the new position
             var cells = GenSight.PointsOnLineOfSight(lastPos, newPos);
