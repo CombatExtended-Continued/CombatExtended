3742f8f7f2c8b9daf45e490f7aa54db7f89fe39d
Alistaire <alistaire@gmx.com>
2017-11-10 18:20:58 +0100

    Compiled DLL, use of GenPlace (not GenSpawn).

--- Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE.cs
+++ Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE.cs
@@ -578,24 +578,24 @@ namespace CombatExtended
 	    
 	    //Spawn things if not an explosive but preExplosionSpawnThingDef != null
             if (comp == null
-	    	&& Position.IsValid
-		&& def.projectile.explosionSpawnChance > 0
-		&& def.projectile.preExplosionSpawnThingDef != null
-		&& Rand.Value < def.projectile.explosionSpawnChance)
+		    	&& Position.IsValid
+				&& def.projectile.explosionSpawnChance > 0
+				&& def.projectile.preExplosionSpawnThingDef != null
+				&& Rand.Value < def.projectile.explosionSpawnChance)
             {
-	    	var thingDef = def.projectile.preExplosionSpawnThingDef;
-		
-	    	//Exact contents of // Verse.Explosion.TrySpawnExplosionThing(ThingDef thingDef, IntVec3 c, int count)
-		if (thingDef.IsFilth)
-		{
-			FilthMaker.MakeFilth(Position, Map, thingDef, 1);
-		}
-		else
-		{
-			Thing thing = ThingMaker.MakeThing(thingDef, null);
-			thing.stackCount = 1;
-			GenSpawn.Spawn(thing, Position, Map);
-		}
+		    	var thingDef = def.projectile.preExplosionSpawnThingDef;
+				
+			    if (thingDef.IsFilth && Position.Walkable(this.Map))
+				{
+					FilthMaker.MakeFilth(Position, Map, thingDef, 1);
+				}
+				else
+				{
+					Thing reusableAmmo = ThingMaker.MakeThing(thingDef, null);
+					reusableAmmo.stackCount = 1;
+					reusableAmmo.SetForbidden(true, false);
+					GenPlace.TryPlaceThing(reusableAmmo, Position, Map, ThingPlaceMode.Near, null);
+				}
             }
 
             // Apply suppression around impact area
