29a0e597c4b444be351b0fdb65f6decf0315b750
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2017-11-29 19:47:11 +0100

    Reworked race patches
    Changed to new tool format.

---
+++ Source/CombatExtended/CombatExtended/ToolCE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace CombatExtended
+{
+    public class ToolCE : Tool
+    {
+        public float armorPenetration = 0;
+    }
+}
