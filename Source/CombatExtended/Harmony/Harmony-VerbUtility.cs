8754d24eb9d9aecf89242bcdb492bb9f6c1d5f31
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2017-12-13 17:47:30 +0100

    Fix for #364

---
+++ Source/CombatExtended/Harmony/Harmony-VerbUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Harmony;
+
+namespace CombatExtended.Harmony
+{
+    [HarmonyPatch(typeof(VerbUtility), "GetProjectile")]
+    internal static class Harmony_VerbUtility
+    {
+        internal static void Postfix(ref ThingDef __result, Verb verb)
+        {
+            if (__result == null)
+            {
+                var verbCE = verb as Verb_LaunchProjectileCE;
+                __result = verbCE?.ProjectileDef;
+                Log.Message("Set result to " + __result.ToStringSafe());
+            }
+        }
+    }
+}
