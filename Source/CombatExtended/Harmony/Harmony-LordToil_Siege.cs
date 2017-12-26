105958e4f2fd0cb288ab9e36ff4acbc3351d171e
Richard Shackleton <richardshackleton@netconstruct.co.uk>
2017-12-26 23:58:44 +0000

    Fix typecast bug and mortar shell spawning bug

---
+++ Source/CombatExtended/Harmony/Harmony-LordToil_Siege.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace CombatExtended.Harmony
+{
+    [HarmonyPatch(typeof(LordToil_Siege), "LordToilTick")]
+    internal static class Harmony_LordToil_Siege
+    {
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions.MethodReplacer(typeof(ThingDef).GetMethod("get_IsShell"),
+                typeof(AmmoUtility).GetMethod(nameof(AmmoUtility.IsShell), BindingFlags.Public | BindingFlags.Static));
+        }
+    }
+}
