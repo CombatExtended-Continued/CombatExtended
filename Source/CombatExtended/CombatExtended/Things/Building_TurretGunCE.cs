3a6d2cfbfcb55410a77f7e7980a0fd5b89109084
Alistaire <alistaire@gmx.com>
2018-01-01 01:31:21 +0100

    Removal of defaultProjectile and use of Projectile
    - Changed ProjectileDef -> Projectile (name of method) in some files, to
    be in accordance with B18's Verb_LaunchProjectile names
    - Replaced defaultProjectile mentions with something like Projectile to
    take into account ammoUsers and such
    - Took into account CompChangeableProjectile in all Projectile-type get
    methods

--- Source/CombatExtended/CombatExtended/Things/Building_TurretGunCE.cs
+++ Source/CombatExtended/CombatExtended/Things/Building_TurretGunCE.cs
@@ -36,6 +36,7 @@ namespace CombatExtended
         // New fields
         private CompAmmoUser compAmmo = null;
         private CompFireModes compFireModes = null;
+        private CompChangeableProjectile compChangeable = null;
         public bool isReloading = false;
         private int ticksUntilAutoReload = 0;
         
@@ -115,7 +116,7 @@ namespace CombatExtended
         }
 
         // New properties
-        public ThingDef ProjectileDef
+        public ThingDef Projectile
         {
             get
             {
@@ -123,10 +124,23 @@ namespace CombatExtended
                 {
                 	return CompAmmo.CurAmmoProjectile;
                 }
+                if (CompChangeable != null && CompChangeable.Loaded)
+                {
+                	return CompChangeable.Projectile;
+                }
                 return this.GunCompEq.PrimaryVerb.verbProps.defaultProjectile;
             }
         }
         
+        
+        public CompChangeableProjectile CompChangeable
+        {
+        	get
+        	{
+	            if (compChangeable == null && Gun != null) compChangeable = Gun.TryGetComp<CompChangeableProjectile>();
+	            return compChangeable;
+        	}
+        }
         public CompAmmoUser CompAmmo
         {
             get
@@ -334,7 +348,7 @@ namespace CombatExtended
             if (pawn != null)
             {
                 //if (this.GunCompEq.PrimaryVerb.verbProps.projectileDef.projectile.flyOverhead)
-            	if (ProjectileDef.projectile.flyOverhead)
+            	if (Projectile.projectile.flyOverhead)
                 {
                     RoofDef roofDef = base.Map.roofGrid.RoofAt(t.Position);
                     if (roofDef != null && roofDef.isThickRoof)
@@ -475,7 +489,7 @@ namespace CombatExtended
             float range = this.GunCompEq.PrimaryVerb.verbProps.range;
             float minRange = this.GunCompEq.PrimaryVerb.verbProps.minRange;
             Building t;
-            if (Rand.Value < 0.5f && ProjectileDef.projectile.flyOverhead && faction.HostileTo(Faction.OfPlayer) && base.Map.listerBuildings.allBuildingsColonist.Where(delegate (Building x)
+            if (Rand.Value < 0.5f && Projectile.projectile.flyOverhead && faction.HostileTo(Faction.OfPlayer) && base.Map.listerBuildings.allBuildingsColonist.Where(delegate (Building x)
             {
                 float num = (float)x.Position.DistanceToSquared(this.Position);
                 return num > minRange * minRange && num < range * range;
@@ -484,7 +498,7 @@ namespace CombatExtended
                 return t;
             }
             TargetScanFlags targetScanFlags = TargetScanFlags.NeedThreat;
-            if (!this.GunCompEq.PrimaryVerb.verbProps.defaultProjectile.projectile.flyOverhead)
+            if (!Projectile.projectile.flyOverhead)
             {
                 targetScanFlags |= TargetScanFlags.NeedLOSToAll;
                 targetScanFlags |= TargetScanFlags.LOSBlockableByGas;
@@ -498,7 +512,7 @@ namespace CombatExtended
             // Check for ammo first
             if (!base.Spawned
                 || (this.holdFire && this.CanToggleHoldFire) 
-                || (ProjectileDef.projectile.flyOverhead && base.Map.roofGrid.Roofed(base.Position))
+                || (Projectile.projectile.flyOverhead && base.Map.roofGrid.Roofed(base.Position))
                 || (CompAmmo != null && (isReloading || (mannableComp == null && CompAmmo.CurMagCount <= 0))))
             {
                 this.ResetCurrentTarget();
