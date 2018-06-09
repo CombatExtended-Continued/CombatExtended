3914e4af73fcfc5695a730363b45803546d71b72
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2018-06-09 19:33:30 +0200

    Suppression fix
    Experimental fix for pawns not respecting suppression. Now disables pathfinding when suppressed and not running for cover.

---
+++ Source/CombatExtended/CombatExtended/Jobs/JobGiver_ReactToSuppression.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace CombatExtended
+{
+    public class JobGiver_ReactToSuppression : ThinkNode_JobGiver
+    {
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            var comp = pawn.TryGetComp<CompSuppressable>();
+
+            if (comp == null) return null;
+
+            Job reactJob = SuppressionUtility.GetRunForCoverJob(pawn);
+
+            // Hunker down
+            if (reactJob == null && comp.IsHunkering)
+            {
+                LessonAutoActivator.TeachOpportunity(CE_ConceptDefOf.CE_Hunkering, pawn, OpportunityType.Critical);
+                return new Job(CE_JobDefOf.HunkerDown, pawn) { checkOverrideOnExpire = true };
+            }
+
+            // Run for cover
+            LessonAutoActivator.TeachOpportunity(CE_ConceptDefOf.CE_SuppressionReaction, pawn, OpportunityType.Critical);
+            return reactJob;
+        }
+    }
+}
