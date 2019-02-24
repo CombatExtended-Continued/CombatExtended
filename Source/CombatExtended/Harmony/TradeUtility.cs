33074526b23bf2f1a37e4cb312411e7eba76f965
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2019-02-24 02:23:55 +0100

    Fix ammo buy value, adv. launcher research position

---
+++ Source/CombatExtended/Harmony/TradeUtility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Harmony;
+using Verse;
+
+namespace CombatExtended.Harmony
+{
+    [HarmonyPatch(typeof(RimWorld.TradeUtility), "GetPricePlayerBuy")]
+    internal static class TradeUtility
+    {
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldc_R4 && (instruction.operand?.Equals(0.5f) ?? false))
+                {
+                    instruction.operand = 0.01f;
+                }
+
+                yield return instruction;
+            }
+        }
+    }
+}
