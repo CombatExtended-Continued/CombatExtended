034ef50e9aa0aed25fce0836dfcff194b8a7fc3f
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2019-06-14 22:16:49 +0200

    Tending outside does not stop infection untended timer

--- Source/CombatExtended/CombatExtended/Comps/HediffComp_InfecterCE.cs
+++ Source/CombatExtended/CombatExtended/Comps/HediffComp_InfecterCE.cs
@@ -10,80 +10,81 @@ namespace CombatExtended
 {
     public class HediffComp_InfecterCE : HediffComp
     {
-        private const float infectionInnerModifier = 3f;                            // Injuries to inner body parts will be this much more likely to get infected
+        private const float InfectionInnerModifier = 3f;                            // Injuries to inner body parts will be this much more likely to get infected
         private static readonly IntRange InfectionDelayHours = new IntRange(6, 12); // Infections will appear somewhere within this timeframe
 
-        private bool alreadyCausedInfection = false;
-        private int ticksUntilInfect = -1;
-        private float infectionModifier = 1;
-        private int ticksTended = 0;
+        private bool _alreadyCausedInfection = false;
+        private int _ticksUntilInfect = -1;
+        private float _infectionModifier = 1;
+        private int _ticksTended = 0;
+        private bool _tendedOutside;
 
-        public HediffCompProperties_InfecterCE Props { get { return (HediffCompProperties_InfecterCE)props; } }
+        public HediffCompProperties_InfecterCE Props => (HediffCompProperties_InfecterCE)props;
 
         private void CheckMakeInfection()
         {
-            ticksUntilInfect = -1;
-            infectionModifier *= Pawn.health.immunity.DiseaseContractChanceFactor(HediffDefOf.WoundInfection, parent.Part); // Apply pawn immunity
-            if (Pawn.def.race.Animal) infectionModifier *= 0.5f;
+            _ticksUntilInfect = -1;
+            _infectionModifier *= Pawn.health.immunity.DiseaseContractChanceFactor(HediffDefOf.WoundInfection, parent.Part); // Apply pawn immunity
+            if (Pawn.def.race.Animal) _infectionModifier *= 0.5f;
 
             // Adjust for difficulty
-            if (Pawn.Faction == Faction.OfPlayer) infectionModifier *= Find.Storyteller.difficulty.playerPawnInfectionChanceFactor;
+            if (Pawn.Faction == Faction.OfPlayer) _infectionModifier *= Find.Storyteller.difficulty.playerPawnInfectionChanceFactor;
 
             // Find out how long the wound was untreated
-            HediffComp_TendDuration compTended = parent.TryGetComp<HediffComp_TendDuration>();
-            int ticksUntended = parent.ageTicks;
+            var compTended = parent.TryGetComp<HediffComp_TendDuration>();
+            var ticksUntended = parent.ageTicks;
             if (compTended != null && compTended.IsTended)
             {
-                ticksUntended -= ticksTended;
+                ticksUntended -= _ticksTended;
 
-                infectionModifier /= Mathf.Pow(compTended.tendQuality + 0.75f, 2);  // Adjust infection chance based on tend quality
+                _infectionModifier /= Mathf.Pow(compTended.tendQuality + 0.75f, 2);  // Adjust infection chance based on tend quality
             }
-            float infectChance = Props.infectionChancePerHourUntended * (ticksUntended / GenDate.TicksPerHour); // Calculate base chance from time untreated
-            if (parent.Part.depth == BodyPartDepth.Inside) infectChance *= infectionInnerModifier;  // Increase chance of infection for inner organs
-            if (Rand.Value < infectChance * infectionModifier)
+            var infectChance = Props.infectionChancePerHourUntended * ((float)ticksUntended / GenDate.TicksPerHour); // Calculate base chance from time untreated
+            if (parent.Part.depth == BodyPartDepth.Inside) infectChance *= InfectionInnerModifier;  // Increase chance of infection for inner organs
+            if (Rand.Value < infectChance * _infectionModifier)
             {
-                alreadyCausedInfection = true;
+                _alreadyCausedInfection = true;
                 Pawn.health.AddHediff(HediffDefOf.WoundInfection, parent.Part);
             }
         }
 
         public override string CompDebugString()
         {
-            if (this.alreadyCausedInfection) return "already caused infection";
-            if (this.ticksUntilInfect <= 0) return "no infection will appear";
-            return "infection may appear after: " + ticksUntilInfect + " ticks (infection chance factor: " + infectionModifier.ToString() + ")";
+            if (this._alreadyCausedInfection) return "already caused infection";
+            if (this._ticksUntilInfect <= 0) return "no infection will appear";
+            return "infection may appear after: " + _ticksUntilInfect + " ticks (infection chance factor: " + _infectionModifier.ToString() + ")";
         }
 
         public override void CompExposeData()
         {
-            Scribe_Values.Look(ref alreadyCausedInfection, "alreadyCausedInfection", false);
-            Scribe_Values.Look(ref ticksUntilInfect, "ticksUntilInfect", -1);
-            Scribe_Values.Look(ref infectionModifier, "infectionModifier", 1);
+            Scribe_Values.Look(ref _alreadyCausedInfection, "alreadyCausedInfection", false);
+            Scribe_Values.Look(ref _ticksUntilInfect, "ticksUntilInfect", -1);
+            Scribe_Values.Look(ref _infectionModifier, "infectionModifier", 1);
         }
 
         public override void CompPostPostAdd(DamageInfo? dinfo)
         {
             // Determine time until infection check is made
-            if (!alreadyCausedInfection 
+            if (!_alreadyCausedInfection 
                 && !parent.Part.def.IsSolid(parent.Part, Pawn.health.hediffSet.hediffs) 
                 && !Pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(parent.Part) 
                 && !parent.IsPermanent())
             {
-                ticksUntilInfect = InfectionDelayHours.RandomInRange * GenDate.TicksPerHour;
+                _ticksUntilInfect = InfectionDelayHours.RandomInRange * GenDate.TicksPerHour;
             }
         }
 
         public override void CompPostTick(ref float severityAdjustment)
         {
-            if (parent.TryGetComp<HediffComp_TendDuration>().IsTended)
+            if (!_tendedOutside && parent.TryGetComp<HediffComp_TendDuration>().IsTended)
             {
-                ticksTended++;
+                _ticksTended++;
             }
 
-            if (!alreadyCausedInfection && ticksUntilInfect > 0)
+            if (!_alreadyCausedInfection && _ticksUntilInfect > 0)
             {
-                ticksUntilInfect--;
-                if (ticksUntilInfect == 0) CheckMakeInfection();
+                _ticksUntilInfect--;
+                if (_ticksUntilInfect == 0) CheckMakeInfection();
 
             }
         }
@@ -92,8 +93,9 @@ namespace CombatExtended
         {
             if (Pawn.Spawned)
             {
-                Room room = Pawn.GetRoom();
-                infectionModifier *= room == null ? 1.5f : room.GetStat(RoomStatDefOf.InfectionChanceFactor);
+                var room = Pawn.GetRoom();
+                _tendedOutside = room == null;
+                _infectionModifier *= room?.GetStat(RoomStatDefOf.InfectionChanceFactor) ?? 1.5f;
             }
         }
     }
