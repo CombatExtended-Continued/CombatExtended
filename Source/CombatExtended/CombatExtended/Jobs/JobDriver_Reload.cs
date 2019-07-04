55d0c779a835b1b9edbaddb1d8e9af94e4d976e1
LockdownX7 <lockdownx7@gmail.com>
2019-07-04 23:28:51 +0100

    Fixed #719

--- Source/CombatExtended/CombatExtended/Jobs/JobDriver_Reload.cs
+++ Source/CombatExtended/CombatExtended/Jobs/JobDriver_Reload.cs
@@ -142,8 +142,10 @@ namespace CombatExtended
             this.FailOn(HasNoGunOrAmmo);
 
             // Throw mote
-            if (compReloader.ShouldThrowMote)
+            if (compReloader.ShouldThrowMote && holder.Map != null)     //holder.Map is temporarily null after game load, skip mote if a pawn was reloading when game was saved
+            {
                 MoteMaker.ThrowText(pawn.Position.ToVector3Shifted(), holder.Map, string.Format("CE_ReloadingMote".Translate(), weapon.def.LabelCap));
+            }
 
             //Toil of do-nothing		
             Toil waitToil = new Toil() { actor = pawn }; // actor was always null in testing...
