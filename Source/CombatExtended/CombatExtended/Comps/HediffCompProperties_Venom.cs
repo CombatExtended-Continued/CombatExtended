4d904fbfae8c8e96bc09cb8d269522d5e2070fe6
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2019-08-01 23:11:50 +0200

    Venom system

---
+++ Source/CombatExtended/CombatExtended/Comps/HediffCompProperties_Venom.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace CombatExtended
+{
+    public class HediffCompProperties_Venom : HediffCompProperties
+    {
+        public float VenomPerSeverity;
+        public int MinTicks;
+        public int MaxTicks;
+
+        public HediffCompProperties_Venom()
+        {
+            compClass = typeof(HediffComp_Venom);
+        }
+    }
+}
---
+++ Source/CombatExtended/CombatExtended/DefOfs/CE_HediffDefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace CombatExtended
+{
+    [DefOf]
+    public static class CE_HediffDefOf
+    {
+        public static HediffDef VenomBuildup;
+    }
+}
