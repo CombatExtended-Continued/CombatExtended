851fc27f0f4a29398f9897d93799e75c226514f8
Alistaire <alistaire@gmx.com>
2019-08-26 14:24:22 +0200

    Add "TurretCaliber" StatDef which displays StatWorker_Caliber but on turrets
    Adds:
    - TurretCaliber StatDef which has showOnNonWorkTables = true
    - StatWorker_Caliber now gets the correct Thing Gun or ThingDef GunDef
    in each situation, also allowing for projectile stats of
    defaultProjectile to be displayed with the ammo system disabled.

--- Source/CombatExtended/CombatExtended/StatWorkers/StatWorker_Caliber.cs
+++ Source/CombatExtended/CombatExtended/StatWorkers/StatWorker_Caliber.cs
@@ -10,31 +10,69 @@ namespace CombatExtended
 {
     public class StatWorker_Caliber : StatWorker
     {
+        private ThingDef GunDef(StatRequest req)
+        {
+            var def = req.Def as ThingDef;
+
+            if (def?.building?.IsTurret ?? false)
+                def = def.building.turretGunDef;
+
+            return def;
+        }
+
+        private Thing Gun(StatRequest req)
+        {
+            return (req.Thing as Building_TurretGunCE)?.Gun ?? req.Thing;
+        }
+
         public override bool ShouldShowFor(StatRequest req)
         {
-            return base.ShouldShowFor(req) && (req.Def as ThingDef)?.GetCompProperties<CompProperties_AmmoUser>()?.ammoSet != null;
+            return base.ShouldShowFor(req) && 
+                (Controller.settings.EnableAmmoSystem
+                    ? GunDef(req)?.GetCompProperties<CompProperties_AmmoUser>()?.ammoSet != null
+                    : (GunDef(req)?.Verbs?.Any(x => x.defaultProjectile != null) ?? false));
         }
 
         public override string GetExplanationUnfinalized(StatRequest req, ToStringNumberSense numberSense)
         {
             StringBuilder stringBuilder = new StringBuilder();
-            var ammoProps = (req.Def as ThingDef)?.GetCompProperties<CompProperties_AmmoUser>();
-            if (ammoProps != null)
+
+            if (Controller.settings.EnableAmmoSystem)
             {
-                // Append various ammo stats
-                stringBuilder.AppendLine(ammoProps.ammoSet.LabelCap + "\n");
-                foreach (var cur in ammoProps.ammoSet.ammoTypes)
+                var ammoSet = GunDef(req)?.GetCompProperties<CompProperties_AmmoUser>().ammoSet;
+                if (ammoSet != null)
                 {
-                    string label = string.IsNullOrEmpty(cur.ammo.ammoClass.LabelCapShort) ? cur.ammo.ammoClass.LabelCap : cur.ammo.ammoClass.LabelCapShort;
-                    stringBuilder.AppendLine(label + ":\n" + cur.projectile.GetProjectileReadout(req.Thing));   //Is fine handling req.Thing == null, then it sets mult = 1
+                    // Append various ammo stats
+                    stringBuilder.AppendLine(ammoSet.LabelCap + "\n");
+                    foreach (var cur in ammoSet.ammoTypes)
+                    {
+                        string label = string.IsNullOrEmpty(cur.ammo.ammoClass.LabelCapShort) ? cur.ammo.ammoClass.LabelCap : cur.ammo.ammoClass.LabelCapShort;
+                        stringBuilder.AppendLine(label + ":\n" + cur.projectile.GetProjectileReadout(Gun(req)));   //Is fine handling req.Thing == null, then it sets mult = 1
+                    }
                 }
             }
+            else
+            {
+                var projectiles = GunDef(req)?.Verbs?.Where(x => x.defaultProjectile != null).Select(x => x.defaultProjectile);
+
+                foreach (var cur in projectiles)
+                    stringBuilder.AppendLine(cur.LabelCap + ":\n" + cur.GetProjectileReadout(Gun(req)));
+            }
+
             return stringBuilder.ToString().TrimEndNewlines();
         }
 
         public override string GetStatDrawEntryLabel(StatDef stat, float value, ToStringNumberSense numberSense, StatRequest optionalReq)
         {
-            return (optionalReq.Def as ThingDef)?.GetCompProperties<CompProperties_AmmoUser>().ammoSet.LabelCap;
+            if (Controller.settings.EnableAmmoSystem)
+            {
+                return GunDef(optionalReq)?.GetCompProperties<CompProperties_AmmoUser>()?.ammoSet?.LabelCap;
+            }
+            else
+            {
+                var projectiles = GunDef(optionalReq)?.Verbs?.Where(x => x.defaultProjectile != null).Select(x => x.defaultProjectile);
+                return projectiles.First().LabelCap + (projectiles.Count() > 1 ? "(+"+(projectiles.Count() - 1)+" more..)" : "");
+            }
         }
     }
 }
