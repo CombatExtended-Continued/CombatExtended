2ae74feb4cb8d0d223fb0dfdbda032fc4d0b0f22
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2019-10-19 01:11:45 +0200

    Fixed gas mask clipping

--- Source/CombatExtended/Harmony/Harmony_PawnGraphicSet.cs
+++ Source/CombatExtended/Harmony/Harmony_PawnGraphicSet.cs
@@ -9,8 +9,39 @@ using Verse;
 
 namespace CombatExtended.Harmony
 {
-    class Harmony_PawnGraphicSet
+    [HarmonyPatch(typeof(PawnGraphicSet), "MatsBodyBaseAt")]
+    internal static class Harmony_PawnGraphicSet
     {
+        private static bool RenderSpecial(ApparelLayerDef layer)
+        {
+            return (layer.GetModExtension<ApparelLayerExtension>()?.IsHeadwear ?? false)
+                   || layer.drawOrder > ApparelLayerDefOf.Shell.drawOrder;
+        }
+
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var write = false;
+
+            foreach (var code in instructions)
+            {
+                if (write)
+                {
+                    write = false;
+                    code.opcode = OpCodes.Brtrue;
+                }
+
+                if (code.opcode == OpCodes.Ldsfld && code.operand == AccessTools.Field(typeof(ApparelLayerDefOf), nameof(ApparelLayerDefOf.Overhead)))
+                {
+                    write = true;
+                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(Harmony_PawnGraphicSet), nameof(RenderSpecial)));
+                }
+                else
+                {
+                    yield return code;
+                }
+            }
+        }
+
         ///// <summary>
         ///// Patches renderer to treat Shell as a regular layer that obeys drawOrder, so apparel like backpacks can be drawn over it.
         ///// As Shell layer is normally drawn later in Harmony_PawnRenderer.RenderPawnInternal(),
--- Source/CombatExtended/Harmony/Harmony_PawnRenderer.cs
+++ Source/CombatExtended/Harmony/Harmony_PawnRenderer.cs
@@ -10,6 +10,87 @@ using Verse;
 
 namespace CombatExtended.Harmony
 {
+    [HarmonyPatch(typeof(PawnRenderer), "RenderPawnInternal",
+        typeof(Vector3), typeof(float), typeof(bool), typeof(Rot4), typeof(Rot4), typeof(RotDrawMode), typeof(bool), typeof(bool))]
+    internal static class Harmony_PawnRenderer_RenderPawnInternal
+    {
+        // Sync these with vanilla PawnRenderer constants
+        private const float YOffsetBehind = 0.00390625f;
+        private const float YOffsetHead = 0.02734375f;
+        private const float YOffsetOnHead = 0.03125f;
+        private const float YOffsetPostHead = 0.03515625f;
+        private const float YOffsetIntervalClothes = 0.00390625f;
+
+        private static void DrawHeadApparel(PawnRenderer renderer, Mesh mesh, Vector3 rootLoc, Vector3 headLoc, Vector3 headOffset, Rot4 bodyFacing, Quaternion quaternion, bool portrait, ref bool hideHair)
+        {
+            var apparelGraphics = renderer.graphics.apparelGraphics;
+            var headwearGraphics = apparelGraphics.Where(a => a.sourceApparel.def.apparel.LastLayer.GetModExtension<ApparelLayerExtension>()?.IsHeadwear ?? false).ToArray();
+            if(!headwearGraphics.Any())
+                return;
+
+            var interval = YOffsetIntervalClothes / headwearGraphics.Length;
+            var headwearPos = headLoc;
+
+            foreach (var apparelRecord in headwearGraphics)
+            {
+                if (!apparelRecord.sourceApparel.def.apparel.hatRenderedFrontOfFace)
+                {
+                    headwearPos.y += interval;
+                    hideHair = true;
+                    var apparelMat = apparelRecord.graphic.MatAt(bodyFacing);
+                    apparelMat = renderer.graphics.flasher.GetDamagedMat(apparelMat);
+                    GenDraw.DrawMeshNowOrLater(mesh, headwearPos, quaternion, apparelMat, portrait);
+                }
+                else
+                {
+                    var maskMat = apparelRecord.graphic.MatAt(bodyFacing);
+                    maskMat = renderer.graphics.flasher.GetDamagedMat(maskMat);
+                    var maskLoc = rootLoc + headOffset;
+                    maskLoc.y += !(bodyFacing == Rot4.North) ? YOffsetPostHead : YOffsetBehind;
+                    GenDraw.DrawMeshNowOrLater(mesh, maskLoc, quaternion, maskMat, portrait);
+                }
+            }
+        }
+
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var delete = false;
+            foreach (var code in instructions)
+            {
+                if (delete)
+                {
+                    if (code.opcode == OpCodes.Ldloc_S && ((LocalBuilder) code.operand).LocalIndex == 13)
+                    {
+                        delete = false;
+
+                        // Insert new calls for head renderer
+                        yield return new CodeInstruction(OpCodes.Ldarg_0);
+                        yield return new CodeInstruction(OpCodes.Ldloc_S, 14);
+                        yield return new CodeInstruction(OpCodes.Ldarg_1);
+                        yield return new CodeInstruction(OpCodes.Ldloc_S, 12);
+                        yield return new CodeInstruction(OpCodes.Ldloc_S, 9);
+                        yield return new CodeInstruction(OpCodes.Ldarg, 4);
+                        yield return new CodeInstruction(OpCodes.Ldloc_0);
+                        yield return new CodeInstruction(OpCodes.Ldarg, 7);
+                        yield return new CodeInstruction(OpCodes.Ldloca_S, 13);
+                        yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(Harmony_PawnRenderer_RenderPawnInternal), nameof(DrawHeadApparel)));
+
+                        yield return code;
+                    }
+
+                    continue;
+                }
+
+                if (code.opcode == OpCodes.Stloc_S && ((LocalBuilder) code.operand).LocalIndex == 14)
+                {
+                    delete = true;
+                }
+
+                yield return code;
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(PawnRenderer), "DrawEquipmentAiming")]
     internal static class Harmony_PawnRenderer_DrawEquipmentAiming
     {
