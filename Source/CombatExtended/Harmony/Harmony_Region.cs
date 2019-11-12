db0de122208f0635b79b824a1cfb2500707344e9
LockdownX7 <lockdownx7@gmail.com>
2019-11-12 17:01:21 +0000

    Remove performance-intensive smoke check

--- Source/CombatExtended/Harmony/Harmony_Region.cs
+++ Source/CombatExtended/Harmony/Harmony_Region.cs
@@ -8,19 +8,20 @@ using Verse;
 
 namespace CombatExtended.Harmony
 {
-    [HarmonyPatch(typeof(Region), "DangerFor")]
-    internal static class Harmony_Region_DangerFor
-    {
-        internal static void Postfix(Pawn p, Region __instance, ref Danger __result)
-        {
-            if (p.GetStatValue(CE_StatDefOf.SmokeSensitivity) > 0)
-            {
-                int smokeCount = __instance?.ListerThings?.AllThings?.Where(t => t is Smoke)?.Count() ?? 0;
-                if (smokeCount > 0)
-                {
-                    __result = Danger.Deadly;
-                }
-            }
-        }
-    }
+    /// Commented out over significant performance issues. Smoke count for the region needs to be cached in order to use this
+    //[HarmonyPatch(typeof(Region), "DangerFor")]
+    //internal static class Harmony_Region_DangerFor
+    //{
+    //    internal static void Postfix(Pawn p, Region __instance, ref Danger __result)
+    //    {
+    //        if (p.GetStatValue(CE_StatDefOf.SmokeSensitivity) > 0)
+    //        {
+    //            int smokeCount = __instance?.ListerThings?.AllThings?.Where(t => t is Smoke)?.Count() ?? 0;
+    //            if (smokeCount > 0)
+    //            {
+    //                __result = Danger.Deadly;
+    //            }
+    //        }
+    //    }
+    //}
 }
