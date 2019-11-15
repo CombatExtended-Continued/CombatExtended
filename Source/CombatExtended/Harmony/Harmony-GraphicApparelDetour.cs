1d8e44e60a6e7d9f1c3a9f00a734c58e65b64966
Dyrewulfe <6437156+Dyrewulfe@users.noreply.github.com>
2019-11-15 00:45:03 -0700

    Disable incompatible GraphicApparelDetour.dll detour injection.
    If GraphicApparelDetour.dll is found in the currently loaded assemblies, detour it's injection method. Fixes pawn layer rendering issues caused by mods including this assembly.

---
+++ Source/CombatExtended/Harmony/Harmony-GraphicApparelDetour.cs
@@ -0,0 +1,59 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+
+namespace CombatExtended.Harmony
+{
+
+    [HarmonyPatch]
+    class GraphicApparelDetour_Disable
+    {
+        static readonly string logPrefix = Assembly.GetExecutingAssembly().GetName().Name + " :: " + typeof(GraphicApparelDetour_Disable).Name + " :: ";
+        static List<Assembly> target_asses = new List<Assembly>();
+
+        static bool Prepare()
+        {
+            foreach (var ass in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (ass.FullName.Contains("GraphicApparelDetour"))
+                {
+                    target_asses.Add(ass);
+                }
+            }
+            if (target_asses.Any())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            foreach (var ass in target_asses)
+            {
+                foreach (var type in ass.GetTypes())
+                {
+                    MethodBase method_info = null;
+                    if (type.Name.Contains("InjectorThingy"))
+                    {
+                        method_info = AccessTools.Method(type, "InjectStuff");
+                    }
+                    if (method_info != null)
+                    {
+                        Log.Message($"{logPrefix}Disabling TryGetGraphicApparel detour injection in: {ass.FullName}");
+                        yield return method_info;
+                    }
+                }
+            }
+        }
+
+        static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
