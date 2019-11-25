0b0d06458946e7f3c4097f8ca336fc231252ee98
Dyrewulfe <6437156+Dyrewulfe@users.noreply.github.com>
2019-11-24 17:14:31 -0700

    Apparel compatibility for BBBodySupport, move compatibility patches to subdir.

---
+++ Source/CombatExtended/Harmony/Compatibility/Harmony-BBBodySupport.cs
@@ -0,0 +1,92 @@
+﻿using Harmony;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System;
+
+namespace CombatExtended.Harmony.Compatibility
+{
+    [HarmonyPatch]
+    class Harmony_Compat_BBBodySupport
+    {
+        static readonly string logPrefix = Assembly.GetExecutingAssembly().GetName().Name + " :: ";
+        static Assembly ass = AppDomain.CurrentDomain.GetAssemblies().
+                                SingleOrDefault(assembly => assembly.
+                                GetName().Name == "BBBodySupport");
+
+        private static bool IsHeadwear(ApparelLayerDef layer)
+        {
+            return layer.GetModExtension<ApparelLayerExtension>()?.IsHeadwear ?? false;
+        }
+
+        static bool Prepare()
+        {
+            if (ass.FullName.Contains("BBBodySupport"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            var found = false;
+            foreach (var t in ass.GetTypes())
+            {
+                foreach (var m in AccessTools.GetDeclaredMethods(t))
+                {
+                    if (m.Name.Contains("BBBody_ApparelPatch") || m.Name.Contains("BBBody_ApparelZombiefiedPatch"))
+                    {
+                        found = true;
+                        yield return m;
+                    }
+                }
+            }
+            if (found)
+            {
+                Log.Message($"{logPrefix}Applying compatibility patch for {ass.FullName}");
+            }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            bool patched = false;
+            bool ready = false;
+
+            List<CodeInstruction> patch = new List<CodeInstruction>
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldind_Ref),
+                new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Thing), nameof(Thing.def))),
+                new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ThingDef), nameof(ThingDef.apparel))),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Property(typeof(ApparelProperties), nameof(ApparelProperties.LastLayer)).GetGetMethod()),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Harmony_Compat_BBBodySupport), nameof(IsHeadwear))),
+                new CodeInstruction(OpCodes.Brtrue)
+            };
+
+            foreach (var code in instructions)
+            {
+                yield return code;
+                if (!patched)
+                {
+                    if (code.opcode == OpCodes.Ldsfld)
+                    {
+                        ready = true;
+                    }
+                    if (ready && code.opcode == OpCodes.Bne_Un)
+                    {
+                        patch.Last().operand = code.operand;
+                        foreach (var c in patch)
+                        {
+                            yield return c;
+                        }
+                        patched = true;
+                    }
+                }
+            }
+        }
+    }
+}
