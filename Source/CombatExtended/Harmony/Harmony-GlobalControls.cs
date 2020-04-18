8c4154d77521b44e0ec5182c8aa8d7d9866e93c9
Madara Uchiha <me@madara.ninja>
2020-04-19 00:59:19 +0300

    Introduce constant for the magic number
    Also introduce local for the xOffset from the original method.

--- Source/CombatExtended/Harmony/Harmony-GlobalControls.cs
+++ Source/CombatExtended/Harmony/Harmony-GlobalControls.cs
@@ -11,9 +11,12 @@ namespace CombatExtended.HarmonyCE
     [HarmonyPatch(typeof(GlobalControlsUtility), nameof(GlobalControlsUtility.DoDate))]
     internal static class Harmony_GlobalControls
     {
+        private const float magicExtraOffset = 8f;
+
         private static void Postfix(ref float curBaseY)
         {
-            Find.CurrentMap.GetComponent<WeatherTracker>().DoWindGUI(UI.screenWidth - 200f + 8f, ref curBaseY);
+            float offsetXFromOriginalMethod = UI.screenWidth - 200f;
+            Find.CurrentMap.GetComponent<WeatherTracker>().DoWindGUI(offsetXFromOriginalMethod + magicExtraOffset, ref curBaseY);
         }
     }
 }
