1d5ccde42fbd0cc60381a191e34475442ca60cc6
Madara Uchiha <me@madara.ninja>
2020-04-19 00:52:08 +0300

    Remove transpiler for displaying wind direction alert.
    Changes to a Postfix on DoDate(), similarly to FoodAlert.
    
    - Also fixes totally unplayable misalignment of label

--- Source/CombatExtended/Harmony/Harmony-GlobalControls.cs
+++ Source/CombatExtended/Harmony/Harmony-GlobalControls.cs
@@ -8,37 +8,12 @@ using Verse;
 
 namespace CombatExtended.HarmonyCE
 {
-    [HarmonyPatch(typeof(GlobalControls), "GlobalControlsOnGUI")]
+    [HarmonyPatch(typeof(GlobalControlsUtility), nameof(GlobalControlsUtility.DoDate))]
     internal static class Harmony_GlobalControls
     {
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        private static void Postfix(ref float curBaseY)
         {
-            var track = false;
-            var write = false;
-            var getComponentMethodInfo = AccessTools.Method(typeof(Map), nameof(Map.GetComponent), new Type[] { }).MakeGenericMethod(typeof(WeatherTracker));
-            foreach (var code in instructions)
-            {
-                if (write)
-                {
-                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Property(typeof(Find), nameof(Find.CurrentMap)).GetGetMethod());
-                    yield return new CodeInstruction(OpCodes.Call, getComponentMethodInfo);
-                    yield return new CodeInstruction(OpCodes.Ldloc_0);
-                    yield return new CodeInstruction(OpCodes.Ldloca_S, 1);
-                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(WeatherTracker), nameof(WeatherTracker.DoWindGUI)));
-                    write = false;
-                }
-                else if (track)
-                {
-                    write = code.opcode == OpCodes.Stloc_1;
-                    track = !write;
-                }
-                else
-                {
-                    track = code.operand is MethodInfo info && info == AccessTools.Method(typeof(WeatherManager), nameof(WeatherManager.DoWeatherGUI));
-                }
-
-                yield return code;
-            }
+            Find.CurrentMap.GetComponent<WeatherTracker>().DoWindGUI(UI.screenWidth - 200f + 8f, ref curBaseY);
         }
     }
 }
