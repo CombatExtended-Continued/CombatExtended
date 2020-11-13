ba67d7c1cb1a5480df360bdf2580807f97a9b55f
Logan Perkins <logan@git.lp-programming.com>
2020-11-12 16:53:37 -0800

    make patch silently skip when SOS2 not installed

--- Source/CombatExtended/Harmony/Compatibility/Harmony_SaveOurShip.cs
+++ Source/CombatExtended/Harmony/Compatibility/Harmony_SaveOurShip.cs
@@ -10,15 +10,46 @@ using Verse.AI;
 
 namespace CombatExtended.HarmonyCE.Compatibility
 {
-    [HarmonyPatch(typeof(RimWorld.WorkGiver_LoadTorpedoTube), "FindAmmo")]
+    [HarmonyPatch]
     class Harmony_Compatibility_SaveOurShip
     {
-	public static void Postfix(Pawn pawn, Building_ShipTurret tube, ref Thing __result)
+        static readonly Assembly ass = AppDomain.CurrentDomain.GetAssemblies().
+                                        SingleOrDefault(assembly => assembly.GetName().Name == "ShipsHaveInsides");
+        static MethodBase targetMethod = null;
+
+	internal static bool Prepare()
+	{
+	    if (ass == null)
+	    {
+		return false;
+	    }
+	    foreach (var t in ass.GetTypes())
+	    {
+		if (t.Name == "WorkGiver_LoadTorpedoTube")
+		{
+		    targetMethod = AccessTools.Method(t, "FindAmmo");
+		}
+	    }
+	    if (targetMethod == null)
+	    {
+		Log.Error($"Failed to find target method while attempting to patch SaveOurShip.");
+		return false;
+	    }
+	    return true;
+	}
+
+	internal static MethodBase TargetMethod()
+        {
+            return targetMethod;
+        }
+
+	public static void Postfix(Pawn pawn, Building_Turret tube, ref Thing __result)
 	{
 	    if (__result!=null) {
 		return;
 	    }
-	    StorageSettings allowedShellsSettings = ThingCompUtility.TryGetComp<CompChangeableProjectilePlural>(tube.gun).allowedShellsSettings;
+	    var turret = tube as Building_ShipTurret;
+	    StorageSettings allowedShellsSettings = ThingCompUtility.TryGetComp<CompChangeableProjectilePlural>(turret.gun).allowedShellsSettings;
 	    Predicate<Thing> pred = (Thing x) =>
             {
 	        if (!ForbidUtility.IsForbidden(x, pawn) && ReservationUtility.CanReserve(pawn, x, 1, -1, (ReservationLayerDef)null, false))
