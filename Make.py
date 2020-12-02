225e0fcad9873d0796da09b89b1521e3efebbb08
Logan Perkins <logan@git.lp-programming.com>
2020-12-01 19:17:03 -0800

    Make more verbose

--- Make.py
+++ Make.py
@@ -116,9 +116,9 @@ with XMLOpen(CSPROJ) as csproj:
             name = package.attributes['Include'].value
             version = package.attributes['Version'].value
             print(f"Downloading {version} of {name} from nuget")
-            if os.system(f"wget -q https://www.nuget.org/api/v2/package/{name}/{version} -O {tdir}/downloads/rwref-{idx}.zip"):
+            if os.system(f"wget https://www.nuget.org/api/v2/package/{name}/{version} -O {tdir}/downloads/rwref-{idx}.zip"):
                 raise Exception(f"Can't find version {version} of {name} on nuget")
-            os.system(f"unzip -q -o {tdir}/downloads/rwref-{idx}.zip -d {tdir}/downloads/unpack")
+            os.system(f"unzip -o {tdir}/downloads/rwref-{idx}.zip -d {tdir}/downloads/unpack")
         os.system(f"cp -r {tdir}/downloads/unpack/ref/net472/* {tdir}/rwreference")
         os.system(f"cp -r {tdir}/downloads/unpack/lib/net472/* {tdir}/rwreference")
     
