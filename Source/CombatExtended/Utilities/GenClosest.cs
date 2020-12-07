64e46dc8d1e2c4f61b43bf3354e0bbd443dfdac0
Karim Batbouta <kbatbouta66@gmail.com>
2020-12-06 17:27:03 -0800

    Added ThingsTracker

---
+++ Source/CombatExtended/Utilities/GenClosest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace CombatExtended.Utilities
+{
+    public static class GenClosest
+    {
+        public static ThingTracker GetThingTracker(this Map map)
+        {
+            return ThingTracker.GetTracker(map);
+        }
+
+        public static IEnumerable<Thing> SimilarInRange(this Thing thing, float range)
+        {
+            ThingTracker tracker = thing.Map.GetThingTracker();
+            return tracker.SimilarInRangeOf(thing, range);
+        }
+
+        public static IEnumerable<Thing> ThingsByDefInRange(this IntVec3 cell, Map map, ThingDef thingDef, float range)
+        {
+            ThingTracker tracker = map.GetThingTracker();
+            return tracker.ThingsInRangeOf(thingDef, cell, range);
+        }
+
+        public static IEnumerable<Pawn> PawnsInRange(this IntVec3 cell, Map map, float range)
+        {
+            ThingTracker tracker = map.GetThingTracker();
+            return tracker.ThingsInRangeOf(null, cell, range).Select(t => t as Pawn);
+        }
+    }
+}
