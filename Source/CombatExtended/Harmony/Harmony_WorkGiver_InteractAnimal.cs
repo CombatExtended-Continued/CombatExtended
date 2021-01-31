dc50fca55d16a2007a1703baa360da5086ec9d62
simplyWiri <48577820+simplyWiri@users.noreply.github.com>
2021-02-01 10:19:19 +1100

    - Add changed JobFailReason

--- Source/CombatExtended/Harmony/Harmony_WorkGiver_InteractAnimal.cs
+++ Source/CombatExtended/Harmony/Harmony_WorkGiver_InteractAnimal.cs
@@ -14,6 +14,8 @@ namespace CombatExtended.HarmonyCE
     [HarmonyPatch(typeof(WorkGiver_InteractAnimal), "TakeFoodForAnimalInteractJob", new Type[] { typeof(Pawn), typeof(Pawn) })]
     public class Harmony_WorkGiver_InteractAnimal_TakeFoodForAnimalInteractJob
     {
+        public static bool rejectedInventoryMass = false;
+
         public static void Postfix(WorkGiver_InteractAnimal __instance, ref Job __result, Pawn pawn, Pawn tamee)
         {
             if (__result != null)
@@ -32,12 +34,34 @@ namespace CombatExtended.HarmonyCE
                         __result.count = Mathf.Min(numToCarry, maxCount);
                         pawn.Notify_HoldTrackerItem(__result.targetA.Thing, __result.count);
                     }
-                    else 
+                    else
                     {
+                        rejectedInventoryMass = true;
                         __result = null;
                     }
                 }
             }
         }
+    }
+
+    [HarmonyPatch]
+    public static class Harmony_MessageFailPatch
+    {
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method(typeof(WorkGiver_Tame), nameof(WorkGiver_Tame.JobOnThing));
+            yield return AccessTools.Method(typeof(WorkGiver_Train), nameof(WorkGiver_Train.JobOnThing));
+        }
+
+        public static void Postfix()
+        {
+            if (Harmony_WorkGiver_InteractAnimal_TakeFoodForAnimalInteractJob.rejectedInventoryMass)
+            {
+                JobFailReason.Is("CE_InventoryFull_TameFail".TranslateSimple());
+                Harmony_WorkGiver_InteractAnimal_TakeFoodForAnimalInteractJob.rejectedInventoryMass = false;
+            }
+
+        }
+
     }
 }
