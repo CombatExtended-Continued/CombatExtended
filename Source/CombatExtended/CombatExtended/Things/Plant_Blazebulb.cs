e7370b74078e656b129b98ced2e9077831403c62
LockdownX7 <lockdownx7@gmail.com>
2021-04-12 18:45:57 +0100

    Fix null map error when blazebulb dies

--- Source/CombatExtended/CombatExtended/Things/Plant_Blazebulb.cs
+++ Source/CombatExtended/CombatExtended/Things/Plant_Blazebulb.cs
@@ -21,14 +21,17 @@ namespace CombatExtended
         public override void TickLong()
         {
             base.TickLong();
-            float temperature = Position.GetTemperature(base.Map);
-            if (temperature > IgnitionTemp)
+            if (!Destroyed)
             {
-                float ignitionChance = 0.005f * Mathf.Pow((temperature - IgnitionTemp), 2);
-                float rand = UnityEngine.Random.value;
-                if(UnityEngine.Random.value < ignitionChance)
+                float temperature = Position.GetTemperature(base.Map);
+                if (temperature > IgnitionTemp)
                 {
-                    FireUtility.TryStartFireIn(Position, base.Map, 0.1f);
+                    float ignitionChance = 0.005f * Mathf.Pow((temperature - IgnitionTemp), 2);
+                    float rand = UnityEngine.Random.value;
+                    if (UnityEngine.Random.value < ignitionChance)
+                    {
+                        FireUtility.TryStartFireIn(Position, base.Map, 0.1f);
+                    }
                 }
             }
         }
