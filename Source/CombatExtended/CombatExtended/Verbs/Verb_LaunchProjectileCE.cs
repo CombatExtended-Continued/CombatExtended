4e90e861ee5b3960084af4650dff1ce37dc55db0
Logan Perkins <logan@git.lp-programming.com>
2021-04-23 12:05:38 -0700

    Set aperature size to 0.03f by default

--- Source/CombatExtended/CombatExtended/Verbs/Verb_LaunchProjectileCE.cs
+++ Source/CombatExtended/CombatExtended/Verbs/Verb_LaunchProjectileCE.cs
@@ -597,7 +597,8 @@ namespace CombatExtended
             if (Projectile.projectile is ProjectilePropertiesCE pprop) {
                 instant = pprop.isInstant;
                 spreadDegrees = (EquipmentSource?.GetStatValue(StatDef.Named("ShotSpread")) ?? 0) * pprop.spreadMult;
-		aperatureSize = (EquipmentSource?.GetStatValue(StatDef.Named("AperatureSize")) ?? 0.03f);
+		aperatureSize = 0.03f; //(EquipmentSource?.GetStatValue(StatDef.Named("AperatureSize"))? ?? 0.03f);
+		
             }
 
             ShiftVecReport report = ShiftVecReportFor(currentTarget);
