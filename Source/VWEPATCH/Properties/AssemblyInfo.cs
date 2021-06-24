01963738eba2dc1e1d7e5006b1955171f7356dec
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-06-24 19:21:47 +0200

    Update AssemblyInfo.cs

--- Source/VWEPATCH/Properties/AssemblyInfo.cs
+++ Source/VWEPATCH/Properties/AssemblyInfo.cs
@@ -2,9 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
-// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
-// powiązane z zestawem.
+
 [assembly: AssemblyTitle("VWEPATCH")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +12,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
-// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
-// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+
 [assembly: ComVisible(false)]
 
-// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+
 [assembly: Guid("fd3fdba3-df19-46fa-b8eb-10da2b21ef43")]
 
-// Informacje o wersji zestawu zawierają następujące cztery wartości:
-//
-//      Wersja główna
-//      Wersja pomocnicza
-//      Numer kompilacji
-//      Poprawka
-//
-// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
-// przy użyciu symbolu „*”, tak jak pokazano poniżej:
-// [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
