b1f94eeabc725623c27a86b706a24a0e3aea2954
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-06-24 18:55:58 +0200

    Added my source code

---
+++ Source/VWEPATCH/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using CombatExtended;
+using UnityEngine;
+using Verse.Sound;
+
+namespace VWEPATCH
+{
+	public class Verb_ShootCEKEK : Verb_ShootCE
+	{
+
+
+
+		protected override int ShotsPerBurst
+		{
+			get
+			{
+				if (base.CompFireModes != null)
+				{
+					if (base.CompFireModes.CurrentFireMode == FireMode.SingleFire)
+					{
+
+						int lint = Rand.Range(0, 2);
+						if (lint != 0)
+						{
+							return lint;
+						}
+						else
+						{
+							if (Rand.Range(0, 1000) == 1000)
+							{
+								GenExplosionCE.DoExplosion(caster.Position, caster.Map, 2f, DamageDefOf.Bomb, null, 5, 0f, SoundDefOf.Psycast_Skip_Exit, null, null, null, null, 0, 0, true, null, 0f, 0, 0f, false);
+							}
+							return 0;
+						}
+
+					}
+					if (base.CompFireModes.CurrentFireMode == FireMode.BurstFire)
+					{
+						return Rand.Range(0, base.CompFireModes.Props.aimedBurstShotCount + 1);
+					}
+				}
+				return Rand.Range(0, base.VerbPropsCE.burstShotCount + 1);
+			}
+		}
+	}
+	public class RandomDamage : ThingComp
+	{
+		public int max_damage  => Props.max_damage;
+		public int Pellets => Props.pelletsMax;
+		public int pletts => Props.pelletsMin;
+		
+		public RandDamageProps Props => (RandDamageProps)this.props;
+		public override void Initialize(CompProperties props)
+		{
+			AmmoDef me = parent.def as AmmoDef;
+			ProjectilePropertiesCE nomo = me.projectile as ProjectilePropertiesCE;
+			nomo.speed = Rand.Range(50, 200);
+			
+			//Log.Error(nomo.pelletCount.ToString());
+			//nomo.armorPenetrationSharp
+			
+			
+			
+			base.Initialize(props);
+		}
+		public override void CompTick()
+		{
+			AmmoDef me = parent.def as AmmoDef;
+			ProjectilePropertiesCE nomo = me.projectile as ProjectilePropertiesCE;
+			nomo.pelletCount = Rand.Range(pletts, Pellets);
+			//Log.Error(nomo.pelletCount.ToString());
+		}
+		
+
+	}
+	public class RandDamageProps : CompProperties
+	{
+		public int pelletsMax;
+		public int max_damage;
+		public int pelletsMin;
+		public RandDamageProps()
+		{
+			this.compClass = typeof(RandomDamage);
+		}
+
+		public RandDamageProps(Type compClass) : base(compClass)
+		{
+			this.compClass = compClass;
+		}
+	}
+}
+
---
+++ Source/VWEPATCH/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("VWEPATCH")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VWEPATCH")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("fd3fdba3-df19-46fa-b8eb-10da2b21ef43")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
