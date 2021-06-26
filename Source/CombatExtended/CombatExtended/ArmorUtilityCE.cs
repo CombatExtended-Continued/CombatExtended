701aec04ea1aa082970e2e5bdde76be8eb72005c
Tostov <57082718+Tostov@users.noreply.github.com>
2021-06-26 20:20:45 +1000

    Minimum damage to 0

--- Source/CombatExtended/CombatExtended/ArmorUtilityCE.cs
+++ Source/CombatExtended/CombatExtended/ArmorUtilityCE.cs
@@ -239,7 +239,7 @@ namespace CombatExtended
                     var armorDamagePen = penAmount * 2f - armorAmount;
                     var armorDmgMult = fullDeflect ? 0 : penAmount == 0 ? 1 : Mathf.Clamp01(armorDamagePen / penAmount);
                     var armorDmgAmount = dmgAmount * armorDmgMult;
-                    var armorDamage = Mathf.Max(1, armorDmgAmount);
+                    var armorDamage = Mathf.Max(0, armorDmgAmount);
                     armor.TakeDamage(new DamageInfo(def, Mathf.CeilToInt(armorDamage)));
                 }
             }
