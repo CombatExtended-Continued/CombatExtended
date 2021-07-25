09b5acc5f5c70e669a279bf8d2e920c1c70815ca
Andross <25396698+androsshi@users.noreply.github.com>
2021-07-25 13:33:36 -0400

    Revert "Revert "Night Vision Flares""

--- Source/CombatExtended/CombatExtended/DefOfs/CE_ThingDefOf.cs
+++ Source/CombatExtended/CombatExtended/DefOfs/CE_ThingDefOf.cs
@@ -10,6 +10,10 @@ namespace CombatExtended
     [DefOf]
     public static class CE_ThingDefOf
     {
+        public static ThingDef Mote_FlareSmoke;
+
+        public static ThingDef Mote_FlareGlow;
+
         public static ThingDef Mote_SuppressIcon;
 
         public static ThingDef Mote_HunkerIcon;
@@ -17,6 +21,8 @@ namespace CombatExtended
         public static FleckDef Mote_EmptyCasing;
 
         public static ThingDef FSX;
+
+        public static ThingDef Flare;
 
         public static ThingDef ExplosionCE;
 
