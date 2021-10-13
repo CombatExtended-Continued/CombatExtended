d136afea515f4751929e2b47523a22473df28bf7
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-10-13 21:43:50 +0200

    Uploaded faction patcher source code

---
+++ Source/CombatExtended/Compatibility/PawnKindAutoPatcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+using RimWorld.Planet;
+
+namespace CombatExtended.Compatibility
+{
+    public class PawnKindPatcher : WorldComponent
+    {
+        public PawnKindPatcher(World world) : base(world)
+        {
+        }
+        public override void FinalizeInit()
+        {
+            List<PawnKindDef> stuff = DefDatabase<PawnKindDef>.AllDefs.ToList().FindAll(i => i.modExtensions?.Any(tt => !(tt is LoadoutPropertiesExtension)) ?? true && i.RaceProps.Animal == false && i.RaceProps.IsMechanoid == false);
+            foreach (PawnKindDef thin in stuff)
+            {
+
+
+                thin.modExtensions = new List<DefModExtension>();
+                thin.modExtensions.Add(new LoadoutPropertiesExtension { primaryMagazineCount = new FloatRange { min = 2, max = 5 } });
+                LoadoutPropertiesExtension aadada = thin.modExtensions.Find(tt => tt is LoadoutPropertiesExtension) as LoadoutPropertiesExtension;
+           
+            }
+            base.FinalizeInit();
+        }
+    }
+}
