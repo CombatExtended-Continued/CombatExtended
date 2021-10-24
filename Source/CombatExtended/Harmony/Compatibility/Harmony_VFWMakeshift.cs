65d3832c0a5832cb112b4bd83109535bddac5c8b
Logan Perkins <logan@git.lp-programming.com>
2021-10-24 00:16:13 -0700

    Add harmony patch to skip redundant VFW gizmos

---
+++ Source/CombatExtended/Harmony/Compatibility/Harmony_VFWMakeshift.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+using CombatExtended.Compatibility;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CombatExtended.HarmonyCE.Compatibility
+{
+    public static class Harmony_VanillaWeaponsExpandedMakeshift
+    {
+        [HarmonyPatch]
+        public class Harmony_Patch_PawnVerbGizmoUtility
+        {
+            private const string targetType = "MVCF.Utilities.PawnVerbGizmoUtility";
+            private const string targetMethod = "GetGizmosForVerb";
+
+            private static MethodBase _target;
+
+            public static bool Prepare()
+            {
+                return (_target = AccessTools.Method($"{targetType}:{targetMethod}")) != null;
+            }
+
+            public static MethodBase TargetMethod()
+            {
+                return _target;
+            }
+
+	    public static void Postfix(ref IEnumerable<Gizmo> __result, Verb verb)
+            {
+		if (verb is Verb_ShootCEKEK)
+		{
+		    __result = new List<Gizmo>();
+		}
+            }
+        }
+    }
+}
