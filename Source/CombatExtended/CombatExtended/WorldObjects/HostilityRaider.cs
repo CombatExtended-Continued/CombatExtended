91acecfa75b1f0520aa8174e9228c8b14ccca9d4
DullSaurcer <kbatbouta66@gmail.com>
2021-11-19 14:25:29 -0800

    Update HostilityRaider.cs

--- Source/CombatExtended/CombatExtended/WorldObjects/HostilityRaider.cs
+++ Source/CombatExtended/CombatExtended/WorldObjects/HostilityRaider.cs
@@ -9,12 +9,15 @@ using Verse;
 
 namespace CombatExtended.WorldObjects
 {
+    // TODO Rework all of this
+    //
+    //
     public class HostilityRaider : IExposable
     {
         public HostilityComp comp;
 
         private int ticksToRaid = -1;
-        private GlobalTargetInfo targetInfo;        
+        private IncidentParms parms;        
         private float points = -1;
 
         public HostilityRaider()
@@ -28,12 +31,13 @@ namespace CombatExtended.WorldObjects
                 ticksToRaid -= WorldObjectTrackerCE.THROTTLED_TICK_INTERVAL;
                 return;
             }
-            if (targetInfo.IsValid)
+            if (parms != null)
             {
-                DoRaid();
+                IncidentDef incidentDef = IncidentDefOf.RaidEnemy;
+                incidentDef.Worker.TryExecute(parms);
             }
             points = -1;
-            targetInfo = GlobalTargetInfo.Invalid;
+            parms = null;
             ticksToRaid = -1;
         }
 
@@ -48,48 +52,46 @@ namespace CombatExtended.WorldObjects
             {
                 return false;
             }
-            this.points = points;
-            targetInfo = new GlobalTargetInfo(IntVec3.Zero, targetMap);
-            targetInfo.tileInt = targetMap.Tile;                        
-            ticksToRaid = Rand.Range(3000, 15000);            
-            return true;
-        }
+            this.points = points;                                   
+            ticksToRaid = Rand.Range(3000, 15000);
 
-        private void DoRaid()
-        {
             string factionName = $"<color=red>{comp.parent.Faction.Name}</color>";
             string objectName = $"<color=blue>{comp.parent.Label}</color>";
+
             StorytellerComp storytellerComp = Find.Storyteller.storytellerComps.First((StorytellerComp x) => x is StorytellerComp_OnOffCycle || x is StorytellerComp_RandomMain);
-            IncidentParms parms = storytellerComp.GenerateParms(IncidentCategoryDefOf.ThreatBig, Find.CurrentMap);            
+            parms = storytellerComp.GenerateParms(IncidentCategoryDefOf.ThreatBig, Find.CurrentMap);
             parms.faction = comp.parent.Faction;
             parms.points = points;
-            parms.customLetterDef = CE_LetterDefOf.CE_ThreatBig;            
+            parms.customLetterDef = CE_LetterDefOf.CE_ThreatBig;
             parms.customLetterLabel = "CE_Message_CounterRaid_Label".Translate(factionName);
             parms.customLetterText = "CE_Message_CounterRaid_Desc".Translate(factionName, objectName);
+            parms.raidStrategy = RaidStrategyDefOf.ImmediateAttack;
             if (comp.parent.Faction.def.techLevel >= TechLevel.Industrial)
             {
-                if(Rand.Chance(Mathf.Min(points / 5000, 0.5f)))
+                
+                if (Rand.Chance(Mathf.Min(points / 10000, 0.5f)))
                 {
-                    parms.raidArrivalMode =  PawnsArrivalModeDefOf.CenterDrop;
+                    parms.raidArrivalMode = PawnsArrivalModeDefOf.CenterDrop;
+                }
+                else if (Rand.Chance(0.25f))
+                {
+                    parms.raidArrivalMode = PawnsArrivalModeDefOf.RandomDrop;
                 }
                 else
                 {
                     parms.raidArrivalMode = PawnsArrivalModeDefOf.EdgeDrop;
-                }
+                }                
             }
             else
             {
                 parms.raidArrivalMode = PawnsArrivalModeDefOf.EdgeWalkIn;
-            }            
-            IncidentDef incidentDef = IncidentDefOf.RaidEnemy;            
-            List<RaidStrategyDef> source = DefDatabase<RaidStrategyDef>.AllDefs.Where((RaidStrategyDef s) => s.Worker.CanUseWith(parms, PawnGroupKindDefOf.Combat)).ToList();                            
-            parms.raidStrategy = source.RandomElement();
-            incidentDef.Worker.TryExecute(parms);
-        }
+            }                               
+            return true;
+        }      
 
         public void ExposeData()
-        {            
-            Scribe_TargetInfo.Look(ref targetInfo, "targetInfo");
+        {
+            Scribe_Deep.Look(ref parms, "raidParms");
             Scribe_Values.Look(ref points, "points");
             Scribe_Values.Look(ref ticksToRaid, "ticksToRaid");
         }
