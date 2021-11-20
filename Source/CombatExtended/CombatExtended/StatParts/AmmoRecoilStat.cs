c0797f500d565781a28f3d594f5c1d3d26ad8469
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-11-21 00:35:36 +0100

    Uploaded the statpart
    Uploaded the loaded ammo statpart code

---
+++ Source/CombatExtended/CombatExtended/StatParts/AmmoRecoilStat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace CombatExtended.CombatExtended.StatParts
+{
+	public class StatPart_Ammo : StatPart
+	{
+		public override void TransformValue(StatRequest req, ref float val)
+		{
+			float num;
+
+			bool HasAmUser = req.Thing.TryGetComp<CompAmmoUser>() != null;
+
+			if (HasAmUser)
+			{
+				bool HasAmmo = req.Thing.TryGetComp<CompAmmoUser>().CurrentAmmo != null;
+				if (HasAmmo)
+				{
+					num = req.Thing.TryGetComp<CompAmmoUser>().CurrentAmmo.RecoilMult;
+					val *= num;
+					Log.Message(num.ToString());
+				}
+				
+			}
+		}
+
+		public override string ExplanationPart(StatRequest req)
+		{
+			float f;
+
+			bool HasAmUser = req.Thing.TryGetComp<CompAmmoUser>() != null;
+
+			if (HasAmUser)
+			{
+				bool HasAmmo = req.Thing.TryGetComp<CompAmmoUser>().CurrentAmmo != null;
+				if (HasAmmo)
+				{
+					f = req.Thing.TryGetComp<CompAmmoUser>().CurrentAmmo.RecoilMult;
+					return "Current ammunition: " + f.ToString();
+				}
+
+					
+			}
+			return null;
+		}
+
+	}
+}
