dd63dcc8446f88eaea2698ce262bd56b78783c13
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-11-21 16:45:21 +0100

    Uploaded defof file
    Uploaded defof file

---
+++ Source/CombatExtended/CombatExtended/DefOfs/CE_Bipod_DefOfs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace CombatExtended
+{
+	[DefOf]
+	public class BipodDefsOfs : DefOf
+	{
+		public static JobDef JobDef_SetUpBipod;
+	}
+}
