46746a103d6dd966cd2529f82ca38531628020d6
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-11-21 20:51:53 +0100

    Uploaded last thing

---
+++ Source/CombatExtended/CombatExtended/StatParts/StatPart_Bipod.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CombatExtended;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.Sound;
+using UnityEngine;
+
+
+namespace CombatExtended
+{
+	public class Bipod_Recoil_StatPart : StatPart
+	{
+		public override void TransformValue(StatRequest req, ref float val)
+		{
+			var varA = req.Thing.TryGetComp<bipodcomp>();
+			if (varA != null)
+			{
+				if (varA.IsSetUpRn == true)
+				{
+					val *= varA.Props.recoilmulton;
+				}
+				else
+				{
+					val *= varA.Props.recoilmultoff;
+				}
+			}
+		}
+
+		public override string ExplanationPart(StatRequest req)
+		{
+			var varA = req.Thing.TryGetComp<bipodcomp>();
+			if (varA != null)
+			{
+				if (varA.IsSetUpRn == true)
+				{
+					return "Bipod is set up - " + varA.Props.recoilmulton.ToString().Colorize(Color.blue);
+				}
+				else
+				{
+					return "Bipod is NOT set up - " + varA.Props.recoilmultoff.ToString().Colorize(Color.blue);
+				}
+			}
+			else
+			{
+				return "";
+			}
+		}
+	}
+}
