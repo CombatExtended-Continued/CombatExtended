8a03d0c060568fccdbac5e5d8390f0674a61c321
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-11-21 16:51:56 +0100

    Uploaded the jobdriver
    Uploaded the jobdriver

---
+++ Source/CombatExtended/CombatExtended/Jobs/JobDriver_SetUpBipod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CombatExtended;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.Sound;
+using UnityEngine;
+
+namespace CombatExtended
+{
+	public class JobDriver_SetUpBipod : JobDriver
+	{
+		private ThingWithComps weapon
+		{
+			get
+			{
+				return base.TargetThingA as ThingWithComps;
+			}
+		}
+
+		private const TargetIndex guntosetup = TargetIndex.A;
+		public override bool TryMakePreToilReservations(bool errorOnFailed)
+		{
+			return this.pawn.Reserve(this.job.GetTarget(guntosetup), this.job, 1, -1, null);
+		}
+		public bipodcomp Bipod
+		{
+			get
+			{
+				return this.weapon.TryGetComp<bipodcomp>();
+			}
+		}
+		public override IEnumerable<Toil> MakeNewToils()
+		{
+			yield return Toils_General.Wait(Bipod.Props.TicksToSetUp);
+			yield return Toils_General.Do(delegate { Bipod.IsSetUpRn = true; });
+
+
+		}
+	}
+}
