05fdfa3ed1450cc7a90d1424717412a96ba386d8
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-11-21 16:53:18 +0100

    Uploaded the bipodcomp props
    Uploaded the bipodcomp props

---
+++ Source/CombatExtended/CombatExtended/Comps/CompProperties_BipodComp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CombatExtended;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.Sound;
+using UnityEngine;
+
+namespace CombatExtended
+{
+	public class CompProperties_BipodComp : CompProperties
+	{
+
+
+		public int additionalrange = 12;
+
+		public float recoilmulton = 0.5f;
+
+		public float recoilmultoff = 1f;
+
+		public float warmupmult = 0.85f;
+
+		public float warmuppenalty = 2f;
+
+		public int TicksToSetUp = 60;
+
+
+
+		public CompProperties_BipodComp()
+		{
+			this.compClass = typeof(bipodcomp);
+		}
+
+		public CompProperties_BipodComp(Type compClass) : base(compClass)
+		{
+			this.compClass = compClass;
+		}
+
+	}
+}
