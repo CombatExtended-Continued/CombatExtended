1fc6eaab76ca462e6ae79a9d088051b3793fc684
DullSaurcer <kbatbouta66@gmail.com>
2021-11-25 23:05:25 -0800

    Update AnimatedPart.cs

--- Source/CombatExtended/CombatExtended/Defs/AnimatedPart.cs
+++ Source/CombatExtended/CombatExtended/Defs/AnimatedPart.cs
@@ -10,18 +10,22 @@ namespace CombatExtended
         /// Graphic data of the animated part.
         /// </summary>
         public GraphicData graphicData;
+
         /// <summary>
         /// Offset for the animated part.
         /// </summary>
         public Vector3 offset = Vector3.zero;
+
         /// <summary>
         /// Scale for the animated part.
         /// </summary>
         public Vector3 scale = new Vector3(1, 1, 1);
+
         /// <summary>
         /// Animation speed.
         /// </summary>
         public float speed = 1f;
+
         /// <summary>
         /// Class of the animator.
         /// </summary>
