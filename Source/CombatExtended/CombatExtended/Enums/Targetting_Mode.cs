7ee76c148855472d31d9510b7cccc50c3944a613
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-12-05 20:14:34 +0100

    Uploaded targetting enum
    Uploaded targetting enum

---
+++ Source/CombatExtended/CombatExtended/Enums/Targetting_Mode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using UnityEngine;
+
+namespace CombatExtended
+{
+    public enum Targetting_Mode
+    {
+        legs_lowertorso,
+        centermass,
+        uppertorso_head
+    }
+}
