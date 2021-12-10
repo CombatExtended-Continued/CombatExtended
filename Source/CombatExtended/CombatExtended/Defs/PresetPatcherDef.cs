e046e152ea21282dfab565be7c93ff8974e23a65
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-12-10 22:06:09 +0100

    uploaded patcherdef
    uploaded patcherdef

---
+++ Source/CombatExtended/CombatExtended/Defs/PresetPatcherDef.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace CombatExtended
+{
+    public class PresetPatcherDef : Def
+    {
+        public string ID = "none";
+
+        public float bulk;
+
+        public float mass;
+
+        public float Sight_Efficiency;
+
+        public float BaseRecoil;
+
+        public int FullAutoShotCount;
+
+        public int Range;
+
+        public float WarmupTime;
+
+        public int shotinterval;
+
+        public ThingDef DefaultProj;
+
+        public int MagSize;
+
+        public float ReloadTime;
+
+        public int BurstCount;
+
+        public float CooldownRanged;
+
+        public float SwayAm;
+
+        public float BaseSpread;
+
+
+        //Will be later changed to SimilarTo system
+        public AmmoSetDef ammoset;
+
+    }
+}
