2a70eefca502a1791a9a575b0adccfa10ffd6512
Logan Perkins <logan@git.lp-programming.com>
2021-12-19 12:02:28 -0800

    Fix whitespace.

--- Source/CombatExtended/Compatibility/Trenches.cs
+++ Source/CombatExtended/Compatibility/Trenches.cs
@@ -12,32 +12,34 @@ namespace CombatExtended.Compatibility
     [StaticConstructorOnStartup]
     public class CETrenches
     {
-	private static bool vfeInstalled;
-	private const string VFES_ModName = "Vanilla Furniture Expanded - Security";
-	static CETrenches()
-	{
-	    vfeInstalled = ModLister.HasActiveModWithName(VFES_ModName);
-	}
-	public static float GetHeightAdjust(IntVec3 cell, Map map)
-	{
-	    if (cell == null || map == null)
-	    {
-		return 0;
-	    }
+        private static bool vfeInstalled;
+        private const string VFES_ModName = "Vanilla Furniture Expanded - Security";
+        static CETrenches()
+        {
+            vfeInstalled = ModLister.HasActiveModWithName(VFES_ModName);
+        }
+        public static float GetHeightAdjust(IntVec3 cell, Map map)
+        {
+            if (cell == null || map == null)
+            {
+                return 0;
+            }
 	       
-	    if (vfeInstalled)
-	    {
-		
-		List<Thing> thingList = GridsUtility.GetThingList(cell, map);
-		foreach (Thing thing in thingList) 
-		{
+            if (vfeInstalled)
+            {
+                List<Thing> thingList = GridsUtility.GetThingList(cell, map);
+                foreach (Thing thing in thingList) 
+                {
 		    CompProperties_TerrainSetter compProperties = thing.def.GetCompProperties<CompProperties_TerrainSetter>();
-		    if (compProperties != null)
-		    {
-			TerrainDef terrainDef = compProperties.terrainDef;
-			TerrainDefExtension terrainDefExtension = TerrainDefExtension.Get(terrainDef);
-			return -terrainDefExtension.coverEffectiveness;
-		    }
+                    if (compProperties == null)
+                    {
+                        return;
+                    }
+		    
+                    TerrainDef terrainDef = compProperties.terrainDef;
+                    TerrainDefExtension terrainDefExtension = TerrainDefExtension.Get(terrainDef);
+                    return -terrainDefExtension.coverEffectiveness;
+		    
 		}
 	    }
 	    return 0f;
