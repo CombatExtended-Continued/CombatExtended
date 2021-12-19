8b445a70f51c60f4912f7e1921310d16c64c1c0a
Logan Perkins <logan@git.lp-programming.com>
2021-12-19 12:08:30 -0800

    Fix missing return value

--- Source/CombatExtended/Compatibility/Trenches.cs
+++ Source/CombatExtended/Compatibility/Trenches.cs
@@ -33,7 +33,7 @@ namespace CombatExtended.Compatibility
 		    CompProperties_TerrainSetter compProperties = thing.def.GetCompProperties<CompProperties_TerrainSetter>();
                     if (compProperties == null)
                     {
-                        return;
+                        return 0f;
                     }
 		    
                     TerrainDef terrainDef = compProperties.terrainDef;
