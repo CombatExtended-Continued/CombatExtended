ddd57bacb1a243af9192c6b35f8b699e3bd04834
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-01-10 22:18:17 +0100

    upload
    upload

---
+++ Source/CombatExtended/CombatExtended/Defs/ApparelPartialStat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using System.Xml;
+
+namespace CombatExtended
+{
+    public class ApparelPartialStat
+    {
+        public StatDef stat;
+
+        public float value;
+
+        public List<BodyPartDef> parts;
+
+        public void LoadDataFromXmlCustom(XmlNode xmlRoot)
+        {
+
+            DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(this, "stat", xmlRoot.FirstChild.Name, null, null);
+            this.value = ParseHelper.FromString<float>(xmlRoot.FirstChild.InnerText);
+            if (parts == null)
+            {
+                parts = new List<BodyPartDef>();
+            }
+            foreach (XmlNode node in xmlRoot.LastChild.ChildNodes)
+            {
+              
+
+                DirectXmlCrossRefLoader.RegisterListWantsCrossRef(parts, node.InnerText);
+            }
+        }
+    }
+}
