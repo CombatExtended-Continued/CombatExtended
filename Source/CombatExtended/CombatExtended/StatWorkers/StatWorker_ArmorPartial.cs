5aefba0c16b8bc9ac81ec3c6b8226afe8980095a
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-02-02 22:57:18 +0100

    upload fix
    upload fix

--- Source/CombatExtended/CombatExtended/StatWorkers/StatWorker_ArmorPartial.cs
+++ Source/CombatExtended/CombatExtended/StatWorkers/StatWorker_ArmorPartial.cs
@@ -13,32 +13,42 @@ namespace CombatExtended
     {
         public override string GetExplanationFinalizePart(StatRequest req, ToStringNumberSense numberSense, float finalVal)
         {
-            if (req.Thing.def.HasModExtension<PartialArmorExt>())
+            if (Controller.settings.PartialStat)
             {
-                var result = "CE_StatWorker_ArmorGeneral".Translate() + finalVal.ToString() + " \n \n" + "CE_StatWorker_ArmorSpecific".Translate();
-                var ext = req.Thing.def.GetModExtension<PartialArmorExt>();
-
-                foreach(ApparelPartialStat partstat in ext.stats)
+                if (req.Thing.def.HasModExtension<PartialArmorExt>())
                 {
-                    if (partstat.stat != this.stat)
+                    var result = "CE_StatWorker_ArmorGeneral".Translate() + finalVal.ToString() + " \n \n" + "CE_StatWorker_ArmorSpecific".Translate();
+                    var ext = req.Thing.def.GetModExtension<PartialArmorExt>();
+
+                    foreach (ApparelPartialStat partstat in ext.stats)
                     {
-                        return base.GetExplanationFinalizePart(req, numberSense, finalVal);
-                    }
+                        if (partstat.stat != this.stat)
+                        {
+                            return base.GetExplanationFinalizePart(req, numberSense, finalVal);
+                        }
 
-                    var value = CE_Utility.PartialStat((Apparel)req.Thing, this.stat, partstat.parts.First());
+                        var value = 0f;
+                        if (req.Thing is Apparel)
+                        {
+                            value = CE_Utility.PartialStat((Apparel)req.Thing, this.stat, partstat.parts.First());
+                        }
+                        else if (req.Thing is Pawn pawn)
+                        {
+                            value = pawn.PartialStat(partstat.stat, partstat.parts.First());
+                        }
 
-                    result += "\n";
-                    result +=  this.stat.formatString.Replace("{0}", value.ToString()) + " for: ";
-
-                    foreach (BodyPartDef bodypart in partstat.parts)
-                    {
                         result += "\n";
-                        result += bodypart.label;
-                    }
-                }
-                return result;
-            }
+                        result += this.stat.formatString.Replace("{0}", value.ToString()) + " for: ";
 
+                        foreach (BodyPartDef bodypart in partstat.parts)
+                        {
+                            result += "\n";
+                            result += bodypart.label;
+                        }
+                    }
+                    return result;
+                }
+            }
             return base.GetExplanationFinalizePart(req, numberSense, finalVal);
         }
     }
