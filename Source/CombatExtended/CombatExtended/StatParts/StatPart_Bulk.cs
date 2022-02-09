64b01346cc06d9847decb653182798e995fa89df
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-02-09 12:04:55 +0100

    added hitchance statpart
    added hitchance statpart

---
+++ Source/CombatExtended/CombatExtended/StatParts/StatPart_Bulk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace CombatExtended
+{
+    public class StatPart_Bulk : StatPart
+    {
+        public bool ValidReq(StatRequest req)
+        {
+            return req.HasThing && inv(req) != null;
+        }
+
+        public CompInventory inv(StatRequest req)
+        {
+            return req.Thing.TryGetComp<CompInventory>();
+        }
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (ValidReq(req))
+            {
+                return "CE_BulkEffect".Translate() + " x" + MassBulkUtility.HitChanceBulkFactor(inv(req).currentBulk, inv(req).capacityBulk);
+            }
+            return null;
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (ValidReq(req))
+            {
+                val *= MassBulkUtility.HitChanceBulkFactor(inv(req).currentBulk, inv(req).capacityBulk);
+            }
+        }
+    }
+}
