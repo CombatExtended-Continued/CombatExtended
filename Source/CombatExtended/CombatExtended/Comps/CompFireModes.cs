c114f922bea764497d0a9049a51f5632e52f0f6a
SamGray <56392968+ItsSamGray@users.noreply.github.com>
2022-02-09 19:48:38 +0330

    Update CompFireModes.cs

--- Source/CombatExtended/CombatExtended/Comps/CompFireModes.cs
+++ Source/CombatExtended/CombatExtended/Comps/CompFireModes.cs
@@ -298,7 +298,7 @@ namespace CombatExtended
 
             if (CurrentAimMode != AimMode.SuppressFire)
             {
-                if ( (HandLing > 2.5f) | IsTurretMannable)
+                if ( (HandLing > 2.45f) | IsTurretMannable)
                 {
                     yield return new Command_Action
                     {
