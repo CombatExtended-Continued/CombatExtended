7aa6b046964470b859ce358fb05302dbd2f45756
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-03-09 16:27:25 +0100

    added extension for skill ranges
    added extension for skill ranges

---
+++ Source/CombatExtended/CombatExtended/Defs/SkillKindDefExt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CombatExtended
+{
+    public class SkillKindDefExt : DefModExtension
+    {
+        public List<SkillRange> skills;
+    }
+}
