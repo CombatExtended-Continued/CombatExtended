2399153d8c466465e4f1ffb537d56fd47d398c3f
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-03-12 13:39:33 +0100

    added jobdriver for giving ammo
    added jobdriver for giving ammo

---
+++ Source/CombatExtended/CombatExtended/Jobs/JobDriver_GiveAmmo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.AI;
+using Verse;
+
+namespace CombatExtended
+{
+    public class JobDriver_GiveAmmo : JobDriver
+    {
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return GetActor().CanReserveAndReach(TargetA, PathEndMode.ClosestTouch, Danger.Deadly);
+        }
+        public override IEnumerable<Toil> MakeNewToils()
+        {
+            yield return Toils_Goto.Goto(TargetIndex.A, PathEndMode.ClosestTouch);
+            yield return Toils_General.Do(
+                delegate
+                {
+                    var targetPawn = (Pawn)TargetA.Thing;
+
+                    var newThing = ThingMaker.MakeThing(TargetB.Thing.def);
+
+                    newThing.HitPoints = TargetB.Thing.HitPoints;
+
+                    newThing.stackCount = TargetB.Thing.stackCount;
+
+                    TargetB.Thing.Destroy();
+
+                    targetPawn.inventory.TryAddItemNotForSale(newThing);
+                });
+        }
+    }
+}
