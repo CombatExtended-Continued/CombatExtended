1e21301c206a1ea03b3c95faa47b79505801240f
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-03-13 10:12:52 +0100

    bugfix
    bugfix

--- Source/CombatExtended/CombatExtended/Comps/AmmoGiverComp.cs
+++ Source/CombatExtended/CombatExtended/Comps/AmmoGiverComp.cs
@@ -16,44 +16,44 @@ namespace CombatExtended
 
         public override IEnumerable<FloatMenuOption> CompFloatMenuOptions(Pawn selPawn)
         {
-            if (!selPawn.Faction.HostileTo(dad.Faction))
+            if (user != null && selPawn != dad)
             {
-                if (selPawn.CanReach(dad, PathEndMode.ClosestTouch, Danger.Deadly)
-                    &&
-                    !selPawn.Downed
-                    &&
-                    selPawn.inventory.innerContainer.Where(x => x is AmmoThing).Any(x => ((AmmoDef)x.def).AmmoSetDefs.Contains(user.Props.ammoSet))
-                    )
+                if (!selPawn.Faction.HostileTo(dad.Faction))
                 {
-                    if (user != null)
+                    if (selPawn.CanReach(dad, PathEndMode.ClosestTouch, Danger.Deadly)
+                        &&
+                        !selPawn.Downed
+                        &&
+                        selPawn.inventory.innerContainer.Where(x => x is AmmoThing).Any(x => ((AmmoDef)x.def).AmmoSetDefs.Contains(user.Props.ammoSet))
+                        )
                     {
                         yield return new FloatMenuOption("CE_GiveAmmoToThing".Translate() + (dad.Name?.ToStringShort ?? dad.def.label),
-                        delegate
-                        {
-                            List<FloatMenuOption> options = new List<FloatMenuOption>();
+                          delegate
+                          {
+                              List<FloatMenuOption> options = new List<FloatMenuOption>();
 
-                            foreach (AmmoThing ammo in selPawn.TryGetComp<CompInventory>().ammoList)
-                            {
-                                if (ammo.AmmoDef.AmmoSetDefs.Contains(user.Props.ammoSet))
-                                {
-                                    options.Add(new FloatMenuOption("CE_Give".Translate() + " " + ammo.Label, delegate
-                                    {
-                                        var jobdef = CE_JobDefOf.GiveAmmo;
+                              foreach (AmmoThing ammo in selPawn.TryGetComp<CompInventory>().ammoList)
+                              {
+                                  if (ammo.AmmoDef.AmmoSetDefs.Contains(user.Props.ammoSet))
+                                  {
+                                      options.Add(new FloatMenuOption("CE_Give".Translate() + " " + ammo.Label, delegate
+                                      {
+                                          var jobdef = CE_JobDefOf.GiveAmmo;
 
-                                        var job = new Job { def = jobdef, targetA = dad, targetB = ammo };
+                                          var job = new Job { def = jobdef, targetA = dad, targetB = ammo };
 
-                                        selPawn.jobs.StartJob(job, JobCondition.InterruptForced);
-                                    }));
-                                }
-                            }
+                                          selPawn.jobs.StartJob(job, JobCondition.InterruptForced);
+                                      }));
+                                  }
+                              }
 
-                            if (!options.Any())
-                            {
-                                options.Add(new FloatMenuOption("CE_NoAmmoToGive", null));
-                            }
+                              if (!options.Any())
+                              {
+                                  options.Add(new FloatMenuOption("CE_NoAmmoToGive", null));
+                              }
 
-                            Find.WindowStack.Add(new FloatMenu(options));
-                        });
+                              Find.WindowStack.Add(new FloatMenu(options));
+                          });
                     }
                 }
             }
