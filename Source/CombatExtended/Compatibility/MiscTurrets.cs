f355d1693ab3181c81217d1eabf2323733e582ee
Logan Perkins <logan@github.lp-programming.com>
2022-04-16 10:02:00 -0700

    Add misc turrets checker

---
+++ Source/CombatExtended/Compatibility/MiscTurrets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using Verse;
+using CombatExtended.Loader;
+using RimWorld;
+
+namespace CombatExtended.Compatibility
+{
+    public class MiscTurrets
+    {
+	public static bool CanInstall() {
+	    Log.Error("Combat Extended :: Checking Misc Turrets");
+	    return ModLister.HasActiveModWithName("Misc. TurretBase, Objects");
+	}
+
+	public static void Install() {
+	    Log.Error("Combat Extended :: Installing Misc Turrets");
+	}
+
+
+
+    }
+}
