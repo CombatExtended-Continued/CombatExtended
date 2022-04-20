786208c60605f8353baa50faf13a9f25d05a36e3
Logan Perkins <logan@github.lp-programming.com>
2022-04-20 03:40:26 -0700

    Add BuildCompat.py to automate building compat libraries from .sln

---
+++ BuildCompat.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import os
+from twisted.python.filepath import FilePath
+from subprocess import Popen
+import re
+
+PROJECT_PATTERN = re.compile(r'''Project.".[0-9A-Za-z]{8}-[0-9A-Za-z]{4}-[0-9A-Za-z]{4}-[0-9A-Za-z]{4}-[0-9A-Za-z]{12}.". = '''
+                             '''"([0-9a-zA-Z]+Compat)", '''
+                             '''"([0-9A-zA-Z\/]+.csproj)", '''
+                             '''".[0-9A-Za-z]{8}-[0-9A-Za-z]{4}-[0-9A-Za-z]{4}-[0-9A-Za-z]{4}-[0-9A-Za-z]{12}."''')
+
+
+def system(*cmd):
+    sp = Popen(cmd)
+    sp.wait()
+    ec = sp.poll()
+    if ec:
+        raise SystemExit(ec)
+
+with open("Source/CombatExtended.sln") as f:
+    for line in f.readlines():
+        line = line.strip()
+        match = PROJECT_PATTERN.match(line)
+        if match:
+            name, csproj = match.groups()
+            csproj = csproj.replace('\\', '/').split('/')
+            csproj = FilePath("Source").descendant(csproj)
+            print(f"Building {name}")
+            output = FilePath("AssembliesCompat").child(name+".dll")
+            system("python3", "Make.py", "--csproj", csproj.path, "--output", output.path, "--download-libs", "--all-libs", "--publicizer", "./AssemblyPublicizer", "--", "-r:Assemblies/CombatExtended.dll")
+
