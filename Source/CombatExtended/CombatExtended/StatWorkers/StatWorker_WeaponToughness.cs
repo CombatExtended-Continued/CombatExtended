864fd7e8eecd0f0d384214b6834fb72e0216acd5
AluminumAlman <Almantas1122334455@gmail.com>
2022-04-20 22:23:21 +0300

    Split StatWorker_WeaponDurability into StatWorker_WeaponToughness, WeaponToughnessAutoPatcher and CE_StatDefOf, added deflection and partial penetration handling to ArmorUtilityCE; updated Keyed/Stats

---
+++ Source/CombatExtended/CombatExtended/StatWorkers/StatWorker_WeaponToughness.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace CombatExtended
+{
+    public class StatWorker_WeaponToughness : StatWorker
+    {
+        public const float parryChanceFactor = 3f; //Factor by which the weapon holder's skill affects the final weapon toughness. 
+        public override void FinalizeValue(StatRequest req, ref float val, bool applyPostProcess)
+        {
+            var thing = req.Thing;
+            if (thing != null)
+            {
+                //Since the material factor acts like a StatPart, if the holder's skill was taken into account first, it wouldn't affect stuffable weapons
+                if (stat.parts != null)
+                {
+                    foreach (StatPart part in this.stat.parts)
+                    {
+                        part.TransformValue(req, ref val);
+                    }
+                }
+
+                //Factors in the holder's skill
+                var compEq = thing.TryGetComp<CompEquippable>();
+                var holder = compEq?.Holder;
+                if (holder?.equipment?.Primary == thing)
+                {
+                    val *= holder.GetStatValue(CE_StatDefOf.MeleeParryChance) * parryChanceFactor;
+                }
+            }
+        }
+
+        public override string GetExplanationFinalizePart(StatRequest req, ToStringNumberSense numberSense, float finalVal)
+        {
+            string result = "";
+
+            if (this.stat.parts != null)
+            {
+                foreach (var part in this.stat.parts)
+                {
+                    if (!part.ExplanationPart(req).NullOrEmpty())
+                    {
+                        result += "\n" + part.ExplanationPart(req);
+                    }
+                }
+            }
+
+            if (req.Thing != null)
+            {
+                if (req.Thing?.TryGetComp<CompEquippable>()?.Holder != null)
+                {
+                    result += "\n" + "CE_Holder_WeaponToughnessEffect".Translate() + ((req.Thing?.TryGetComp<CompEquippable>()?.Holder.GetStatValue(CE_StatDefOf.MeleeParryChance) ?? 1f) * parryChanceFactor).ToStringPercent();
+                }
+            }
+
+            result += "\n" + "StatsReport_FinalValue".Translate() + ": " + stat.ValueToString(finalVal, stat.toStringNumberSense);
+
+            return result;
+        }
+
+        public override bool ShouldShowFor(StatRequest req)
+        {
+            return req.HasThing && req.Thing.def.IsWeapon && base.ShouldShowFor(req);
+        }
+    }
+}
---
+++ Source/CombatExtended/Compatibility/WeaponToughnessAutoPatcher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace CombatExtended.Compatibility
+{
+    [StaticConstructorOnStartup]
+    class WeaponToughnessAutoPatcher
+    {
+        static WeaponToughnessAutoPatcher()
+        {
+            foreach (var def in DefDatabase<ThingDef>.AllDefs.Where(x => x.IsWeapon && !x.IsApparel))
+            {
+                if (!def.statBases.Any(x => x.stat == CE_StatDefOf.StuffEffectMultiplierToughness || x.stat == CE_StatDefOf.ToughnessRating))
+                {
+                    //Approximate weapon thickness via the bulk of the weapon. Longswords get 2mm, knives get 1mm, spears get about 2.2mm
+                    float weaponThickness = Mathf.Pow(def.statBases?.Find(statMod => statMod.stat.defName == CE_StatDefOf.Bulk.defName)?.value ?? 0f, 1f / 3f);
+
+                    //Tech level improves weapon stats
+                    switch (def.techLevel)
+                    {
+                        //Plasteel
+                        case (TechLevel.Spacer):
+                            weaponThickness *= 2f;
+                            break;
+                        case (TechLevel.Ultra):
+                            weaponThickness *= 4f;
+                            break;
+                        case (TechLevel.Archotech):
+                            weaponThickness *= 8f;
+                            break;
+                    }
+
+                    //Blunt weapons get double thickness because edges are easier to damage
+                    if (!def.tools?.Any(tool => tool.capacities?.Any(capacityDef => DefDatabase<DamageDef>.defsList.Any(damageDef => damageDef.armorCategory == DamageArmorCategoryDefOf.Sharp && capacityDef.defName == damageDef.defName)) ?? false) ?? false)
+                        weaponThickness *= 2f;
+
+                    //Stuffable weapons get the multiplier stat
+                    if (def.MadeFromStuff)
+                        def.statBases.Add(new StatModifier { stat = CE_StatDefOf.StuffEffectMultiplierToughness, value = weaponThickness });
+                    //Non-stuffable weapons get the rating value
+                    else
+                    {
+                        //C# magic
+                        var largestIngredientCount = DefDatabase<RecipeDef>.AllDefs.ToList().Find(recipeDef => (bool)recipeDef.products?.Any(product => product.thingDef.defName == def.defName))?.ingredients?.MaxBy(ingredientCount => ingredientCount.count);
+                        float largestIngredientSharpArmor = largestIngredientCount?.filter?.thingDefs?.MaxBy(thingDef => thingDef.statBases?.Find(statMod => statMod.stat.defName == StatDefOf.ArmorRating_Sharp.GetStatPart<StatPart_Stuff>().stuffPowerStat.defName)?.value)?.statBases?.Find(statMod => statMod.stat.defName == StatDefOf.ArmorRating_Sharp.GetStatPart<StatPart_Stuff>().stuffPowerStat.defName)?.value ?? 1f;
+
+                        //Compensating for plasteel
+                        if (def.techLevel > TechLevel.Industrial)
+                            largestIngredientSharpArmor = Math.Max(1f, largestIngredientSharpArmor / 2f);
+
+                        def.statBases.Add(new StatModifier { stat = CE_StatDefOf.ToughnessRating, value = weaponThickness * largestIngredientSharpArmor });
+                    }
+                }
+            }
+        }
+    }
+}
