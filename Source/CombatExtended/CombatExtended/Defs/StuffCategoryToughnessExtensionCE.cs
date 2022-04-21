f68acdc42cfce44c50b573d9eeb3419e845f487f
AluminumAlman <Almantas1122334455@gmail.com>
2022-04-21 13:58:24 +0300

    Add requested comments

--- Source/CombatExtended/CombatExtended/Defs/StuffCategoryToughnessExtensionCE.cs
+++ Source/CombatExtended/CombatExtended/Defs/StuffCategoryToughnessExtensionCE.cs
@@ -2,8 +2,12 @@
 
 namespace CombatExtended
 {
+    /// <summary>
+    /// Used in WeaponToughnessAutoPatcher for non-stuffable weapons and in StatWorker_WeaponToughness for stuffable weapons to affect the final toughness value.
+    /// Assigned to ThingDefs as a mod extension. Acts as a value modifier.
+    /// </summary>
     class StuffToughnessMultiplierExtensionCE : DefModExtension
     {
-        public float toughnessMultiplier = 1f;
+        public float toughnessMultiplier = 1f; // Value to multiply the original toughness value
     }
 }
