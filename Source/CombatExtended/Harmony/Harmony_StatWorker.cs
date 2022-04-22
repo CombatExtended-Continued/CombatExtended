eaac56bda2e71b11a23a69e875703295af0ca85f
Logan Perkins <logan@github.lp-programming.com>
2022-04-22 05:00:12 -0700

    Display armor range on ghost armors (research / recipe screens).

--- Source/CombatExtended/Harmony/Harmony_StatWorker.cs
+++ Source/CombatExtended/Harmony/Harmony_StatWorker.cs
@@ -26,23 +26,60 @@ namespace CombatExtended.HarmonyCE
     {
         internal static bool Prefix(ref string __result, StatWorker __instance, StatDef stat, float value, ToStringNumberSense numberSense, StatRequest optionalReq, bool finalized = true)
         {
-	    if(optionalReq != null && optionalReq.Thing is Apparel apparel) {
-		if (__instance.stat == global::RimWorld.StatDefOf.ArmorRating_Blunt ||
+	    if (__instance.stat == global::RimWorld.StatDefOf.ArmorRating_Blunt ||
 		    __instance.stat == global::RimWorld.StatDefOf.ArmorRating_Sharp) {
-		    float minArmor = value;
-		    float maxArmor = value;
-		    if (apparel.def.HasModExtension<PartialArmorExt>()) {
-			foreach (ApparelPartialStat p in apparel.def.GetModExtension<PartialArmorExt>().stats) {
-			    float thisArmor = p.mult * value;
+		if(optionalReq != null) {
+		    if (optionalReq.Thing is Apparel apparel) {
+			float minArmor = value;
+			float maxArmor = value;
+			if (apparel.def.HasModExtension<PartialArmorExt>()) {
+			    foreach (ApparelPartialStat p in apparel.def.GetModExtension<PartialArmorExt>().stats) {
+				
+				float thisArmor = value;
+				if (p.useStatic) {
+				    value = p.staticValue;
+				}
+				else {
+				    value *= p.mult;
+				}
+				if (thisArmor < minArmor) {
+				    minArmor = thisArmor;
+				}
+				else if (thisArmor > maxArmor) {
+				    maxArmor = thisArmor;
+				}
+			    }
+			    __result = string.Format(stat.formatString, $"{minArmor} ~ {maxArmor}");
+			    return false;
+			}
+		    }
+		    else if (optionalReq.Def!=null && optionalReq.Def.HasModExtension<PartialArmorExt>()) {
+			float minArmor = value;
+			float maxArmor = value;
+			var ext = optionalReq.Def.GetModExtension<PartialArmorExt>();
+			foreach (ApparelPartialStat partstat in ext.stats)
+			{
+			    float thisArmor = value;
+			    if (partstat.stat == stat)
+			    {
+				if (!partstat.useStatic)
+				{
+				    thisArmor *= partstat.mult;
+				}
+				else
+				{
+				    thisArmor = partstat.staticValue;
+				}
+			    }
 			    if (thisArmor < minArmor) {
 				minArmor = thisArmor;
 			    }
 			    else if (thisArmor > maxArmor) {
 				maxArmor = thisArmor;
 			    }
+			    __result = string.Format(stat.formatString, $"{minArmor} ~ {maxArmor}");
+			    return false;
 			}
-			__result = string.Format(stat.formatString, $"{minArmor} ~ {maxArmor}");
-			return false;
 		    }
 		}
 	    }
