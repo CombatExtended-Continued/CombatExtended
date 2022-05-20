616c0c84dc7f331e3d2557a6f5b9af73ceeb6984
Logan Perkins <logan@github.lp-programming.com>
2022-05-20 14:08:15 -0700

    Mark Compatibility.Multiplayer class as an IPatch so compatibility assembly loads

--- Source/CombatExtended/Compatibility/Multiplayer.cs
+++ Source/CombatExtended/Compatibility/Multiplayer.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace CombatExtended.Compatibility
 {
-    public class Multiplayer
+    public class Multiplayer : IPatch
     {
         private static bool isMultiplayerActive = false;
 
