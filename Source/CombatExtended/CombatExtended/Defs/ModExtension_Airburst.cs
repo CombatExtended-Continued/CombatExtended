3e8042ba02eff93b49bb7e8e011643ed343f49e0
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-08-01 17:48:55 +0200

    Add files via upload

---
+++ Source/CombatExtended/CombatExtended/Defs/ModExtension_Airburst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CombatExtended
+{
+    public class ModExtension_Airburst : DefModExtension
+    {
+        public float distToExplode;
+
+        public int updateRate;
+    }
+}
