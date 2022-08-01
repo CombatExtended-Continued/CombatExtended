927f4d56f1819c1215e8d64daa5d380d326e33ce
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-08-01 17:48:13 +0200

    Add files via upload

---
+++ Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE_Airburst.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace CombatExtended
+{
+    public class ProjectileCE_Airburst : ProjectileCE
+    {
+        public int updateRate => props.updateRate;
+
+        public int timer;
+
+        public ModExtension_Airburst props => this.def.GetModExtension<ModExtension_Airburst>();
+        public override void Tick()
+        {
+            if (timer >= updateRate)
+            {
+                UpdateDistance();
+                timer = 0;
+            }
+            timer++;
+            base.Tick();
+        }
+
+        public virtual void UpdateDistance()
+        {
+            if (this.Position.DistanceTo(this.intendedTarget.Cell) <= props.distToExplode)
+            {
+                this.Impact(null);
+            }
+        }
+    }
+}
