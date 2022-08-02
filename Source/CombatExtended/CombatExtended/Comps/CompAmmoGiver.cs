09894886ce244d9df3539db83ecf4835ea634d68
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-08-02 11:18:53 +0200

    fixed cap

--- Source/CombatExtended/CombatExtended/Comps/CompAmmoGiver.cs
+++ Source/CombatExtended/CombatExtended/Comps/CompAmmoGiver.cs
@@ -38,7 +38,7 @@ namespace CombatExtended
                               {
                                   if (ammo.AmmoDef.AmmoSetDefs.Contains(user.Props.ammoSet))
                                   {
-                                      options.Add(new FloatMenuOption("CE_Give".Translate() + " " + ammo.def.Label, delegate
+                                      options.Add(new FloatMenuOption("CE_Give".Translate() + " " + ammo.def.label, delegate
                                       {
                                           Find.WindowStack.Add(new Window_GiveAmmoAmountSlider() { dad = dad, sourceAmmo = ammo, selPawn = selPawn, sourceComp = this });
                                       }));
