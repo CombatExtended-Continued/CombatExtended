7d42d1fedba3e02fec1b2fca2b92ad913aee87df
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-08-06 19:01:08 +0200

    Update ProjectileCE_Airburst.cs

--- Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE_Airburst.cs
+++ Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE_Airburst.cs
@@ -13,24 +13,33 @@ namespace CombatExtended
         public int updateRate => props.updateRate;
 
         public int timer;
+        
+        public bool detonated;
 
         public ModExtension_Airburst props => this.def.GetModExtension<ModExtension_Airburst>();
         public override void Tick()
         {
             if (timer >= updateRate)
             {
-                UpdateDistance();
-                timer = 0;
+                 if (!detonated)
+                {
+                    if (timer >= updateRate)
+                    {
+                        UpdateDistance();
+                        timer = 0;
+                    }
+                    timer++;
+                    base.Tick();
+                }
             }
-            timer++;
-            base.Tick();
         }
 
         public virtual void UpdateDistance()
         {
-            if (this.Position.DistanceTo(this.intendedTarget.Cell) <= props.distToExplode)
+            if (detonated || this.Position.DistanceTo(this.intendedTarget.Cell)  <= props.distToExplode)
             {
-                this.Impact(null);
+                detonated = true;
+                base.ImpactSomething();
             }
         }
     }
