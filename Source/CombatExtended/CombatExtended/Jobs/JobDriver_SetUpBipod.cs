179c416b0aec13c8b0d2aef4ef7523e04e717e7c
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-08-30 00:14:23 +0200

    fixed bipods not setting up

--- Source/CombatExtended/CombatExtended/Jobs/JobDriver_SetUpBipod.cs
+++ Source/CombatExtended/CombatExtended/Jobs/JobDriver_SetUpBipod.cs
@@ -38,10 +38,12 @@ namespace CombatExtended
 		{
 			var Pawn = GetActor();
 
-			int timeToSetUpTrue = (int)(Bipod.Props.ticksToSetUp / (Pawn.GetStatValue(StatDefOf.ShootingAccuracyPawn) * 0.5f));
+			int timeToSetUpTrue = Mathf.Clamp((int)(Bipod.Props.ticksToSetUp / ( Pawn.GetStatValue(StatDefOf.ShootingAccuracyPawn) * 0.5f)), 1, Bipod.Props.ticksToSetUp * 3);
 
 			var cells = Pawn.CellsAdjacent8WayAndInside();
 
+			//Commented out as it wasn't used before, but also contains a fix for it at line 57 in case it is accepted. Log.Message was 
+			/*
 			bool hasCover = false;
 
 			foreach (IntVec3 vec in cells)
@@ -52,10 +54,11 @@ namespace CombatExtended
 
 					if (hasCover)
 					{
+						timeToSetUpTrue = Mathf.Max(timeToSetUpTrue - 60, 1);
 						Log.Message(vec.GetThingList(Pawn.Map).Find(x => x.def.fillPercent >= 0.2f && x.def.fillPercent < 1f).Label);
 					}
 				}
-			}
+			}*/
 
 			yield return Toils_General.Wait(timeToSetUpTrue).WithProgressBarToilDelay(TargetIndex.A);
 			yield return Toils_General.Do(delegate { Bipod.SetUpEnd(weapon); });
