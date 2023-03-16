00ef4b371d0bfc0bb98c217df0c11c001b8516f4
trist <trist@LAPTOP-CP1EBFT0>
2023-03-16 12:13:29 +0100

    #2396

--- Source/CombatExtended/Harmony/Harmony_AvoidGrid.cs
+++ Source/CombatExtended/Harmony/Harmony_AvoidGrid.cs
@@ -2,23 +2,49 @@
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using HarmonyLib;
+using RimWorld;
+using Verse;
 using Verse.AI;
 
 namespace CombatExtended.HarmonyCE
 {
-    [HarmonyPatch(typeof(AvoidGrid), "PrintAvoidGridAroundTurret")]
-    internal static class Harmony_AvoidGrid_PrintAvoidGridAroundTurret
+    [HarmonyPatch(typeof(AvoidGrid), "Regenerate")]
+    static class Harmony_AvoidGrid_Regenerate
     {
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        static bool Prefix(AvoidGrid __instance)
         {
-            foreach (var code in instructions)
+            __instance.gridDirty = false;
+            __instance.grid.Clear(0);
+            List<Building> allBuildingsColonist = __instance.map.listerBuildings.allBuildingsColonist;
+            for (int i = 0; i < allBuildingsColonist.Count; i++)
             {
-                if (code.opcode == OpCodes.Ldc_I4_S && (sbyte)code.operand == 45)
+                if (allBuildingsColonist[i].def.building.ai_combatDangerous)
                 {
-                    code.operand = 8;
+                    var building_TurretGun = allBuildingsColonist[i] as Building_TurretGunCE;
+                    if (building_TurretGun != null)
+                    {
+                        PrintAvoidGridAroundTurret(__instance, building_TurretGun);
+                    }
                 }
-                yield return code;
+            }
+            __instance.ExpandAvoidGridIntoEdifices();
+            return false;
+        }
+
+        static void PrintAvoidGridAroundTurret(Verse.AI.AvoidGrid __instance, Building_TurretGunCE tur)
+        {
+            float range = tur.GunCompEq.PrimaryVerb.verbProps.range;
+            float num = tur.GunCompEq.PrimaryVerb.verbProps.EffectiveMinRange(true);
+            int num2 = GenRadial.NumCellsInRadius(range + 4f);
+            for (int i = num < 1f ? 0 : GenRadial.NumCellsInRadius(num); i < num2; i++)
+            {
+                IntVec3 intVec = tur.Position + GenRadial.RadialPattern[i];
+                if (intVec.InBounds(tur.Map) && intVec.WalkableByNormal(tur.Map) && GenSight.LineOfSight(intVec, tur.Position, tur.Map, true, null, 0, 0))
+                {
+                    __instance.IncrementAvoidGrid(intVec, 45);
+                }
             }
         }
     }
+
 }
