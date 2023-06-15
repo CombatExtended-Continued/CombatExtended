bc77e01b089d55b63cbca74256b4f7cd46f7848c
MaxDorob <maxdor81@gmail.com>
2023-06-16 02:40:04 +0600

    Whitespaces fix

--- Source/CombatExtended/CombatExtended/WorldObjects/HealthComp.cs
+++ Source/CombatExtended/CombatExtended/WorldObjects/HealthComp.cs
@@ -194,7 +194,7 @@ namespace CombatExtended.WorldObjects
 
         public virtual void Notify_PreDestroyed(Faction attackingFaction, GlobalTargetInfo sourceInfo)
         {
-            foreach (var turret in Find.Maps.SelectMany(x => x.GetComponent<TurretTracker>().Turrets).Where(x=>x.Faction == attackingFaction && x is Building_TurretGunCE).Cast<Building_TurretGunCE>().Where(x=>(x.globalTargetInfo.WorldObject?.tileInt ?? -2) == parent.tileInt))
+            foreach (var turret in Find.Maps.SelectMany(x => x.GetComponent<TurretTracker>().Turrets).Where(x => x.Faction == attackingFaction && x is Building_TurretGunCE).Cast<Building_TurretGunCE>().Where(x => (x.globalTargetInfo.WorldObject?.tileInt ?? -2) == parent.tileInt))
             {
                 turret.ResetForcedTarget();
                 turret.ResetForcedTarget();
