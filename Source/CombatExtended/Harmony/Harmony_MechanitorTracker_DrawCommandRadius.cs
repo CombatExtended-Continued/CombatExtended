c16660e074753c6d44796648093985c2e3f16be6
Safairette <71556532+Safairette@users.noreply.github.com>
2023-06-17 15:21:41 +0300

    Mechanitor range patches

---
+++ Source/CombatExtended/Harmony/Harmony_MechanitorTracker_DrawCommandRadius.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace CombatExtended.HarmonyCE
+{
+    [HarmonyPatch(typeof(Pawn_MechanitorTracker), nameof(Pawn_MechanitorTracker.DrawCommandRadius))]
+    public static class Harmony_MechanitorTracker_DrawCommandRadius
+    {
+        public static bool Prefix(Pawn_MechanitorTracker __instance, Pawn ___pawn)
+        {
+            if (___pawn != null && ___pawn.Spawned && __instance.AnySelectedDraftedMechs)
+            {
+                GenDraw.DrawRadiusRing(___pawn.Position, 43.9f, Color.white, (IntVec3 c) => ___pawn.mechanitor.CanCommandTo(c));
+            }
+            return false;
+        }
+    }
+}
