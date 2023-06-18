a0f9a9977cd5694fd0f855b7728ea7145b1bd55f
Safairette <71556532+Safairette@users.noreply.github.com>
2023-06-18 17:49:35 +0300

    Comment out the predicate and range back to 43.9

--- Source/CombatExtended/Harmony/Harmony_MechanitorTracker_DrawCommandRadius.cs
+++ Source/CombatExtended/Harmony/Harmony_MechanitorTracker_DrawCommandRadius.cs
@@ -12,7 +12,7 @@ namespace CombatExtended.HarmonyCE
         {
             if (___pawn != null && ___pawn.Spawned && __instance.AnySelectedDraftedMechs)
             {
-                GenDraw.DrawRadiusRing(___pawn.Position, 43.9f, Color.white, (IntVec3 c) => ___pawn.mechanitor.CanCommandTo(c));
+                GenDraw.DrawRadiusRing(___pawn.Position, 43.9f, Color.white, /*(IntVec3 c) => ___pawn.mechanitor.CanCommandTo(c)*/null); //radii ending in .9 match up with the radial pattern, predicates aren't needed
             }
             return false;
         }
