3dc4f8420d909a0ca0d5ae091abe719ee15f93a8
Safairette <71556532+Safairette@users.noreply.github.com>
2023-07-02 23:31:53 +0300

    Bombardment damage/pen transpiler

---
+++ Source/CombatExtended/Harmony/Harmony_Bombardment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace CombatExtended.HarmonyCE
+{
+    [HarmonyPatch(typeof(Bombardment), nameof(Bombardment.TryDoExplosion))]
+    public static class Bombardment_TryDoExplosion_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler (IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            foreach (var instruction in instructions)
+            {  // Replaces the default damage and armor pen values of -1
+                if (instruction.opcode == OpCodes.Ldc_I4_M1)
+                {
+                    yield return new CodeInstruction(OpCodes.Ldc_I4, 546); // new damage
+                }
+                else if (instruction.opcode == OpCodes.Ldc_R4 &&  (float)instruction.operand == -1f)
+                {
+                    yield return new CodeInstruction(OpCodes.Ldc_R4, 180f); // new pen
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}
