1747bbec7a0ce1b74cebfd080dbe5122a936be1b
Safairette <71556532+Safairette@users.noreply.github.com>
2023-07-02 23:40:40 +0300

    Whitespace fix for style check

--- Source/CombatExtended/Harmony/Harmony_Bombardment.cs
+++ Source/CombatExtended/Harmony/Harmony_Bombardment.cs
@@ -13,7 +13,7 @@ namespace CombatExtended.HarmonyCE
     [HarmonyPatch(typeof(Bombardment), nameof(Bombardment.TryDoExplosion))]
     public static class Bombardment_TryDoExplosion_Patch
     {
-        static IEnumerable<CodeInstruction> Transpiler (IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
             foreach (var instruction in instructions)
             {  // Replaces the default damage and armor pen values of -1
@@ -21,7 +21,7 @@ namespace CombatExtended.HarmonyCE
                 {
                     yield return new CodeInstruction(OpCodes.Ldc_I4, 546); // new damage
                 }
-                else if (instruction.opcode == OpCodes.Ldc_R4 &&  (float)instruction.operand == -1f)
+                else if (instruction.opcode == OpCodes.Ldc_R4 && (float)instruction.operand == -1f)
                 {
                     yield return new CodeInstruction(OpCodes.Ldc_R4, 180f); // new pen
                 }
