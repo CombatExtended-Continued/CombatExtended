bc8844d02cf42edb9f5a930af4f81c3f59b42535
Thorbane <Thorbane@users.noreply.github.com>
2023-07-31 01:42:05 -0700

    remove debug logging statements

--- Source/CombatExtended/Harmony/Harmony_Pawn_HealthTracker.cs
+++ Source/CombatExtended/Harmony/Harmony_Pawn_HealthTracker.cs
@@ -26,8 +26,6 @@ namespace CombatExtended.HarmonyCE
 
             bool flag = false;
 
-            Log.Message("Combat Extended :: Harmony_Pawn_HealthTracker_CheckForStateChange transpiler called");
-
             List<CodeInstruction> codes = instructions.ToList();
             for (int i = 0; i < codes.Count; i++)
             {
@@ -35,7 +33,6 @@ namespace CombatExtended.HarmonyCE
                 if (codes[i].opcode == OpCodes.Callvirt && codes[i].OperandIs(mGet_IsMechanoid))
                 {
                     flag = true;
-                    Log.Message("Combat Extended :: Found Get_IsMecchanoid call");
                 }
                 //take the next unconditional branch
                 else if (flag == true && codes[i].opcode == OpCodes.Br_S)
@@ -43,7 +40,6 @@ namespace CombatExtended.HarmonyCE
                     // point branch at the down on death code, instead of the random chance check blocking it
                     codes[i] = new CodeInstruction(OpCodes.Br_S, deathOnDownLabel);
                     flag = false;
-                    Log.Message("Combat Extended :: Branch at " + i.ToString("X") + " replaced");
                 }
                 // find instruction calling Verse.DebugViewSettings::logCauseOfDeath and label it
                 else if (codes[i].opcode == OpCodes.Ldsfld && ReferenceEquals(codes[i].operand, AccessTools.Field(typeof(DebugViewSettings), "logCauseOfDeath")))
@@ -52,7 +48,6 @@ namespace CombatExtended.HarmonyCE
                         {
                             deathOnDownLabel
                         };
-                    Log.Message("Combat Extended :: logCauseOfDeath label placed");
                 }
             }
             return Transpilers.MethodReplacer(
@@ -64,7 +59,6 @@ namespace CombatExtended.HarmonyCE
 
         static bool NoChance(float unusedChance)
         {
-            Log.Message("Combat Extended :: Random Death on down bypassed");
             return false;
         }
     }
