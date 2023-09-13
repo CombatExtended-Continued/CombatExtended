c5121a846eb729a8ed2d773011dad80cd2a2a648
CMDR-Bill-Doors <Billyanghk@qq.com>
2023-09-13 13:15:31 +0100

    Update WorldObjectDamageWorker.cs

--- Source/CombatExtended/CombatExtended/WorldObjects/WorldObjectDamageWorker.cs
+++ Source/CombatExtended/CombatExtended/WorldObjects/WorldObjectDamageWorker.cs
@@ -35,12 +35,13 @@ namespace CombatExtended
                 }
             }
             var result = FragmentsPotentialDamage(projectile) + ExplosionPotentialDamage(projectile) + FirePotentialDamage(projectile) + EMPPotentialDamage(projectile, empModifier);
-            //Damage calculated as in-map damage, needs to be converted into world object damage. 3500f experimentally obtained
-            result /= 3500f;
             if (projectile.projectile is ProjectilePropertiesCE projectileProperties && projectileProperties.shellingProps.damage > 0f)
             {
                 result = projectileProperties.shellingProps.damage;
             }
+            //Damage calculated as in-map damage, needs to be converted into world object damage. 3500f experimentally obtained
+            result /= 3500f;
+            Log.Message(result.ToString());
             //Crit/Miss imitation
             result *= Rand.Range(0.4f, 1.5f);
             return result;
