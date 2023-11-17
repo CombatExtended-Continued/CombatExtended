afd2b15ae327e8fafc7e90e202cc2915af0a17df
CMDR-Bill-Doors <Billyanghk@qq.com>
2023-11-17 07:35:36 +0000

    height fuse

---
+++ Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE_HeightFuse.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+using CombatExtended.Compatibility;
+using CombatExtended.Lasers;
+using ProjectileImpactFX;
+using CombatExtended.Utilities;
+
+namespace CombatExtended
+{
+    class ProjectileCE_HeightFuse : ProjectileCE
+    {
+        float detonationHeight => (def.projectile as ProjectilePropertiesCE).aimHeightOffset;
+
+        bool armed;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref armed, "armed", false);
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (!armed && LastPos.y > detonationHeight)
+            {
+                armed = true;
+            }
+            if (armed && Height <= detonationHeight)
+            {
+                HeightFuseAirBurst();
+            }
+        }
+
+        public override void Impact(Thing hitThing)
+        {
+            //intercept impact if it hit something after where height fuse should have triggered
+            if (armed && Height <= detonationHeight)
+            {
+                HeightFuseAirBurst();
+            }
+            else
+            {
+                base.Impact(hitThing);
+            }
+        }
+
+        void HeightFuseAirBurst()
+        {
+            Mote mote = MoteMaker.MakeStaticMote(DrawPos, Map, CE_ThingDefOf.Mote_BigExplode, (def.projectile as ProjectilePropertiesCE).explosionRadius * 2);
+            float f = (LastPos.y - detonationHeight) / (LastPos.y - Height);
+            ExactPosition = f * (LastPos - ExactPosition);
+            base.Impact(null);
+        }
+    }
+}
