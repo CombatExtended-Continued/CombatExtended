bdfddc769d9c015f9fbcbbf837a1e1bbb614f6fc
MaxDorob <maxdor81@gmail.com>
2024-04-15 23:34:10 +0600

    ProjectileCE_SpawnsThing

---
+++ Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE_SpawnsThing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.Noise;
+
+namespace CombatExtended
+{
+    public class ProjectileCE_SpawnsThing : ProjectileCE
+    {
+        public override void Impact(Thing hitThing)
+        {
+            base.Impact(hitThing);
+            Thing thing = GenSpawn.Spawn(ThingMaker.MakeThing(this.def.projectile.spawnsThingDef, null), Position, Map, WipeMode.Vanish);
+            if (thing.def.CanHaveFaction && launcher != null)
+            {
+                thing.SetFaction(this.launcher.Faction, null);
+            }
+        }
+    }
+}
