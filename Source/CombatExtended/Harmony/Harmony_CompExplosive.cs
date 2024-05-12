183778e9f14ac341bfad7b5b2afbbe003827eff7
Manifold Paradox <masakitenchi@users.noreply.github.com>
2024-05-12 12:40:23 +0900

    CompExplosive Transpiler rework
    Also add new fields in GenExplosion to GenExplosionCE

--- Source/CombatExtended/Harmony/Harmony_CompExplosive.cs
+++ Source/CombatExtended/Harmony/Harmony_CompExplosive.cs
@@ -27,90 +27,87 @@ namespace CombatExtended.HarmonyCE
             }
         }
 
+        /*
+            instance void Detonate (
+            class Verse.Map map, -> Ldarg_1
+            [opt] bool ignoreUnspawned
+            )
+            
+            1.5
+            .locals init (
+            [0] class RimWorld.CompProperties_Explosive,
+            [1] float32,
+            [2] class Verse.Thing,
+            [3] valuetype [mscorlib]System.Nullable`1<valuetype Verse.DamageInfo>,
+            [4] bool,
+            [5] valuetype [mscorlib]System.Nullable`1<float32>,
+            [6] valuetype [mscorlib]System.Nullable`1<valuetype Verse.FloatRange>
+            )
+
+            1.4
+            .locals init (
+            [0] class RimWorld.CompProperties_Explosive,
+            [1] float32,
+            [2] class Verse.Thing,
+            [3] valuetype [mscorlib]System.Nullable`1<valuetype Verse.DamageInfo>,
+            [4] valuetype [mscorlib]System.Nullable`1<float32>,
+            [5] valuetype [mscorlib]System.Nullable`1<valuetype Verse.FloatRange>
+            )
+        */
+        [HarmonyDebug]
         internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             //TODO: 1.5
-            foreach (var code in instructions)
+            /* foreach (var code in instructions)
             {
                 yield return code;
+            } */
+            /* bool codeFound = false;
+            bool codeChanged = false; */
+
+            foreach (var code in instructions)
+            {
+
+                // Replace GenExplosion.DoExplosion with GenExplosionCE.DoExplosion, populate extra four fields with "0f" and "Mathf.Pow(this.parent.stackCount, 0.333f)", "this.destroyedThroughDetonation" and "this.parent"
+                if (!codeFound && code.Calls(AccessTools.Method(typeof(GenExplosion), nameof(GenExplosion.DoExplosion))))
+                {
+                    codeFound = true;
+
+                    yield return new CodeInstruction(OpCodes.Ldc_R4, 0f);  //Populate the height field added in the new doExplosion method
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ThingComp), nameof(ThingComp.parent)));
+                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Thing), nameof(Thing.stackCount)));
+                    yield return new CodeInstruction(OpCodes.Conv_R4);
+                    yield return new CodeInstruction(OpCodes.Ldc_R4, 0.333f);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Mathf), nameof(Mathf.Pow)));    //Populate the scaleFactor field with parent.stackCount ^ (1/3)*/
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(CompExplosive), nameof(CompExplosive.destroyedThroughDetonation)));    //Send along whether the thing should be destroyed
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ThingComp), nameof(ThingComp.parent)));                //Send along parent, e.g the thing that should be destroyed
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(GenExplosionCE), nameof(GenExplosionCE.DoExplosion)));
+
+                    // Fragment part
+                    yield return new CodeInstruction(OpCodes.Ldarg_0); //instance
+                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ThingComp), nameof(ThingComp.parent))); //instance.parent
+                    yield return new CodeInstruction(OpCodes.Ldarg_1); //map
+                    yield return new CodeInstruction(OpCodes.Ldloc_2); //instigator
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Harmony_CompExplosive_Detonate_Transpiler), nameof(Harmony_CompExplosive_Detonate_Transpiler.ThrowFragments)));
+
+                    continue;
+                }
+
+                yield return code;
             }
-            yield break;
-            //            bool codeFound = false;
-            //            int parentCalls = 0;
-            //            bool codeChanged = false;
-            //            bool storeDestructionCall = false;
-            //
-            //            foreach (var code in instructions)
-            //            {
-            //                if (storeDestructionCall)
-            //                {
-            //                    if (code.opcode == OpCodes.Callvirt)
-            //                    {
-            //                        storeDestructionCall = false;
-            //                    }
-            //
-            //                    continue;
-            //                }
-            //
-            //                if (code.opcode == OpCodes.Stfld && code.operand is FieldInfo info
-            //                    && info == AccessTools.Field(typeof(CompExplosive), nameof(CompExplosive.destroyedThroughDetonation)))
-            //                {
-            //                    yield return code;
-            //                    storeDestructionCall = true;
-            //                    continue;
-            //                }
-            //
-            //                if (code.opcode == OpCodes.Stloc_2)
-            //                {
-            //                    parentCalls++;
-            //                }
-            //
-            //                // Replace GenExplosion.DoExplosion with GenExplosionCE.DoExplosion, populate extra four fields with "0f" and "Mathf.Pow(this.parent.stackCount, 0.333f)", "this.destroyedThroughDetonation" and "this.parent"
-            //                if (codeChanged && !codeFound && code.opcode == OpCodes.Call && code.operand is MethodInfo info2
-            //                    && info2 == AccessTools.Method(typeof(GenExplosion), nameof(GenExplosion.DoExplosion)))
-            //                {
-            //                    codeFound = true;
-            //
-            //                    yield return new CodeInstruction(OpCodes.Ldc_R4, 0f);  //Populate the height field added in the new doExplosion method
-            //                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-            //                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ThingComp), nameof(ThingComp.parent)));
-            //                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Thing), nameof(Thing.stackCount)));
-            //                    yield return new CodeInstruction(OpCodes.Conv_R4);
-            //                    yield return new CodeInstruction(OpCodes.Ldc_R4, 0.333f);
-            //                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Mathf), nameof(Mathf.Pow)));    //Populate the scaleFactor field with parent.stackCount ^ (1/3)*/
-            //                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-            //                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(CompExplosive), nameof(CompExplosive.destroyedThroughDetonation)));    //Send along whether the thing should be destroyed
-            //                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-            //                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ThingComp), nameof(ThingComp.parent)));                //Send along parent, e.g the thing that should be destroyed
-            //                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(GenExplosionCE), nameof(GenExplosionCE.DoExplosion)));
-            //
-            //                    continue;
-            //                }
-            //
-            //                if (parentCalls == 2 && !codeChanged && code.opcode == OpCodes.Callvirt)
-            //                {
-            //                    // Call ThrowFragments and duplicate previous calls
-            //                    yield return new CodeInstruction(OpCodes.Ldarg_1);
-            //                    yield return new CodeInstruction(OpCodes.Ldloc_2);
-            //                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Harmony_CompExplosive_Detonate_Transpiler), nameof(Harmony_CompExplosive_Detonate_Transpiler.ThrowFragments)));
-            //                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-            //                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ThingComp), nameof(ThingComp.parent)));
-            //
-            //                    codeChanged = true;
-            //                }
-            //
-            //                yield return code;
-            //            }
-            //
-            //            if (!codeFound)
-            //            {
-            //                Log.Warning("CombatExtended :: Could not find doExplosionInfo");
-            //            }
-            //
-            //            if (!codeChanged)
-            //            {
-            //                Log.Warning("CombatExtended :: Could not insert ThrowFragments call");
-            //            }
+
+            if (!codeFound)
+            {
+                Log.Warning("CombatExtended :: Could not find doExplosionInfo");
+            }
+
+            /* if (!codeChanged)
+            {
+                Log.Warning("CombatExtended :: Could not insert ThrowFragments call");
+            } */
         }
     }
 }
