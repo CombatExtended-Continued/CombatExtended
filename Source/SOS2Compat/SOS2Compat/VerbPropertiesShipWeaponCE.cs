4dba172b1944dc5e406ad3744e7d9b382eff3d13
Rhinous <rhinous00@gmail.com>
2024-06-30 22:58:45 +1000

    Move SOS2 patch to its own project

---
+++ Source/SOS2Compat/SOS2Compat/VerbPropertiesShipWeaponCE.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CombatExtended.Compatibility
+{
+    public class VerbPropertiesShipWeaponCE : VerbPropertiesCE
+    {
+        public ThingDef defaultProjectileGround;
+    }
+}
