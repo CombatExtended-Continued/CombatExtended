c32a33440bf7778da57c41f49fa0e5f2bff563c3
Rhinous <rhinous00@gmail.com>
2024-07-10 14:50:32 +1000

    Merge and update shield collision logic

--- Source/SOS2Compat/SOS2Compat/SOS2Compat.cs
+++ Source/SOS2Compat/SOS2Compat/SOS2Compat.cs
@@ -48,21 +48,77 @@ namespace CombatExtended.Compatibility.SOS2Compat
             var exactPosition = projectile.ExactPosition;
             foreach (var shield in shields)
             {
-                if (ShieldInterceptsProjectile(shield, projectile, launcher))
+                // If shield is shut down, skip it
+                if (shield.shutDown)
                 {
-                    // Succesful Intercept
-                    // Replicate non-ce SOS2 shield effects
-                    shield.lastInterceptAngle = projectile.DrawPos.AngleToFlat(shield.parent.Position.ToVector3());
-                    shield.lastIntercepted = Find.TickManager.TicksGame;
+                    continue;
+                }
+
+                Vector3 shieldPosition = shield.parent.Position.ToVector3Shifted();
+                Vector3 lastExactPos = projectile.LastPos.Yto0();
+                var newExactPos = projectile.ExactPosition.Yto0();
+                Vector3[] intersectionPoints;
+                if (!CE_Utility.IntersectionPoint(lastExactPos, newExactPos, shieldPosition, shield.radius, out intersectionPoints, false))
+                {
+                    continue;
+                }
+
+                // Check if shield has enough heat to block
+                if (!shield.AddHeatToNetwork(CalcHeatGenerated(projectile, shield)))
+                {
+                    // Not enough heat, break the shield using the last of the heat up
+                    if (shield.myNet != null)
+                    {
+                        // Use up any last bit of heat
+                        shield.AddHeatToNetwork(shield.myNet.StorageCapacity - shield.myNet.StorageUsed);
+                    }
+                    if (shield.breakComp != null)
+                    {
+                        // Break the shield if not a vehicle
+                        shield.breakComp.DoBreakdown();
+                    }
+                    else
+                    {
+                        // Break the shield component if a vehicle
+                        shield.parentVehicle.statHandler.SetComponentHealth("shieldGenerator", 0);
+                        if (shield.parentVehicle.Spawned)
+                        {
+                            shield.parentVehicle.Map.GetComponent<ListerVehiclesRepairable>().Notify_VehicleTookDamage(shield.parentVehicle);
+                        }
+                    }
+                    // Break Effects
                     if (shield.parent.Spawned)
                     {
-                        FleckMaker.ThrowMicroSparks(shield.parent.DrawPos, shield.parent.Map);
-                        GenExplosion.DoExplosion(projectile.Position, shield.parent.Map, 3, DefDatabase<DamageDef>.GetNamed("ShieldExplosion"), null, screenShakeFactor: 0);
+                        GenExplosion.DoExplosion(shield.parent.Position, shield.parent.Map, 1.9f, DamageDefOf.Flame, shield.parent);
                     }
-                    exactPosition = BlockerRegistry.GetExactPosition(projectile.OriginIV3.ToVector3(), exactPosition, new Vector3(shield.parent.Position.x, 0, shield.parent.Position.z), shield.radius * shield.radius);
-                    projectile.InterceptProjectile(shield, exactPosition, true);
-                    return true;
+                    SoundDef.Named("EnergyShield_Broken").PlayOneShot(new TargetInfo(shield.parent));
+                    // Notifs
+                    if (shield.parent.Faction != Faction.OfPlayer)
+                    {
+                        Messages.Message(TranslatorFormattedStringExtensions.Translate("SoS.CombatShieldBrokenEnemy"), shield.parent, MessageTypeDefOf.PositiveEvent);
+                    }
+                    else
+                    {
+                        Messages.Message(TranslatorFormattedStringExtensions.Translate("SoS.CombatShieldBroken"), shield.parent, MessageTypeDefOf.NegativeEvent);
+                    }
+                    continue;
                 }
+                // Succesful Intercept
+                // Shield effects
+                Vector3 interceptPos = intersectionPoints.OrderBy(x => (projectile.OriginIV3.ToVector3() - x).sqrMagnitude).First();
+
+                shield.lastInterceptAngle = interceptPos.AngleToFlat(shield.parent.Position.ToVector3());
+                shield.lastIntercepted = Find.TickManager.TicksGame;
+
+                if (shield.parent.Spawned)
+                {
+                    SoundDef.Named("Interceptor_BlockProjectile").PlayOneShot((SoundInfo)new TargetInfo(shield.parent.Position, map, false));
+                    FleckMaker.ThrowMicroSparks(shield.parent.DrawPos, shield.parent.Map);
+                    FleckMakerCE.ThrowLightningGlow(interceptPos, map, 0.5f);
+                }
+                // Actually Intercept the projectile
+                projectile.InterceptProjectile(shield, interceptPos, true);
+                return true;
             }
             return false;
         }
@@ -84,78 +140,6 @@ namespace CombatExtended.Compatibility.SOS2Compat
                 result.Add(GenRadial.RadialCellsAround(shield.parent.Position, shield.radius, true));
             }
             return result;
-        }
-
-        private static bool ShieldInterceptsProjectile(CompShipHeatShield shield, ProjectileCE projectile, Thing launcher)
-        {
-            // If shield is shut down, skip it
-            if (shield.shutDown)
-            {
-                return false;
-            }
-
-            // Converting for distance calcs
-            Vector3 shieldPos = shield.parent.Position.ToVector3Shifted();
-            Vector3 launcherPos = launcher.Position.ToVector3Shifted();
-            Vector3 projectilePos = projectile.Position.ToVector3Shifted();
-
-            shieldPos.y = launcherPos.y; // We just want to check x,z distance
-            // Check if shot is originating from inside the shield
-            if (Vector3.Distance(shieldPos, launcherPos) < shield.radius)
-            {
-                // From inside shield so ignore
-                return false;
-            }
-
-            shieldPos.y = projectilePos.y; // We just want to check x,z distance
-            // Check if shot is inside shield radius
-            if (Vector3.Distance(shieldPos, projectilePos) > shield.radius)
-            {
-                // outside shield radius so ignore
-                return false;
-            }
-
-            // Check if shield has enough heat to block
-            if (!shield.AddHeatToNetwork(CalcHeatGenerated(projectile, shield)))
-            {
-                // Not enough heat, break the shield using the last of the heat up
-                if (shield.myNet != null)
-                {
-                    // Use up any last bit of heat
-                    shield.AddHeatToNetwork(shield.myNet.StorageCapacity - shield.myNet.StorageUsed);
-                }
-                if (shield.breakComp != null)
-                {
-                    // Break the shield if not a vehicle
-                    shield.breakComp.DoBreakdown();
-                }
-                else
-                {
-                    // Break the shield component if a vehicle
-                    shield.parentVehicle.statHandler.SetComponentHealth("shieldGenerator", 0);
-                    if (shield.parentVehicle.Spawned)
-                    {
-                        shield.parentVehicle.Map.GetComponent<ListerVehiclesRepairable>().Notify_VehicleTookDamage(shield.parentVehicle);
-                    }
-                }
-                // Break Effects
-                if (shield.parent.Spawned)
-                {
-                    GenExplosion.DoExplosion(shield.parent.Position, shield.parent.Map, 1.9f, DamageDefOf.Flame, shield.parent);
-                }
-                SoundDef.Named("EnergyShield_Broken").PlayOneShot(new TargetInfo(shield.parent));
-                // Notifs
-                if (shield.parent.Faction != Faction.OfPlayer)
-                {
-                    Messages.Message(TranslatorFormattedStringExtensions.Translate("SoS.CombatShieldBrokenEnemy"), shield.parent, MessageTypeDefOf.PositiveEvent);
-                }
-                else
-                {
-                    Messages.Message(TranslatorFormattedStringExtensions.Translate("SoS.CombatShieldBroken"), shield.parent, MessageTypeDefOf.NegativeEvent);
-                }
-                return false;
-            }
-            return true;
         }
 
         private static float CalcHeatGenerated(ProjectileCE projectile, CompShipHeatShield shield)
