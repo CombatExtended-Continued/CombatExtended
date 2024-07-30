574827956e14e8bdaeaddb655edea2c01e696d62
CMDR-Bill-Doors <Billyanghk@qq.com>
2024-07-30 13:15:20 +0100

    List based ammo user

---
+++ Source/CombatExtended/CombatExtended/Comps/CompAmmoListUser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace CombatExtended
+{
+    public class CompAmmoListUser : CompVariableAmmoUser
+    {
+        public new CompProperties_AmmoListUser Props
+        {
+            get
+            {
+                return (CompProperties_AmmoListUser)props;
+            }
+        }
+
+
+        public override List<AmmoSetDef> UsableAmmoSets
+        {
+            get
+            {
+                if (usableAmmoSets.NullOrEmpty())
+                {
+                    foreach (var def in Props.additionalAmmoSets)
+                    {
+                        if (!def.ammoTypes.First().ammo.menuHidden && !IsIdenticalToAny(def))
+                        {
+                            usableAmmoSets.Add(def);
+                        }
+                    }
+                }
+                return usableAmmoSets;
+            }
+        }
+    }
+}
---
+++ Source/CombatExtended/CombatExtended/Comps/CompProperties_AmmoListUser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CombatExtended
+{
+    public class CompProperties_AmmoListUser : CompProperties_AmmoUser
+    {
+        public List<AmmoSetDef> additionalAmmoSets = new List<AmmoSetDef>();
+
+        public CompProperties_AmmoListUser()
+        {
+            compClass = typeof(CompAmmoUser);
+        }
+    }
+}
