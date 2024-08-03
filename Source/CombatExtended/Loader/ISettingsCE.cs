fb837a39f91addfe08729abcfc066fe2d2bd4888
Logan Perkins <logan@github.lp-programming.com>
2024-08-03 00:41:09 -0700

    Move loader functionality into CE itself

---
+++ Source/CombatExtended/Loader/ISettingsCE.cs
@@ -0,0 +1,11 @@
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace CombatExtended.Loader
+{
+    public interface ISettingsCE
+    {
+        public void DoWindowContents(Listing_Standard list);
+    }
+}
