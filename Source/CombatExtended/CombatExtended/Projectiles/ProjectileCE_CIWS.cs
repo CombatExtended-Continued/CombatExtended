b4b8bd1ac6b40d485ea87169d3a2a9c285ca65b6
MaxDorob <maxdor81@gmail.com>
2024-08-16 00:25:58 +0600

    Initial commit for CIWS projectile and turret

---
+++ Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE_CIWS.cs
@@ -0,0 +1,65 @@
+﻿using Mono.Unix.Native;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace CombatExtended
+{
+    internal class ProjectileCE_CIWS : ProjectileCE
+    {
+        #region Caching
+        protected static Dictionary<Map, List<ProjectileCE_CIWS>> CIWSProjectiles = new Dictionary<Map, List<ProjectileCE_CIWS>>();
+        public static IEnumerable<ProjectileCE_CIWS> ProjectilesAt(Map map)
+        {
+            CIWSProjectiles.TryGetValue(map, out var targets);
+            return targets ?? Enumerable.Empty<ProjectileCE_CIWS>();
+        }
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            if (map == null)
+            {
+                return;
+            }
+            if (!CIWSProjectiles.TryGetValue(map, out var targetList))
+            {
+                CIWSProjectiles[map] = targetList = new List<ProjectileCE_CIWS>();
+            }
+            targetList.Add(this);
+        }
+        public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+        {
+            Map map = Map;
+            base.DeSpawn(mode);
+            if (CIWSProjectiles.TryGetValue(map, out var targetList))
+            {
+                targetList.Remove(this);
+                if (targetList.Count <= 0)
+                {
+                    CIWSProjectiles.Remove(map);
+                }
+            }
+        }
+        #endregion
+
+        public virtual float CollideDistance => (def.projectile as ProjectilePropertiesCE)?.collideDistance ?? 1f;
+        public virtual float ImpactChance => (def.projectile as ProjectilePropertiesCE)?.impactChance ?? 1f;
+        public override void Tick()
+        {
+            ticksToImpact++; //do not allow it hit zero
+            base.Tick();
+            TryCollideWith(intendedTargetThing);
+        }
+        protected override bool CheckForCollisionBetween()
+        {
+            return false;
+        }
+    }
+}
---
+++ Source/CombatExtended/CombatExtended/Things/Building_CIWS_CE.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace CombatExtended
+{
+    public class Building_CIWS_CE : Building_TurretGunCE
+    {
+        IEnumerable<CompCIWS> ciws;
+        public IEnumerable<CompCIWS> CIWS => ciws ??= this.GetComps<CompCIWS>().ToList();
+
+        public override Verb AttackVerb
+        {
+            get
+            {
+                foreach (var ciws in CIWS)
+                {
+                    if (ciws.HasTarget)
+                    {
+                        var verb = ciws.Verb;
+                        if (verb != null)
+                        {
+                            return verb;
+                        }
+                    }
+                }
+                return base.AttackVerb;
+            }
+        }
+        public override void DrawExtraSelectionOverlays()
+        {
+            base.DrawExtraSelectionOverlays();
+            foreach (var verb in GunCompEq.AllVerbs.Except(AttackVerb))
+            {
+                float range = verb.verbProps.range;
+                if (range < 90f)
+                {
+                    GenDraw.DrawRadiusRing(base.Position, range);
+                }
+                float num = verb.verbProps.EffectiveMinRange(true);
+                if (num < 90f && num > 0.1f)
+                {
+                    GenDraw.DrawRadiusRing(base.Position, num);
+                }
+            }
+
+        }
+    }
+}
