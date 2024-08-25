3c47a1e29a8dc7910035975c2f99b7c9d28d238c
CMDR-Bill-Doors <Billyanghk@qq.com>
2024-08-25 20:49:44 +0100

    Create ThingSetMaker_CountEnabledAmmoOnly.cs

---
+++ Source/CombatExtended/ThingSetMaker_CountEnabledAmmoOnly.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace CombatExtended
+{
+    public class ThingSetMaker_CountEnabledAmmoOnly : ThingSetMaker_Count
+    {
+        public override void Generate(ThingSetMakerParams parms, List<Thing> outThings)
+        {
+            parms.validator = delegate (ThingDef d)
+            {
+                if (d is AmmoDef ammodef && (ammodef.tradeTags?.Contains(AmmoInjector.destroyWithAmmoDisabledTag) ?? false))
+                {
+                    return AmmoUtility.IsAmmoSystemActive(ammodef);
+                }
+                return true;
+            };
+            base.Generate(parms, outThings);
+        }
+    }
+}
