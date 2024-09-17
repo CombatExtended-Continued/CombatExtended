a405cd82b58ae1275b6bdce9100eac66369906eb
CMDR-Bill-Doors <Billyanghk@qq.com>
2024-09-17 04:46:53 +0100

    Create Harmony_StartingPawnUtility.cs

---
+++ Source/CombatExtended/Harmony/Harmony_StartingPawnUtility.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using Verse;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CombatExtended.HarmonyCE
+{
+    [HarmonyPatch(typeof(StartingPawnUtility), nameof(StartingPawnUtility.GeneratePossessions))]
+    internal static class StartingPawnUtility_GenerateAmmo
+    {
+        static IntRange magRange = new IntRange(3, 5);
+
+        internal static void Postfix(Pawn pawn)
+        {
+            var startingPossessions = Find.GameInitData.startingPossessions;
+            if (startingPossessions.ContainsKey(pawn))
+            {
+                List<ThingDefCount> ammoList = new List<ThingDefCount>();
+                foreach (var possession in startingPossessions[pawn])
+                {
+                    if (possession.thingDef.GetCompProperties<CompProperties_AmmoUser>() is CompProperties_AmmoUser ammoUser && ammoUser.ammoSet != null)
+                    {
+                        int count = ammoUser.AmmoGenPerMagOverride;
+                        if (count <= 0)
+                        {
+                            count = ammoUser.magazineSize;
+                        }
+                        count *= magRange.RandomInRange;
+
+                        ammoList.Add(new ThingDefCount(ammoUser.ammoSet.ammoTypes.First().ammo, count));
+                    }
+                }
+                foreach (var ammo in ammoList)
+                {
+                    startingPossessions[pawn].Add(ammo);
+                }
+            }
+        }
+    }
+}
