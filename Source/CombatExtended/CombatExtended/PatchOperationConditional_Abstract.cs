be6a2a9eb7121db55a1169d2bc73c7fb70862759
CMDR-Bill-Doors <Billyanghk@qq.com>
2024-09-18 09:31:06 +0100

    Create PatchOperationConditional_Abstract.cs

---
+++ Source/CombatExtended/CombatExtended/PatchOperationConditional_Abstract.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml;
+using Verse;
+
+namespace CombatExtended.CombatExtended
+{
+    public abstract class PatchOperationConditional_Abstract : PatchOperation
+    {
+        private PatchOperation match;
+
+        private PatchOperation nomatch;
+
+        protected abstract bool Applicable(XmlDocument xml);
+
+        public override bool ApplyWorker(XmlDocument xml)
+        {
+            if (Applicable(xml))
+            {
+                if (match != null)
+                {
+                    return match.Apply(xml);
+                }
+            }
+            else if (nomatch != null)
+            {
+                return nomatch.Apply(xml);
+            }
+            if (match == null)
+            {
+                return nomatch != null;
+            }
+            return true;
+        }
+    }
+
+    public class PatchOperationConditional_GenericAmmo : PatchOperationConditional_Abstract
+    {
+        protected override bool Applicable(XmlDocument xml)
+        {
+            return Controller.settings.GenericAmmo;
+        }
+    }
+    public class PatchOperationConditional_AmmoSystemOff : PatchOperationConditional_Abstract
+    {
+        protected override bool Applicable(XmlDocument xml)
+        {
+            return !Controller.settings.EnableAmmoSystem;
+        }
+    }
+}
