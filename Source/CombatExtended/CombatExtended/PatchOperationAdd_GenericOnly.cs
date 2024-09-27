41ffce5d6c0dfe9f80f819eacc34146793dc2d4d
Safairette <71556532+Safairette@users.noreply.github.com>
2024-09-27 14:20:11 +0300

    Add Generic-only patch ops

---
+++ Source/CombatExtended/CombatExtended/PatchOperationAdd_GenericOnly.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml;
+using Verse;
+
+namespace CombatExtended
+{
+    public class PatchOperationAdd_GenericOnly : PatchOperationAdd
+    {
+        public override bool ApplyWorker(XmlDocument xml)
+        {
+            if (!Controller.settings.GenericAmmo)
+            {
+                return true;
+            }
+            return base.ApplyWorker(xml);
+        }
+    }
+}
---
+++ Source/CombatExtended/CombatExtended/PatchOperationRemove_GenericOnly.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml;
+using Verse;
+
+namespace CombatExtended
+{
+    public class PatchOperationRemove_GenericOnly : PatchOperationRemove
+    {
+        public override bool ApplyWorker(XmlDocument xml)
+        {
+            if (!Controller.settings.GenericAmmo)
+            {
+                return true;
+            }
+            return base.ApplyWorker(xml);
+        }
+    }
+}
