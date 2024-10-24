CombatExtended-Continued/CombatExtended

commit 65ff9683efa00fa36d68ec6e11e523c44ff3d998

Ensure mech ammo beacons don't provide infinite ammo
Make it so that mech ammo beacons only drop ammo if the cluster still
has active mechanoids, so that they can't be milked for infinite mech
ammo. Do this by finding the Lord associated with the cluster and
checking that it still has pawns attached.

Source/CombatExtended/CombatExtended/Comps/CompAmmoResupplyOnWakeup.cs
@@ -27,23 +27,55 @@ namespace CombatExtended
 
         public CompProperties_AmmoResupplyOnWakeup Props => (CompProperties_AmmoResupplyOnWakeup)props;
 
-        //Only do something if not dormant
-        //Only do something when ammo system is enabled
-        public bool IsActive => Controller.settings.EnableAmmoSystem && (parent.TryGetComp<CompCanBeDormant>()?.Awake ?? true);
+        /// <summary>
+        /// Cached reference to the <see cref="Lord"/> of the mech cluster that the owner of this comp is part of.
+        /// </summary>
+        private Lord lord;
 
-        LordJob_MechanoidDefendBase parentLordJob => ((parent as Building)?.GetLord()?.LordJob as LordJob_MechanoidDefendBase);
-        public bool ClusterAlive
+        /// <summary>
+        /// Determine whether this beacon should be supplying ammo.
+        /// </summary>
+        public bool IsActive
         {
             get
             {
-                var lordJob = parentLordJob;
-
-                if (lordJob == null)
+                // No point in supplying ammo if it's not enabled
+                if (!Controller.settings.EnableAmmoSystem)
                 {
                     return false;
                 }
 
-                return !lordJob.mechClusterDefeated;
+                if (parent.TryGetComp<CompCanBeDormant>()?.Awake ?? false)
+                {
+                    return false;
+                }
+
+                // Ensure this beacon does not provide an infinite source of ammo
+                // if there are no remaining mechs that could use it to reload turrets.
+                return lord?.AnyActivePawn ?? false;
+            }
+        }
+
+        /// <summary>
+        /// Find the owning <see cref="Lord"/> (i.e. mech cluster) for this beacon after spawning.
+        /// </summary>
+        /// <remarks>
+        /// Ammo beacons are not considered to be threats like turrets or assemblers and so are not directly owned
+        /// by the <see cref="Lord"/> of the parent cluster; it is therefore necessary to find their owner manually.
+        /// </remarks>
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            var faction = parent.Faction;
+            foreach (var lord in parent.Map.lordManager.lords)
+            {
+                if (lord.faction == faction && lord.LordJob is LordJob_MechanoidDefendBase mechDefendBaseJob)
+                {
+                    if (mechDefendBaseJob.isMechCluster && mechDefendBaseJob.things.Contains(parent))
+                    {
+                        this.lord = lord;
+                        break;
+                    }
+                }
             }
         }
 

commit 0793a9801f2ee80fbe9cad0d721e5480f8063656

Fix initially dormant mech ammo beacons
65ff9683efa00fa36d68ec6e11e523c44ff3d998 mistakenly turned off mech ammo beacons for clusters that start off dormant.
Fix the logic so that beacons in such clusters correctly drop ammo once the cluster is awake and other conditions are met.

Source/CombatExtended/CombatExtended/Comps/CompAmmoResupplyOnWakeup.cs
@@ -45,7 +45,7 @@ namespace CombatExtended
                     return false;
                 }
 
-                if (parent.TryGetComp<CompCanBeDormant>()?.Awake ?? false)
+                if (!(parent.TryGetComp<CompCanBeDormant>()?.Awake ?? true))
                 {
                     return false;
                 }