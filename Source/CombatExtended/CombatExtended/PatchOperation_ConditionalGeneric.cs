b1a7e714cebe36bc0417c7f8f31170962af74701
Safairette <71556532+Safairette@users.noreply.github.com>
2024-10-25 20:30:41 +0300

    Add conditional for generics

---
+++ Source/CombatExtended/CombatExtended/PatchOperation_ConditionalGeneric.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+using Verse;
+
+namespace CombatExtended
+{
+    public class PatchOperation_ConditionalGeneric : PatchOperation
+    {
+        public PatchOperation standard;
+        public PatchOperation generic;
+
+        public override bool ApplyWorker(XmlDocument xml)
+        {
+            if (Controller.settings.GenericAmmo && generic != null)
+            {
+                return generic.Apply(xml);
+            }
+            else if (standard != null)
+            {
+                return standard.Apply(xml);
+            }
+
+            return true;
+        }
+    }
+}
