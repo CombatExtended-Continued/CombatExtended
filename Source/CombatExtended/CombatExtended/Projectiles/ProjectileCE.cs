CombatExtended-Continued/CombatExtended

commit d90f3e1395438165e9bade2780299cb2b64b177d

Update ProjectileCE.cs

Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE.cs
@@ -106,6 +106,7 @@ namespace CombatExtended
         public bool canTargetSelf;
         public bool castShadow = true;
         public bool logMisses = true;
+        protected bool ignoreRoof;
 
         public GlobalTargetInfo globalTargetInfo = GlobalTargetInfo.Invalid;
         public GlobalTargetInfo globalSourceInfo = GlobalTargetInfo.Invalid;
@@ -348,6 +349,7 @@ namespace CombatExtended
             Scribe_Values.Look<bool>(ref logMisses, "logMisses", true);
             Scribe_Values.Look<bool>(ref castShadow, "castShadow", true);
             Scribe_Values.Look<bool>(ref lerpPosition, "lerpPosition", true);
+            Scribe_Values.Look(ref ignoreRoof, "ignoreRoof", true);
 
             //To fix landed grenades sl problem
             Scribe_Values.Look(ref exactPosition, "exactPosition");
@@ -550,6 +552,10 @@ namespace CombatExtended
                 this.castShadow = props.castShadow;
                 this.lerpPosition = props.lerpPosition;
                 this.GravityFactor = props.Gravity;
+            }
+            if (shotHeight >= CollisionVertical.WallCollisionHeight && launcher.Position.Roofed(launcher.Map))
+            {
+                ignoreRoof = true;
             }
             Launch(launcher, origin, equipment);
         }
@@ -867,7 +873,7 @@ namespace CombatExtended
 
         protected virtual bool TryCollideWithRoof(IntVec3 cell)
         {
-            if (!cell.Roofed(Map))
+            if (!cell.Roofed(Map) || ignoreRoof)
             {
                 return false;
             }

commit 2b14d2e29a51a432d1a7d7dbe1061891faf90b2d

Fix retaliation raids and shelling
As reported in #3419, retaliation raids currently cause infinite error spam
if the bombardment that triggered the retaliation was from a temporary map
that was disposed of by the time the raid was actually executed. Fix it by
checking whether the target map for the raid is actually valid.

While testing this, I found that retaliation shelling has also been broken since d90f3e1395438165e9bade2780299cb2b64b177d.
Fix it by accounting for the case where the launcher is not spawned at all,
since retaliation bombardment shells are "launched" by world pawns.

Source/CombatExtended/CombatExtended/Projectiles/ProjectileCE.cs
@@ -551,7 +551,9 @@ namespace CombatExtended
         /// <param name="shotSpeed">The shot speed (default: def.projectile.speed)</param>
         /// <param name="equipment">The equipment used to fire the projectile.</param>
         /// <param name="distance">The distance to the estimated intercept point</param>
-        /// <param name="ticksToTruePosition">The number of ticks before the bullet is drawn at its true height instead of the muzzle height</param>
+        /// <remarks>
+        /// Note that the launcher may not be spawned at all, e.g. for projectiles launched by enemy bases as retaliation.
+        /// </remarks>
         public virtual void Launch(Thing launcher, Vector2 origin, float shotAngle, float shotRotation, float shotHeight = 0f, float shotSpeed = -1f, Thing equipment = null, float distance = -1)
         {
             this.shotAngle = shotAngle;
@@ -565,7 +567,7 @@ namespace CombatExtended
                 this.lerpPosition = props.lerpPosition;
                 this.GravityFactor = props.Gravity;
             }
-            if (shotHeight >= CollisionVertical.WallCollisionHeight && Position.Roofed(launcher.Map))
+            if (shotHeight >= CollisionVertical.WallCollisionHeight && launcher.Spawned && Position.Roofed(launcher.Map))
             {
                 ignoreRoof = true;
             }

Source/CombatExtended/CombatExtended/WorldObjects/HostilityRaider.cs
@@ -67,7 +67,7 @@ namespace CombatExtended.WorldObjects
                 ticksToRaid -= WorldObjectTrackerCE.THROTTLED_TICK_INTERVAL;
                 return;
             }
-            if (parms != null)
+            if (parms != null && Find.Maps.Contains(parms.target))
             {
                 IncidentDef incidentDef = IncidentDefOf.RaidEnemy;
                 incidentDef.Worker.TryExecute(parms);

Source/CombatExtended/CombatExtended/WorldObjects/TravelingShell.cs
@@ -109,7 +109,7 @@ namespace CombatExtended
         private bool TryShell(WorldObject worldObject)
         {
             bool shelled = false;
-            if (worldObject is MapParent mapParent && mapParent.HasMap)
+            if (worldObject is MapParent mapParent && mapParent.HasMap && Find.Maps.Contains(mapParent.Map))
             {
                 shelled = true;
                 Map map = mapParent.Map;