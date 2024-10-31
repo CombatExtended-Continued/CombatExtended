c082f99fa524852b009bc73cf979ea5ba40e092d
MaxDorob <maxdor81@gmail.com>
2024-10-31 23:43:27 +0600

    Moved projectile code

--- Source/CombatExtended/CombatExtended/Comps/VerbCIWSProjectile.cs
+++ Source/CombatExtended/CombatExtended/Comps/VerbCIWSProjectile.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
+using static UnityEngine.UI.Image;
 
 namespace CombatExtended
 {
@@ -17,6 +19,64 @@ namespace CombatExtended
         public override IEnumerable<ProjectileCE> Targets => Caster.Map?.listerThings.ThingsInGroup(ThingRequestGroup.Projectile).OfType<ProjectileCE>() ?? Array.Empty<ProjectileCE>();
 
         protected override bool IsFriendlyTo(ProjectileCE thing) => base.IsFriendlyTo(thing) && !thing.launcher.HostileTo(Caster);
+        public override bool TryFindCEShootLineFromTo(IntVec3 root, LocalTargetInfo target, out ShootLine resultingLine)
+        {
+            if (base.TryFindCEShootLineFromTo(root, target, out resultingLine))
+            {
+                return true;
+            }
+            if (!(target.Thing is ProjectileCE targetProjectile))
+            {
+                return false;
+            }
+            var originV3 = Caster.Position.ToVector3Shifted();
+            var ticksToSkip = (int)verbProps.warmupTime;
+            var instant = Projectile.projectile is ProjectilePropertiesCE CIWSProjectilePropertiesCE && CIWSProjectilePropertiesCE.isInstant;
+            if (instant)
+            {
+                var to = targetProjectile.NextPositions.Skip(ticksToSkip).FirstOrFallback(Vector3.negativeInfinity);
+                if (to == Vector3.negativeInfinity)
+                {
+                    resultingLine = default;
+                    return false;
+                }
+                resultingLine = new ShootLine(originV3.ToIntVec3(), to.ToIntVec3());
+                return true;
+            }
+            int i = 1;
+            if (targetProjectile.def.projectile is ProjectilePropertiesCE targetProjectileProperties && Projectile.projectile is ProjectilePropertiesCE CIWS_ProjectileProperties)
+            {
+                var targetPos1 = new Vector2(targetProjectile.Position.x, targetProjectile.Position.z);
+                foreach (var pos in targetProjectile.NextPositions.Skip(ticksToSkip))
+                {
+                    if (i > maximumPredectionTicks)
+                    {
+                        break;
+                    }
+                    var report = ShiftVecReportFor(pos.ToIntVec3());
+                    ShiftTarget(report);
+
+                    Vector2 originV2 = new Vector2(originV3.x, originV3.z), destinationV2 = new Vector2(pos.x, pos.z);
+                    var positions = CIWS_ProjectileProperties.NextPositions(shotRotation, shotAngle, originV2, destinationV2, maximumPredectionTicks, ShotHeight, false, Vector3.zero, ShotSpeed, originV3, -1f, -1f, -1f, -1f, ShotSpeed, 0).Skip(i - 1).Take(2).ToList();
+                    if (positions.Count < 2)
+                    {
+                        resultingLine = default(ShootLine);
+                        return false;
+                    }
+                    Vector2 ciwsPos1 = new Vector2(positions.First().x, positions.First().z), ciwsPos2 = new Vector2(positions.Last().x, positions.Last().z), targetPos2 = new Vector2(pos.x, pos.z);
+
+                    if (CE_Utility.TryFindIntersectionPoint(ciwsPos1, ciwsPos2, targetPos1, targetPos2, out var point))
+                    {
+                        resultingLine = new ShootLine(Shooter.Position, point.ToVector3().ToIntVec3());
+                        return true;
+                    }
+                    targetPos1 = targetPos2;
+                    i++;
+                }
+            }
+            resultingLine = default;
+            return false;
+        }
     }
     public class CompProperties_CIWSProjectile : VerbProperties_CIWS
     {
--- Source/CombatExtended/CombatExtended/Verbs/Verb_ShootCE_CIWS.cs
+++ Source/CombatExtended/CombatExtended/Verbs/Verb_ShootCE_CIWS.cs
@@ -37,51 +37,8 @@ namespace CombatExtended
                 var result = targetComp.CalculatePointForPreemptiveFire(Projectile, originV3, out var targetPos, ticksToSkip);
                 resultingLine = new ShootLine(originV3.ToIntVec3(), targetPos.ToIntVec3());
                 return result;
-            }
-            else if (targ.Thing is Skyfaller skyfaller)
-            {
-
-            }
-            else if (targ.Thing is ProjectileCE projectile)
-            {
-                var instant = Projectile.projectile is ProjectilePropertiesCE projectilePropertiesCE && projectilePropertiesCE.isInstant;
-                if (instant)
-                {
-                    resultingLine = new ShootLine(root, projectile.Position);
-                    return true;
-                }
-                int i = 1;
-                if (projectile.def.projectile is ProjectilePropertiesCE target_ProjectilePropeties && Projectile.projectile is ProjectilePropertiesCE CIWS_ProjectilePropeties)
-                {
-                    var targetPos1 = new Vector2(projectile.Position.x, projectile.Position.z);
-                    foreach (var pos in projectile.NextPositions.Skip(ticksToSkip))
-                    {
-                        if (i > maximumPredectionTicks)
-                        {
-                            break;
-                        }
-                        var report = ShiftVecReportFor(pos.ToIntVec3());
-                        ShiftTarget(report);
-
-                        Vector2 originV2 = new Vector2(originV3.x, originV3.z), destinationV2 = new Vector2(pos.x, pos.z);
-                        var positions = CIWS_ProjectilePropeties.NextPositions(shotRotation, shotAngle, originV2, destinationV2, maximumPredectionTicks, ShotHeight, false, Vector3.zero, ShotSpeed, originV3, -1f, -1f, -1f, -1f, ShotSpeed, 0).Skip(i - 1).Take(2).ToList();
-                        if (positions.Count < 2)
-                        {
-                            resultingLine = default(ShootLine);
-                            return false;
-                        }
-                        Vector2 ciwsPos1 = new Vector2(positions.First().x, positions.First().z), ciwsPos2 = new Vector2(positions.Last().x, positions.Last().z), targetPos2 = new Vector2(pos.x, pos.z);
-
-                        if (CE_Utility.TryFindIntersectionPoint(ciwsPos1, ciwsPos2, targetPos1, targetPos2, out var point))
-                        {
-                            resultingLine = new ShootLine(Shooter.Position, point.ToVector3().ToIntVec3());
-                            return true;
-                        }
-                        targetPos1 = targetPos2;
-                        i++;
-                    }
-                }
-            }
+            }           
+            
             return base.TryFindCEShootLineFromTo(root, targ, out resultingLine);
         }
     }
