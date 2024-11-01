72958afa2da025423d0da34917366a165e2ba74b
Safairette <71556532+Safairette@users.noreply.github.com>
2024-11-01 12:18:22 +0200

    Backpack props, maybe won't need 'em

---
+++ Source/CombatExtended/CombatExtended/Render/RenderNodeProperties_Backpack.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace CombatExtended
+{
+    public class RenderNodeProperties_Backpack : PawnRenderNodeProperties
+    {
+        public RenderNodeProperties_Backpack()
+        {
+            nodeClass = typeof(PawnRenderNode_Apparel);
+            workerClass = typeof(PawnRenderNodeWorker_Backpack);
+        }
+        public WornGraphicData offsetData;
+    }
+}
