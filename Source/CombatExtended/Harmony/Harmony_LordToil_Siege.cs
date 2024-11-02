e7872d4736c0a6b367cd7f40b5a3b9b1c35d4d10
mszabo <mszabo-oss@protonmail.com>
2024-11-02 05:11:28 +0100

    Support arbitrary ammosets for siege artillery
    The vanilla logic around sieges is fairly inflexible -- it mostly assumes
    that only 81mm mortars are used, which effectively prevents using
    modded artillery in sieges without hacks like the "raider artillery"
    in our VFE:S patch that consumes the 81mm ammoset.
    
    So, rework our patches around sieges to support artillery pieces
    with arbitrary ammosets, and remove raider artillery.

--- Source/CombatExtended/Harmony/Harmony_LordToil_Siege.cs
+++ Source/CombatExtended/Harmony/Harmony_LordToil_Siege.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
 using RimWorld;
@@ -8,66 +6,107 @@ using Verse;
 
 namespace CombatExtended.HarmonyCE
 {
+    /// <summary>
+    /// Transpile LordToil_Siege.LordToilTick to support artillery pieces with different ammosets than the 81mm mortar.
+    /// </summary>
     [HarmonyPatch(typeof(LordToil_Siege), "LordToilTick")]
-    internal static class Harmony_LordToil_Siege
+    internal static class Harmony_LordToil_Siege_LordToilTick
     {
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions,
+            ILGenerator generator)
         {
-            var codes = new List<CodeInstruction>(instructions);
-            var methodCustomCondition = typeof(Harmony_LordToil_Siege).GetMethod(nameof(CustomCondition), BindingFlags.Static | BindingFlags.Public);
-            var startIndex = -1;
-            var endIndex = -1;
-            MethodInfo targetMethod = AccessTools.Method(typeof(ThingDef), "get_IsShell"); // Start of the if condition
-            FieldInfo targetField = AccessTools.Field(typeof(DamageDef), "harmsHealth"); // End of the if condition
-            for (int i = 0; i < codes.Count; i++)
-            {
-                if (startIndex == -1)
-                {
-                    // Find the start of the if statement
-                    if (codes[i].opcode == OpCodes.Callvirt && codes[i].Calls(targetMethod))
-                    {
-                        startIndex = i - 1; // We want to edit -1 instruction from the call of IsShell
-                    }
-                }
-                else if (codes[i].opcode == OpCodes.Ldfld && codes[i].LoadsField(targetField))
-                {
-                    endIndex = i;
-                }
-            }
-            if (startIndex == -1 || endIndex == -1)
-            {
-                Log.Error("CombatExtended :: Harmony_LordToil_Siege couldn't find code block for patching");
-                return codes; // Don't modify as we couldn't find the original code block
-            }
-            else
-            {
-                codes[startIndex] = new CodeInstruction(OpCodes.Call, methodCustomCondition); // Call the new method for evaluation
-                codes.RemoveRange(startIndex + 1, endIndex - startIndex); // Remove the default code
-                return codes;
-            }
+            var methodCustomCondition = AccessTools.Method(typeof(SiegeUtility), nameof(SiegeUtility.IsValidShellType));
+            var dropAdditionalShells = AccessTools.Method(typeof(SiegeUtility), nameof(SiegeUtility.DropAdditionalShells));
+
+            var isShellMethod = AccessTools.PropertyGetter(typeof(ThingDef), nameof(ThingDef.IsShell));
+            var harmsHealthField = AccessTools.Field(typeof(DamageDef), nameof(DamageDef.harmsHealth));
+            var tryFindRandomShellDefMethod =
+                AccessTools.Method(typeof(TurretGunUtility), nameof(TurretGunUtility.TryFindRandomShellDef));
+
+            var codeMatcher = new CodeMatcher(instructions, generator);
+
+            var isShellPos = codeMatcher
+                .Start()
+                .MatchStartForward(CodeMatch.Calls(isShellMethod))
+                .ThrowIfInvalid("CombatExtended :: Harmony_LordToil_Siege_LordToilTick couldn't find call to IsShell")
+                .Pos;
+
+            var harmsHealthPos = codeMatcher.MatchStartForward(CodeMatch.LoadsField(harmsHealthField))
+                .ThrowIfInvalid("CombatExtended :: Harmony_LordToil_Siege_LordToilTick couldn't find harmsHealth")
+                .Pos;
+
+            // Consider all shell types usable by the siege when assessing how many are available to the siege
+            codeMatcher
+                .Advance(isShellPos - harmsHealthPos - 1)
+                .Insert(
+                    CodeInstruction.LoadArgument(0),
+                    new CodeInstruction(OpCodes.Call, methodCustomCondition)
+                )
+                .RemoveInstructionsInRange(isShellPos + 1, harmsHealthPos + 2);
+
+            var ifBlockStart = codeMatcher.Start()
+                .MatchStartForward(CodeMatch.Calls(tryFindRandomShellDefMethod))
+                .ThrowIfInvalid("CombatExtended :: Harmony_LordToil_Siege_LordToilTick couldn't find call to TryFindRandomShellDef")
+                .MatchEndBackwards(CodeMatch.Branches())
+                .ThrowIfInvalid("CombatExtended :: Harmony_LordToil_Siege_LordToilTick couldn't find start of enclosing if block")
+                .Pos;
+
+            var ifBlockEndLabel = (Label)codeMatcher.Operand;
+
+            // Drop shells for every type of artillery piece used by the siege
+            codeMatcher.MatchStartForward(new CodeMatch(instruction => instruction.labels.Contains(ifBlockEndLabel)))
+                .ThrowIfInvalid("CombatExtended :: Harmony_LordToil_Siege_LordToilTick couldn't find end of enclosing if block")
+                .Insert(
+                    CodeInstruction.LoadArgument(0),
+                    new CodeInstruction(OpCodes.Call, dropAdditionalShells)
+                )
+                .RemoveInstructionsInRange(ifBlockStart + 1, codeMatcher.Pos - 1);
+
+            return codeMatcher.Instructions();
         }
 
-        public static bool CustomCondition(Thing thing)
+    }
+
+    /// <summary>
+    /// Transpile LordToil_Siege.Init to remove the market price cap on the initially dropped shell stacks.
+    /// </summary>
+    [HarmonyPatch(typeof(LordToil_Siege), nameof(LordToil_Siege.Init))]
+    internal static class Harmony_LordToil_Siege_Init
+    {
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions,
+            ILGenerator generator)
         {
-            // Ensure check is the same here as from CombatExtended.HarmonyCE.Harmony_TurretGunUtility
-            var ammoDef = thing.def as AmmoDef;
-            if (ammoDef == null)
-            {
-                return false;
-            }
+            var codeMatcher = new CodeMatcher(instructions, generator);
 
-            // Ignore all non-shell defs.
-            if (ammoDef == null || !AmmoUtility.IsShell(ammoDef))
-            {
-                return false;
-            }
+            codeMatcher.MatchStartForward(CodeMatch.LoadsConstant(250f))
+                .ThrowIfInvalid("CombatExtended :: Harmony_LordToil_Siege_Init couldn't find market price cap")
+                .RemoveInstruction()
+                .Insert(new CodeInstruction(OpCodes.Ldc_R4, -1f));
 
-            // Check if shell is blacklisted
-            if (!ammoDef.spawnAsSiegeAmmo)
-            {
-                return false;
-            }
-            return true;
+            return codeMatcher.Instructions();
+        }
+    }
+
+    /// <summary>
+    /// Transpile LordToil_Siege.SetAsBuilder to ensure that builders are capable enough to build any siege artillery.
+    /// </summary>
+    [HarmonyPatch(typeof(LordToil_Siege), nameof(LordToil_Siege.SetAsBuilder))]
+    internal static class Harmony_LordToil_Siege_SetAsBuilder
+    {
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions,
+            ILGenerator generator)
+        {
+            var codeMatcher = new CodeMatcher(instructions, generator);
+
+            codeMatcher.MatchStartForward(
+                    CodeMatch.LoadsField(AccessTools.Field(typeof(ThingDefOf), nameof(ThingDefOf.Turret_Mortar))),
+                    CodeMatch.LoadsField(AccessTools.Field(typeof(BuildableDef), nameof(BuildableDef.constructionSkillPrerequisite)))
+                    )
+                .ThrowIfInvalid("CombatExtended :: Harmony_LordToil_Siege_SetAsBuilder couldn't find required construction skill")
+                .RemoveInstructions(2)
+                .Insert(CodeInstruction.LoadField(typeof(SiegeUtility), nameof(SiegeUtility.MinRequiredConstructionSkill)));
+
+            return codeMatcher.Instructions();
         }
     }
 }
