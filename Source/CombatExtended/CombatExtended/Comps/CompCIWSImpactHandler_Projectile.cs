2866d225fa997eb958a74925c6e46d195f115dc6
MaxDorob <maxdor81@gmail.com>
2024-11-07 01:58:18 +0600

    Missing class

---
+++ Source/CombatExtended/CombatExtended/Comps/CompCIWSImpactHandler_Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CombatExtended
+{
+    public class CompCIWSImpactHandler_Projectile : CompCIWSImpactHandler
+    {
+        public override void OnImpact(ProjectileCE projectile, DamageInfo dinfo)
+        {
+            base.OnImpact(projectile, dinfo);
+        }
+    }
+}
