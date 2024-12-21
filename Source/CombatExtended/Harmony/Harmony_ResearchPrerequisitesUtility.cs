51ff7989d3f0d7ffbf840d2f9fcf0dedea6c1f69
Safairette <71556532+Safairette@users.noreply.github.com>
2024-12-21 20:25:01 +0200

    Hide disabled ammo from research

---
+++ Source/CombatExtended/Harmony/Harmony_ResearchPrerequisitesUtility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace CombatExtended
+{
+    [HarmonyPatch(typeof(ResearchPrerequisitesUtility), nameof(ResearchPrerequisitesUtility.UnlockedDefsGroupedByPrerequisites))]
+    public class Harmony_ResearchPrerequisitesUtility
+    {
+        public static List<Pair<ResearchPrerequisitesUtility.UnlockedHeader, List<Def>>> Postfix(List<Pair<ResearchPrerequisitesUtility.UnlockedHeader, List<Def>>> input)
+        {
+            foreach (var pair in input)
+            {
+                pair.second.RemoveWhere(x => x is AmmoDef ammoDef && ammoDef.menuHidden);
+            }
+            return input;
+        }
+    }
+}
