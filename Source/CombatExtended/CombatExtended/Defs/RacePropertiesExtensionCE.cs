2c8d1c694906e9b6530344c1adaa53f1d8907dff
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-03-24 20:41:37 -0600

    RaceProperties CanParry
    * Fixes NRE on Verb_MeleeAttackCE.TryCast
    * Fixes NRE on the parry tracker
    * Scyther given parry for testing

--- Source/CombatExtended/CombatExtended/Defs/RacePropertiesExtensionCE.cs
+++ Source/CombatExtended/CombatExtended/Defs/RacePropertiesExtensionCE.cs
@@ -12,5 +12,7 @@ namespace CombatExtended
     public class RacePropertiesExtensionCE : DefModExtension
     {
         public BodyShapeDef bodyShape;
+        public bool canParry = false;
+        public int maxParry = 1;
     }
 }
