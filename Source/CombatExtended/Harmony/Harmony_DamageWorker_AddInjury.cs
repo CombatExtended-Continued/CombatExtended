ec36376f28a7c42191dd0aecd27fb0cb846c6b40
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-04-06 15:11:54 -0600

    Housekeeping

--- Source/CombatExtended/Harmony/Harmony_DamageWorker_AddInjury.cs
+++ Source/CombatExtended/Harmony/Harmony_DamageWorker_AddInjury.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Reflection;
 using System.Reflection.Emit;
 using Verse;
 using RimWorld;
-using UnityEngine;
 using HarmonyLib;
-using System.Runtime.Remoting.Messaging;
 
 namespace CombatExtended.HarmonyCE
 {
