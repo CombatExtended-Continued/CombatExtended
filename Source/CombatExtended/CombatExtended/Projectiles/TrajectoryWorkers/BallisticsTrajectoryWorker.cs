6c198ad0cefd4ec68294a881fac1c20d3417a026
perkinslr <logan@github.lp-programming.com>
2025-04-16 01:58:38 -0700

    De-potatoify ballistic projectile calculations

--- Source/CombatExtended/CombatExtended/Projectiles/TrajectoryWorkers/BallisticsTrajectoryWorker.cs
+++ Source/CombatExtended/CombatExtended/Projectiles/TrajectoryWorkers/BallisticsTrajectoryWorker.cs
@@ -26,48 +26,46 @@ namespace CombatExtended
 
         public override Vector3 MoveForward(ProjectileCE projectile)
         {
-            projectile.shotSpeed = projectile.velocity.magnitude * GenTicks.TicksPerRealSecond / 5;
-            var ep = BallisticMove(projectile.intendedTarget, projectile.ExactPosition, projectile.Props.speedGain, projectile.Props.speed, ref projectile.velocity, ref projectile.shotSpeed, ref projectile.mass, ref projectile.ballisticCoefficient, ref projectile.radius, ref projectile.gravity);
-            return ep;
+            projectile.shotSpeed = GetSpeed(projectile.velocity);
+            return BallisticMove(projectile);
         }
-        protected virtual Vector3 BallisticMove(LocalTargetInfo currentTarget, Vector3 exactPosition, float speedGain, float maxSpeed, ref Vector3 velocity, ref float shotSpeed, ref float mass, ref float ballisticCoefficient, ref float radius, ref float gravity)
+        protected virtual Vector3 BallisticMove(ProjectileCE projectile)
         {
-            Accelerate(currentTarget, radius, ballisticCoefficient, mass, gravity, speedGain, maxSpeed, exactPosition, ref velocity, ref shotSpeed);
-            shotSpeed = GetSpeed(velocity);
-            return exactPosition + velocity;
+            Accelerate(projectile);
+            return projectile.ExactPosition + projectile.velocity;
         }
-        protected virtual void Accelerate(LocalTargetInfo currentTarget, float radius, float ballisticCoefficient, float mass, float gravity, float speedGain, float maxSpeed, Vector3 exactPosition, ref Vector3 velocity, ref float shotSpeed)
+        protected virtual void Accelerate(ProjectileCE projectile)
         {
-            AffectedByDrag(radius, shotSpeed, ballisticCoefficient, mass, ref velocity);
-            AffectedByGravity(gravity, ref velocity);
-            ReactiveAcceleration(currentTarget, speedGain, maxSpeed, exactPosition, ref velocity, ref shotSpeed);
+            AffectedByDrag(projectile);
+            AffectedByGravity(projectile);
+            ReactiveAcceleration(projectile);
         }
 
-        protected virtual void ReactiveAcceleration(LocalTargetInfo currentTarget, float speedGain, float maxSpeed, Vector3 exactPosition, ref Vector3 velocity, ref float shotSpeed)
+        protected virtual void ReactiveAcceleration(ProjectileCE projectile)
         {
-            var speedChange = Mathf.Max(Mathf.Min(maxSpeed - shotSpeed, speedGain), 0f);
-            if (speedChange > 0.001f)
+            if (projectile.fuelTicks > 0)
             {
-                velocity = velocity + GetVelocity(speedChange, Vector3.zero, velocity);
+                projectile.fuelTicks--;
+                projectile.velocity += projectile.velocity.normalized * projectile.Props.speedGain;
             }
         }
 
-        protected void AffectedByGravity(float gravity, ref Vector3 velocity)
+        protected void AffectedByGravity(ProjectileCE projectile)
         {
-            var original = velocity;
-            velocity.y -= gravity / GenTicks.TicksPerRealSecond;
+            projectile.velocity.y -= projectile.gravity / GenTicks.TicksPerRealSecond;
         }
 
-        protected void AffectedByDrag(float radius, float shotSpeed, float ballisticCoefficient, float mass, ref Vector3 velocity)
+        protected void AffectedByDrag(ProjectileCE projectile)
         {
-            float crossSectionalArea = radius;
-            crossSectionalArea *= crossSectionalArea * 3.14159f;
+            Vector3 velocity = projectile.velocity;
+            float crossSectionalArea = projectile.radius;
+            crossSectionalArea *= crossSectionalArea * Mathf.PI;
             // 2.5f is half the mass of 1m² x 1cell of air.
             var q = 2.5f * velocity.sqrMagnitude;
-            var dragForce = q * crossSectionalArea / ballisticCoefficient;
+            var dragForce = q * crossSectionalArea / projectile.ballisticCoefficient;
             // F = mA
             // A = F / m
-            var a = (float)-dragForce / mass;
+            var a = (float)-dragForce / projectile.mass;
             var normalized = velocity.normalized;
             if (a * a > velocity.sqrMagnitude)
             {
