6a21df36ba49e8b5f86f6fbf1da14cb208a054c3
Logan Perkins <logan@git.lp-programming.com>
2025-05-04 21:57:39 -0700

    add setting for arrow flecks

--- Source/CombatExtended/Contrib/ProjectileImpactFX/ImpactFleckThrower.cs
+++ Source/CombatExtended/Contrib/ProjectileImpactFX/ImpactFleckThrower.cs
@@ -150,7 +150,7 @@ namespace ProjectileImpactFX
                 map.flecks.CreateFleck(creationData);
             }
 
-            if (StuckProjectileFleck != null)
+            if (Settings.StuckArrowsAsFlecks && StuckProjectileFleck != null)
             {
                 FleckCreationData creationData = FleckMaker.GetDataStatic(loc, map, StuckProjectileFleck);
                 creationData.scale = ext.StuckProjectileFleckSize;
