653d40a3735b6016cba2f4d6e585f76ee52c448b
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-05-17 10:33:00 -0600

    Map null check

--- Source/CombatExtended/CombatExtended/MoteMakerCE.cs
+++ Source/CombatExtended/CombatExtended/MoteMakerCE.cs
@@ -8,6 +8,7 @@ namespace CombatExtended
     {
         public static void ThrowText(Vector3 loc, Map map, string text, float timeBeforeStartFadeout = -1f)
         {
+            if (map == null) return;
             Rand.PushState();
             MoteMaker.ThrowText(loc, map, text, timeBeforeStartFadeout);
             Rand.PopState();
@@ -15,6 +16,7 @@ namespace CombatExtended
 
         public static void ThrowText(Vector3 loc, Map map, string text, Color color, float timeBeforeStartFadeout = -1f)
         {
+            if (map == null) return;
             Rand.PushState();
             MoteMaker.ThrowText(loc, map, text, color, timeBeforeStartFadeout);
             Rand.PopState();
