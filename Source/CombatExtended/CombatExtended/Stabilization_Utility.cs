a09b8e7e766ba5bbcb501b32109831141b007ca3
Safairette <71556532+Safairette@users.noreply.github.com>
2025-06-12 15:48:54 +0300

    Remove pickup methods from stabilization util Got a bit too overzealous with the pasting

--- Source/CombatExtended/CombatExtended/Stabilization_Utility.cs
+++ Source/CombatExtended/CombatExtended/Stabilization_Utility.cs
@@ -112,36 +112,4 @@ public class Stabilization_Utility
             }
         }
     }
-    
-    [global::CombatExtended.Compatibility.Multiplayer.SyncMethod]
-    private static void Pickup(Pawn pawn, Thing item)
-    {
-        item.SetForbidden(false, false);
-        Job job = JobMaker.MakeJob(JobDefOf.TakeInventory, item);
-        job.count = 1;
-        pawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
-        pawn.Notify_HoldTrackerItem(item, 1);
-        PlayerKnowledgeDatabase.KnowledgeDemonstrated(CE_ConceptDefOf.CE_InventoryWeightBulk, KnowledgeAmount.SpecificInteraction);
-    }
-    
-    [global::CombatExtended.Compatibility.Multiplayer.SyncMethod]
-    private static void PickupAll(Pawn pawn, Thing item)
-    {
-        item.SetForbidden(false, false);
-        Job job = JobMaker.MakeJob(JobDefOf.TakeInventory, item);
-        job.count = item.stackCount;
-        pawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
-        pawn.Notify_HoldTrackerItem(item, item.stackCount);
-        PlayerKnowledgeDatabase.KnowledgeDemonstrated(CE_ConceptDefOf.CE_InventoryWeightBulk, KnowledgeAmount.SpecificInteraction);
-    }
-    
-    [global::CombatExtended.Compatibility.Multiplayer.SyncMethod]
-    private static void PickupCount(Pawn pawn, Thing item, int selectCount)
-    {
-        item.SetForbidden(false, false);
-        Job job = JobMaker.MakeJob(JobDefOf.TakeInventory, item);
-        job.count = selectCount;
-        pawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
-        pawn.Notify_HoldTrackerItem(item, selectCount);
-    }
 }
