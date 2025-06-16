c7225a42316f1eda596e4c3d490d3a97e5c3068f
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-06-16 15:41:24 -0600

    Fix AI GasMask Wearing

---
+++ Source/CombatExtended/Harmony/Harmony_GasUtility_PawnGasEffectsTickInterval.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using CombatExtended.AI;
+using HarmonyLib;
+using Verse;
+
+namespace CombatExtended.HarmonyCE
+{
+    internal static class Harmony_GasUtility
+    {
+        /// <summary>
+        /// Transpile <see cref="GasUtility.DoAirbornePawnToxicDamage"/> to tell AI pawns to wear mask
+        /// when exposed to toxic gas
+        /// </summary>
+        [HarmonyPatch(typeof(GasUtility), nameof(GasUtility.PawnGasEffectsTickInterval))]
+        static class Patch_PawnGasEffectsTickInterval
+        {
+
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+            {
+                List<CodeInstruction> codes = instructions.ToList();
+
+                MethodInfo targetMethod = AccessTools.Method(typeof(GasUtility), nameof(GasUtility.ShouldGetGasExposureHediff));
+                MethodInfo additionalMethod = AccessTools.Method(typeof(Harmony_GasUtility), nameof(Harmony_GasUtility.TryNotify_ShouldEquipGasMask));
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    // Injecting this before the target method, so that pawns can put the gas mask on to avoid the debilitating exposure hediff.
+                    if (codes[i].opcode == OpCodes.Call && codes[i].operand as MethodInfo == targetMethod)
+                    {
+                        codes.Insert(i, new CodeInstruction(OpCodes.Call, additionalMethod));
+                        codes.Insert(i, new CodeInstruction(OpCodes.Dup));
+                        break;
+                    }
+                }
+                return codes;
+            }
+        }
+
+        public static void TryNotify_ShouldEquipGasMask(Pawn pawn)
+        {
+            if (pawn.RaceProps.Humanlike && !pawn.IsSubhuman)
+            {
+                pawn.TryGetComp<CompTacticalManager>()?.GetTacticalComp<CompGasMask>()?.Notify_ShouldEquipGasMask();
+            }
+        }
+    }
+}
