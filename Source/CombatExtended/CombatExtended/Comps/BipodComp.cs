80b410fc04130a842120e9ba6dd22ceaaff4b3b2
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-07-06 17:06:02 -0600

    Fix Bipod Spawn and Load Verbs

--- Source/CombatExtended/CombatExtended/Comps/BipodComp.cs
+++ Source/CombatExtended/CombatExtended/Comps/BipodComp.cs
@@ -40,13 +40,27 @@ namespace CombatExtended
         {
             base.PostSpawnSetup(respawningAfterLoad);
             compFireMode = this.parent.TryGetComp<CompFireModes>();
+            SetUpInvert(this.parent);
         }
+
         public override void PostExposeData()
         {
             Scribe_Values.Look(ref IsSetUpRn, "isBipodSetUp");
             if (Scribe.mode == LoadSaveMode.PostLoadInit)
             {
                 compFireMode = this.parent.TryGetComp<CompFireModes>();
+                if (Controller.settings.BipodMechanics && compFireMode != null)
+                {
+
+                    if (IsSetUpRn)
+                    {
+                        SetupFromLoad();
+                    }
+                    else
+                    {
+                        SetUpInvert(this.parent);
+                    }
+                }
             }
             base.PostExposeData();
         }
@@ -186,8 +200,15 @@ namespace CombatExtended
                 }
                 starts = 5;
             }
+        }
 
-
+        public void SetupFromLoad()
+        {
+            VerbPropertiesCE changed = CopyVerbPropsFromThing(this.parent);
+            changed.range += Props.additionalrange;
+            changed.recoilAmount *= Props.recoilMulton * Props.recoilMultoff;
+            changed.warmupTime *= Props.warmupMult * Props.warmupPenalty;
+            AssignVerbProps(this.parent, changed);
         }
         #endregion
     }
