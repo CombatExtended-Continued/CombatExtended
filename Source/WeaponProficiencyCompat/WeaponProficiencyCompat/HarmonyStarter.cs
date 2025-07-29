fc6938f505f72d23e8814a22b043a29d96fdf98a
SaltyKarl <84060664+SaltyKarl@users.noreply.github.com>
2025-07-30 00:08:55 +0800

    HarmonyStarter to load patch

---
+++ Source/WeaponProficiencyCompat/WeaponProficiencyCompat/HarmonyStarter.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CombatExtended.Compatibility.WeaponProficiency.HarmonyPatches
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyStarter
+    {
+        static HarmonyStarter()
+        {
+            Harmony harmony = new Harmony("CombatExtended.Compatibility.WeaponProficiency");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Source/WeaponProficiencyCompat/WeaponProficiencyCompat/WeaponProficiencyCompatVerb.cs
@@ -0,0 +1,22 @@
+﻿using CombatExtended;
+using HarmonyLib;
+using System;
+using Verse;
+using WeaponProficiency.Patches;
+
+namespace CombatExtended.Compatibility.WeaponProficiencyVerb
+{
+    [HarmonyPatch(typeof(Pawn_HealthTracker_Notify_UsedVerb_WeaponProficiencyPatch))]
+    [HarmonyPatch("IsValidVerb")]
+    public static class IsValidVerb_PostfixPatch
+    {
+        // Correct postfix signature: use __result to modify the return value
+        static void Postfix(Verb verb, ref bool __result)
+        {
+            if (verb is Verb_LaunchProjectileCE)
+            {
+                __result = true;
+            }
+        }
+    }
+}
