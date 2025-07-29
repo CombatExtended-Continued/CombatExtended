94e06e8ac7c472a4550b2a0bdeac09e8f6f29b41
SaltyKarl <84060664+SaltyKarl@users.noreply.github.com>
2025-07-29 21:59:30 +0800

    Update WeaponProficiencyCompat.cs

--- Source/WeaponProficiencyCompat/WeaponProficiencyCompat/WeaponProficiencyCompat.cs
+++ Source/WeaponProficiencyCompat/WeaponProficiencyCompat/WeaponProficiencyCompat.cs
@@ -10,7 +10,7 @@ namespace CombatExtended.Compatibility.WeaponProficiencyCompat
 {
     [HarmonyPatch(typeof(Pawn_HealthTracker_Notify_UsedVerb_WeaponProficiencyPatch), MethodType.Constructor)]
     [HarmonyPatch(MethodType.Normal)]
-    internal static class Harmony_VerbLaunchProjectilePatch_Copy
+    internal static class Harmony_VerbLaunchProjectilePatch
     {
         private static bool Comparer(Verb verb)
         {
@@ -19,31 +19,31 @@ namespace CombatExtended.Compatibility.WeaponProficiencyCompat
 
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
         {
-            bool FoundIsInst = false;
-            bool DoPatch = false;
+            bool foundIsInst = false;
+            bool doPatch = false;
             bool blockFlag = false;
             object operand = null;
             foreach (CodeInstruction instruction in instructions)
             {
-                if (DoPatch)
+                if (doPatch)
                 {
-                    DoPatch = false;
+                    doPatch = false;
                     yield return new(OpCodes.Ldarg_1);
-                    yield return new(OpCodes.Call, AccessTools.Method(typeof(Harmony_VerbLaunchProjectilePatch_Copy), "Comparer"));
+                    yield return new(OpCodes.Call, AccessTools.Method(typeof(Harmony_VerbLaunchProjectilePatch), "Comparer"));
                     CodeInstruction codeInstruction = new(OpCodes.Brtrue_S, operand);
                 }
-                if (FoundIsInst)
+                if (foundIsInst)
                 {
                     if (instruction.opcode == OpCodes.Brtrue_S)
                     {
-                        DoPatch = true;
-                        FoundIsInst = false;
+                        doPatch = true;
+                        foundIsInst = false;
                         operand = instruction.operand;
                     }
                 }
                 if (!blockFlag && instruction.opcode == OpCodes.Isinst && instruction.operand.GetType() == typeof(Verb_MeleeAttack))
                 {
-                    FoundIsInst = true;
+                    foundIsInst = true;
                     blockFlag = true;
                 }
                 yield return instruction;
