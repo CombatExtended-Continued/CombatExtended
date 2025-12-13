b7a47e4e6861e43dfaee04d5f833396aa63b76eb
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-12-12 18:22:39 -0700

    Detect Unpatched Tools

---
+++ Source/CombatExtended/Compatibility/WeaponAutoPatcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using Verse;
+
+namespace CombatExtended.Compatibility;
+
+public static class LogUnpatchedTools
+{
+    public static void DetectAndLogUnpatchedTools()
+    {
+        bool logUnpatchedDefsFlag = Controller.settings.LogUnpatchedDefs;
+        if (!logUnpatchedDefsFlag)
+        {
+            return;
+        }
+        foreach (ThingDef def in DefDatabase<ThingDef>.AllDefs)
+        {
+            if (def.tools == null)
+            {
+                continue;
+            }
+            bool unpatchedTool = false;
+            for (int i = 0; i < def.tools.Count; i++)
+            {
+                Tool tool = def.tools[i];
+                if (tool is null or ToolCE)
+                {
+                    continue;
+                }
+                unpatchedTool = true;
+                break;
+            }
+            if (!unpatchedTool)
+            {
+                continue;
+            }
+            Log.Message($"CE: Detected unpatched tool on {def.defName}. Recommend patching or turning on autopatcher");
+        }
+
+    }
+}
+
