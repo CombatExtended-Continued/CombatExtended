5e82b266613282a0bb1654a1455eef6aa5864025
MaxDorob <maxdor81@gmail.com>
2025-12-17 02:51:51 +0600

    Set expiryInterval to Wait_Combat job to let other jobs start

---
+++ Source/CombatExtended/Harmony/Harmony_JobGiver_Orders_TryGiveJob.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Utility;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace CombatExtended.HarmonyCE;
+[HarmonyPatch(typeof(JobGiver_Orders), "TryGiveJob")]
+internal static class Harmony_JobGiver_Orders_TryGiveJob
+{
+    internal static void Postfix(Pawn pawn, Job __result)
+    {
+        if (__result != null && __result.expiryInterval < 0)
+        {
+            __result.expiryInterval = GenTicks.TicksPerRealSecond * 5; //Let other jobs (like a reload) start sometimes
+        }
+    }
+}
