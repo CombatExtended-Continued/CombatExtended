f5239697934c04e312f21442fb75fe0ff00cae53
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-12-20 13:31:56 -0700

    Update Unpatched Tool Detector

--- Source/CombatExtended/Compatibility/LogUnpatchedTools.cs
+++ Source/CombatExtended/Compatibility/LogUnpatchedTools.cs
@@ -36,7 +36,38 @@ public static class LogUnpatchedTools
             }
             Log.Message($"CE: Detected unpatched tool(s) on {def.defName}. Recommend patching or turning on autopatcher. \nUnpatched Tool Capacities:\n {unpatchedToolList}");
         }
+        foreach (HediffDef hediff in DefDatabase<HediffDef>.AllDefs)
+        {
+            string unpatchedToolList = "";
+            if (hediff.comps.NullOrEmpty())
+            {
+                continue;
+            }
+            bool unpatchedTool = false;
+            foreach (HediffCompProperties hediffCompProperties in hediff.comps)
+            {
+                if (hediffCompProperties is not HediffCompProperties_VerbGiver hediffVerbGiver)
+                {
+                    continue;
+                }
+                for (int i = 0; i < hediffVerbGiver.tools.Count; i++)
+                {
+                    Tool tool = hediffVerbGiver.tools[i];
+                    if (tool is null or ToolCE)
+                    {
+                        continue;
+                    }
+                    unpatchedTool = true;
+                    unpatchedToolList += tool + "\n";
+                }
+                if (!unpatchedTool)
+                {
+                    continue;
+                }
+                Log.Message($"CE: Detected unpatched tool(s) on {hediff.}. Recommend patching or turning on autopatcher. \nUnpatched Tool Capacities:\n {unpatchedToolList}");
+            }
+
+        }
 
     }
 }
-
