4cd9dc2fbc9a003ee12d1154d73aefd4d7434995
mszabo <mszabo-oss@protonmail.com>
2025-12-22 00:34:21 +0100

    Fix relative path handling

--- Source/Tests/LoadFoldersXmlTests.cs
+++ Source/Tests/LoadFoldersXmlTests.cs
@@ -18,19 +18,53 @@ public class LoadFoldersXmlTests
         var root = doc.DocumentElement;
         var entries = root.FirstChild.SelectNodes("//li");
         HashSet<(string, string)> modsWithPath = [];
+        Dictionary<string, string> relativePaths = [];
+
         foreach (XmlNode entry in entries)
         {
             var packageId = entry.Attributes.GetNamedItem("IfModActive")?.Value ?? "CETeam.CombatExtended";
-            var patchesPath = entry.InnerText;
+            var modSpecificPath = entry.InnerText;
 
-            Assert.False(modsWithPath.Contains((packageId, patchesPath)), $"Mod {packageId} is listed in LoadFolders.xml twice, both pointing to {patchesPath}");
-            modsWithPath.Add((packageId, patchesPath));
+            Assert.False(modsWithPath.Contains((packageId, modSpecificPath)), $"Mod {packageId} is listed in LoadFolders.xml twice, both pointing to {modSpecificPath}");
+            modsWithPath.Add((packageId, modSpecificPath));
 
-            DirectoryInfo patchesDir = new(Path.Combine(loadFoldersDir.FullName, patchesPath));
-            Assert.True(patchesDir.Exists, $"Mod {packageId} has an invalid patch directory {patchesPath} in LoadFolders.xml");
+            DirectoryInfo loadFolder = new(Path.Combine(loadFoldersDir.FullName, modSpecificPath.TrimStart(Path.DirectorySeparatorChar)));
+            Assert.True(loadFolder.Exists, $"Mod {packageId} has an invalid patch directory {modSpecificPath} in LoadFolders.xml");
+
+            foreach (var xmlFile in GetPatchesAndDefs(loadFolder))
+            {
+                var relPath = Path.GetRelativePath(loadFolder.FullName, xmlFile);
+                if (relativePaths.TryGetValue(relPath, out var duplicatePackageId))
+                {
+                        Assert.Fail($"Relative paths collision found: {relPath} is duplicated for mods {duplicatePackageId} and {packageId}");
+                }
+
+                relativePaths[relPath] = packageId;
+            }
         }
 
         Assert.True(modsWithPath.Count > 0, "No entries found while checking LoadFolders.xml, is the file valid?");
+    }
+
+    /// <summary>
+    /// Get all XML file paths under the Defs/ and Patches/ subdirectories of a single LoadFolders.xml entry.
+    /// </summary>
+    private IEnumerable<string> GetPatchesAndDefs(DirectoryInfo loadFolder)
+    {
+        List<string> folders = ["Defs", "Patches"];
+        foreach (var folder in folders)
+        {
+            DirectoryInfo subFolder = new(Path.Combine(loadFolder.FullName, folder));
+            if (!subFolder.Exists)
+            {
+                continue;
+            }
+
+            foreach (var file in Directory.EnumerateFiles(subFolder.FullName, "*.xml", SearchOption.AllDirectories))
+            {
+                yield return file;
+            }
+        }
     }
 
     private DirectoryInfo FindLoadFoldersXmlDirectory()
@@ -39,8 +73,7 @@ public class LoadFoldersXmlTests
              directoryInfo != null;
              directoryInfo = directoryInfo.Parent)
         {
-            var loadFoldersXml =
-                new FileInfo(directoryInfo.FullName + Path.DirectorySeparatorChar + "LoadFolders.xml");
+            FileInfo loadFoldersXml = new(Path.Combine(directoryInfo.FullName, "LoadFolders.xml"));
             if (loadFoldersXml.Exists)
             {
                 return directoryInfo;
