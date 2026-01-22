df8f0bc49b57ab765758cff0fc419db04327ddaa
Haecriver <haecriver@outlook.fr>
2026-01-22 19:58:57 +0100

    New Project created

---
+++ Source/VGECompat/VGECompat/Building_GravshipTurrentCE.cs
@@ -0,0 +1,25 @@
+﻿using CombatExtended.Compatibility.VGECompat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VanillaGravshipExpanded;
+
+namespace CombatExtended.Compatibility.VGECompat;
+
+public class Building_GravshipTurrentCE: Building_TurretGunCE
+{
+    #region License
+    // Any VGE Code used for compatibility has been taken from the following source
+    // https://github.com/Vanilla-Expanded/VanillaGravshipExpanded/blob/main/Source/Things/Building_GravshipTurret.cs
+    #endregion
+
+    // Explicit type conversion that will be used in SOS2 code for turrets added to heat net turrets list.
+    // Also allows conversion of Building_ShipTurretCE into Building_ShipTurret as the SaveOurShip2.ShipCombatProjectile class which is used in Verb_ShootShipCE requires a Building_ShipTurret to be passed into its constructor.
+    public Building_GravshipTurret ToBuilding_ShipTurret()
+    {
+        return new GravshipTurretWrapperCE(this);
+    }
+
+}
---
+++ Source/VGECompat/VGECompat/GravshipTurretWrapperCE.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VanillaGravshipExpanded;
+
+namespace CombatExtended.Compatibility.VGECompat;
+
+public class GravshipTurretWrapperCE: Building_GravshipTurret
+{
+    // This is so that we can fire ShipCombatProjectile from Verb_ShootShipCE by converting Building_ShipTurretCE into something inheriting from Building_ShipTurret.
+    // This class is only used when creating a new SaveOurShip2.ShipCombatProjectile and passing through a Building_ShipTurretCE as one of its constructor arguements.
+
+    // This class uses reflection to automatically delegate all properties and fields. The performance impact "should" be minimal as once the delegation is complete subsequent access calls will be direct.
+    // There will be an overhead for the initial conversion of each Building_ShipTurretCE though.
+
+    public Building_GravshipTurrentCE turretCE;
+
+    public GravshipTurretWrapperCE(Building_GravshipTurrentCE turretCE)
+    {
+        this.turretCE = turretCE;
+    }
+}
