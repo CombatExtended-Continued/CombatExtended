572f5b446c185a992c74e795cae82c71231109b2
Haecriver <haecriver@outlook.fr>
2026-02-16 17:03:37 +0100

    reintroduce other files

---
+++ Source/VGECompat/VGECompat/Things/Building_EnemyMechTurretCE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VGECompat.VGECompat.Things
+{
+    internal class Building_EnemyMechTurretCE
+    {
+    }
+}
