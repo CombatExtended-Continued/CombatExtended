cf06097854e94600cbfd6b88d70904dafbc5fc39
Haecriver <haecriver@outlook.fr>
2026-02-16 16:51:04 +0100

    try to reintroduce Verb_ShootWithSmokeCE

---
+++ Source/VGECompat/VGECompat/Verbs/Verb_ShootWithSmokeCE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VGECompat.VGECompat.Verbs
+{
+    internal class Verb_ShootWithSmokeCE
+    {
+    }
+}
