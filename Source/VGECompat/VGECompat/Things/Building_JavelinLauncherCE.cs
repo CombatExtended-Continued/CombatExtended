24420fa04ad4f0aebd4e4ec61792664f24492fae
Haecriver <haecriver@outlook.fr>
2026-02-16 17:08:30 +0100

    modified them

--- Source/VGECompat/VGECompat/Things/Building_JavelinLauncherCE.cs
+++ Source/VGECompat/VGECompat/Things/Building_JavelinLauncherCE.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
 
-namespace VGECompat.VGECompat.Things
+#region License
+// This file includes modified portions of code from:
+// https://github.com/Vanilla-Expanded/VanillaGravshipExpanded/blob/main/Source/Things/Building_JavelinLauncher.cs
+//
+// Original code © Oskar Potocki and the Vanilla Gravship Expanded Team.
+// Incorporated with permission for Combat Extended-Vanilla Gravship Expended compatibility purposes only.
+// All rights to the original code remain with the original authors.
+#endregion
+
+namespace CombatExtended.Compatibility.VGECompat;
+
+[StaticConstructorOnStartup]
+public class Building_JavelinLauncherCE : Building_GravshipTurretCE
 {
-    internal class Building_JavelinLauncherCE
+    public CompRefuelable RefuelableComp => this.TryGetComp<CompRefuelable>();
+    public override bool CanFire
     {
+        get
+        {
+            // make a check here to prevent false artillery command
+            if (AttackVerb.verbProps.consumeFuelPerShot > 0)
+            {
+                if (RefuelableComp == null)
+                {
+                    return false;
+                }
+
+                if (RefuelableComp.Fuel < AttackVerb.verbProps.consumeFuelPerShot)
+                {
+                    return false;
+                }
+            }
+            return base.CanFire;
+        }
+    }
+    public override Material TurretTopMaterial
+    {
+        get
+        {
+            if (RefuelableComp?.IsFull ?? false)
+            {
+                return def.building.turretGunDef.building.turretTopLoadedMat;
+            }
+            return def.building.turretTopMat;
+        }
     }
 }
--- Source/VGECompat/VGECompat/Things/Building_JavelinPodCE.cs
+++ Source/VGECompat/VGECompat/Things/Building_JavelinPodCE.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Verse;
 
-namespace VGECompat.VGECompat.Things
+#region License
+// This file includes modified portions of code from:
+// https://github.com/Vanilla-Expanded/VanillaGravshipExpanded/blob/main/Source/Things/Building_JavelinPod.cs
+//
+// Original code © Oskar Potocki and the Vanilla Gravship Expanded Team.
+// Incorporated with permission for Combat Extended-Vanilla Gravship Expended compatibility purposes only.
+// All rights to the original code remain with the original authors.
+#endregion
+
+namespace CombatExtended.Compatibility.VGECompat;
+
+[StaticConstructorOnStartup]
+public class Building_JavelinPodCE : Building_JavelinLauncherCE
 {
-    internal class Building_JavelinPodCE
-    {
-    }
+    public override bool CanAutoAttack => true;
 }
