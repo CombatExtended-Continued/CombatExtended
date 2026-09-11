70c128840084317ef961cdba8df2a66572e75307
Haecriver <haecriver@outlook.fr>
2026-09-11 23:21:42 +0200

    create new project

---
+++ Source/VGECompat2/VGE2Compat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using Verse;
+using CombatExtended.Loader;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace CombatExtended.Compatibility.VGE2Compat;
+
+[StaticConstructorOnStartup]
+public class VGE2Compat : IModPart
+{
+    private static Harmony harmony;
+
+    public Type GetSettingsType()
+    {
+        return null;
+    }
+
+    public IEnumerable<string> GetCompatList()
+    {
+        yield break;
+    }
+
+    public void PostLoad(ModContentPack content, ISettingsCE _)
+    {
+        harmony = new Harmony("CombatExtended.Compatibility.VGE2Compat");
+        LongEventHandler.ExecuteWhenFinished(() =>
+        {
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        });
+    }
+}
